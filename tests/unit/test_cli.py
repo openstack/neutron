de4177e2614db8b02f98b71298403f7218ff399a
Salvatore Orlando <salvatore.orlando@eu.citrix.com>
2011-08-10 18:22:17 +0100

    skeleton for cli unit tests

---
+++ tests/unit/test_cli.py
@@ -0,0 +1,41 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2010-2011 ????
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#    @author: Salvatore Orlando, Citrix Systems
+
+""" Module containing unit tests for Quantum 
+    command line interface
+    
+"""
+
+
+import logging
+import sys
+import unittest
+
+from quantum import api as server
+
+LOG = logging.getLogger('quantum.tests.test_cli')
+
+class CLITest(unittest.TestCase):
+
+    def setUp(self):
+        """Prepare the test environment"""
+        pass
+    
+    def tearDown(self):
+        """Clear the test environment"""
+        pass
