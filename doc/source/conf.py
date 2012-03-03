1769ce8581ad532d203c3c09bec35fb1752b3f72
Jesse Andrews <anotherjesse@gmail.com>
2012-03-02 16:09:16 -0800

    add git commit date / sha1 to sphinx html docs
    Change-Id: Ie286200e3f569d6d5b0debf66baf24229b0b86ab

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -169,6 +169,8 @@ html_static_path = ['_static']
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
 #html_last_updated_fmt = '%b %d, %Y'
+git_cmd = "git log --pretty=format:'%ad, commit %h' --date=local -n1"
+html_last_updated_fmt = os.popen(git_cmd).read()
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
