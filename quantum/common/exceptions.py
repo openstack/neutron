0d3cfea2e193a17afbf65c765dabbbb5cf18fa9c
Zhongyue Luo <lzyeval@gmail.com>
2012-06-14 16:09:34 +0800

    Use openstack.common.exception
    Fixes bug #1013040
    
    1. Edit openstack-common.conf and import openstack.common.exception
    2. Remove redundant code
    
    Change-Id: Ia440422e283f8e6796d9a1f720d73b3bcf2f981e

--- quantum/common/exceptions.py
+++ quantum/common/exceptions.py
@@ -19,28 +19,19 @@
 Quantum base exception handling.
 """
 
+from quantum.openstack.common.exception import Error
+from quantum.openstack.common.exception import OpenstackException
 
-class QuantumException(Exception):
+
+class QuantumException(OpenstackException):
     """Base Quantum Exception
 
-    Taken from nova.exception.NovaException
     To correctly use this class, inherit from it and define
     a 'message' property. That message will get printf'd
     with the keyword arguments provided to the constructor.
 
     """
     message = _("An unknown exception occurred.")
-
-    def __init__(self, **kwargs):
-        try:
-            self._error_string = self.message % kwargs
-
-        except Exception:
-            # at least get the core message out if something happened
-            self._error_string = self.message
-
-    def __str__(self):
-        return self._error_string
 
 
 class NotFound(QuantumException):
@@ -95,23 +86,6 @@ class AlreadyAttached(QuantumException):
     message = _("Unable to plug the attachment %(att_id)s into port "
                 "%(port_id)s for network %(net_id)s. The attachment is "
                 "already plugged into port %(att_port_id)s")
-
-
-class ProcessExecutionError(IOError):
-    def __init__(self, stdout=None, stderr=None, exit_code=None, cmd=None,
-                 description=None):
-        if description is None:
-            description = "Unexpected error while running command."
-        if exit_code is None:
-            exit_code = '-'
-        message = "%s\nCommand: %s\nExit code: %s\nStdout: %r\nStderr: %r" % (
-                  description, cmd, exit_code, stdout, stderr)
-        IOError.__init__(self, message)
-
-
-class Error(Exception):
-    def __init__(self, message=None):
-        super(Error, self).__init__(message)
 
 
 class MalformedRequestBody(QuantumException):
