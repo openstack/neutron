e1b3e1678d37756692796b2099e75a872a8b35ea
mouad benchchaoui <m.benchchaoui@cloudbau.de>
2013-03-30 11:56:44 +0100

    Pass metadata port to metadata proxy
    Make the l3 agent pass the port used by the metadata service to the router namespace proxy when
    creating it and add also new tests for this.
    
    Fixes LP# 1160955
    Change-Id: Iec8a5238345b26e70b0aa1dc96a896e26af34722

--- quantum/agent/l3_agent.py
+++ quantum/agent/l3_agent.py
@@ -19,8 +19,6 @@
 #
 """
 
-import sys
-
 import eventlet
 from eventlet import semaphore
 import netaddr
@@ -258,7 +256,8 @@ class L3NATAgent(manager.Manager):
             proxy_cmd = ['quantum-ns-metadata-proxy',
                          '--pid_file=%s' % pid_file,
                          '--router_id=%s' % router_info.router_id,
-                         '--state_path=%s' % self.conf.state_path]
+                         '--state_path=%s' % self.conf.state_path,
+                         '--metadata_port=%s' % self.conf.metadata_port]
             proxy_cmd.extend(config.get_log_args(
                 cfg.CONF, 'quantum-ns-metadata-proxy-%s.log' %
                 router_info.router_id))
