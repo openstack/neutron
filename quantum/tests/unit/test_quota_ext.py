4c49b238c3cf54b0e0bfa5f8799112a30957a724
Stephen Ma <stephen.ma@hp.com>
2013-05-10 20:24:55 -0700

    DbQuotaDriver allows negative quotas to be set.
    Bug 1178886
    
    Change-Id: I06ece65c2d77b8258bb871bb396edc5c145c383a
    (cherry picked from commit 9e7933dc55ea086b3a2977d9c25ab503174ba7af)

--- quantum/tests/unit/test_quota_ext.py
+++ quantum/tests/unit/test_quota_ext.py
@@ -173,6 +173,36 @@ class QuotaExtensionDbTestCase(QuotaExtensionTestCase):
                            expect_errors=True)
         self.assertEqual(400, res.status_int)
 
+    def test_update_quotas_with_negative_integer_returns_400(self):
+        tenant_id = 'tenant_id1'
+        env = {'quantum.context': context.Context('', tenant_id,
+                                                  is_admin=True)}
+        quotas = {'quota': {'network': -2}}
+        res = self.api.put(_get_path('quotas', id=tenant_id, fmt=self.fmt),
+                           self.serialize(quotas), extra_environ=env,
+                           expect_errors=True)
+        self.assertEqual(400, res.status_int)
+
+    def test_update_quotas_to_unlimited(self):
+        tenant_id = 'tenant_id1'
+        env = {'quantum.context': context.Context('', tenant_id,
+                                                  is_admin=True)}
+        quotas = {'quota': {'network': -1}}
+        res = self.api.put(_get_path('quotas', id=tenant_id, fmt=self.fmt),
+                           self.serialize(quotas), extra_environ=env,
+                           expect_errors=False)
+        self.assertEqual(200, res.status_int)
+
+    def test_update_quotas_exceeding_current_limit(self):
+        tenant_id = 'tenant_id1'
+        env = {'quantum.context': context.Context('', tenant_id,
+                                                  is_admin=True)}
+        quotas = {'quota': {'network': 120}}
+        res = self.api.put(_get_path('quotas', id=tenant_id, fmt=self.fmt),
+                           self.serialize(quotas), extra_environ=env,
+                           expect_errors=False)
+        self.assertEqual(200, res.status_int)
+
     def test_update_quotas_with_non_support_resource_returns_400(self):
         tenant_id = 'tenant_id1'
         env = {'quantum.context': context.Context('', tenant_id,
@@ -249,26 +279,12 @@ class QuotaExtensionDbTestCase(QuotaExtensionTestCase):
                                  tenant_id,
                                  network=4)
 
-    def test_quotas_limit_check_with_over_quota(self):
-        tenant_id = 'tenant_id1'
-        env = {'quantum.context': context.Context('', tenant_id,
-                                                  is_admin=True)}
-        quotas = {'quota': {'network': 5}}
-        res = self.api.put(_get_path('quotas', id=tenant_id,
-                                     fmt=self.fmt),
-                           self.serialize(quotas), extra_environ=env)
-        self.assertEqual(200, res.status_int)
-        with testtools.ExpectedException(exceptions.OverQuota):
-            quota.QUOTAS.limit_check(context.Context('', tenant_id),
-                                     tenant_id,
-                                     network=6)
-
     def test_quotas_limit_check_with_invalid_quota_value(self):
         tenant_id = 'tenant_id1'
         with testtools.ExpectedException(exceptions.InvalidQuotaValue):
             quota.QUOTAS.limit_check(context.Context('', tenant_id),
                                      tenant_id,
-                                     network=-1)
+                                     network=-2)
 
     def test_quotas_get_tenant_from_request_context(self):
         tenant_id = 'tenant_id1'
