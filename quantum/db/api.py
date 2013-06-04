725d636da5959aee1954a8e8d5ff383d2a8b7bb9
Gary Kotton <gkotton@redhat.com>
2013-06-04 15:14:39 +0000

    Expose most common QueuePoll parameters from SQLAlchemy
    Fixes bug 1160442
    
    The defualt parameters are used unless otherwise configured. This
    is applicable for:
     - pool_size
     - max_overflow
     - pool_timeout
    
    By increasing the pool_size and the max_overflow and lowering the timeout
    the issue reported in the bug is resolved.
    
    NOTE: the configuration values enable the user to make use of the default
    QueuePool tuning values.
    
    Change-Id: Ic258442531a2caf2db4f05c4ddd6fbfb0c180f17

--- quantum/db/api.py
+++ quantum/db/api.py
@@ -64,9 +64,17 @@ database_opts = [
                 default=False,
                 help=_("Enable the use of eventlet's db_pool for MySQL")),
     cfg.IntOpt('sqlalchemy_pool_size',
-               default=5,
+               default=None,
                help=_("Maximum number of SQL connections to keep open in a "
                       "QueuePool in SQLAlchemy")),
+    cfg.IntOpt('sqlalchemy_max_overflow',
+               default=None,
+               help=_("If set, use this value for max_overflow with "
+                      "sqlalchemy")),
+    cfg.IntOpt('sqlalchemy_pool_timeout',
+               default=None,
+               help=_("If set, use this value for pool_timeout with "
+                      "sqlalchemy")),
 ]
 
 cfg.CONF.register_opts(database_opts, "DATABASE")
@@ -124,8 +132,17 @@ def configure_db():
             'pool_recycle': 3600,
             'echo': False,
             'convert_unicode': True,
-            'pool_size': cfg.CONF.DATABASE.sqlalchemy_pool_size,
         }
+
+        if cfg.CONF.DATABASE.sqlalchemy_pool_size is not None:
+            pool_size = cfg.CONF.DATABASE.sqlalchemy_pool_size
+            engine_args['pool_size'] = pool_size
+        if cfg.CONF.DATABASE.sqlalchemy_max_overflow is not None:
+            max_overflow = cfg.CONF.DATABASE.sqlalchemy_max_overflow
+            engine_args['max_overflow'] = max_overflow
+        if cfg.CONF.DATABASE.sqlalchemy_pool_timeout is not None:
+            pool_timeout = cfg.CONF.DATABASE.sqlalchemy_pool_timeout
+            engine_args['pool_timeout'] = pool_timeout
 
         if 'mysql' in connection_dict.drivername:
             engine_args['listeners'] = [MySQLPingListener()]
