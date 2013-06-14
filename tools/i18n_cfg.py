5586fc3973367bc2f702441bf368a6fc2b785670
HenryVIII <gessau@cisco.com>
2013-06-13 20:03:20 -0400

    Remove unit tests that are no longer run.
    A long time ago, quantum/tests/unit became the home for all unit
    tests, but these ones in the Cisco plugin directory got left
    behind. They have suffered bit-rot and need to be removed.
    
    Also:
    - Move the fake Nexus driver to new home.
    - Filed new bugs to track the task of improving unit test coverage of
      Cisco plugin code.
    
    Fixes: bug #1174311
    Change-Id: I372e24aebbe1804e5b6ce62984bfd76b030a44b1

--- tools/i18n_cfg.py
+++ tools/i18n_cfg.py
@@ -92,7 +92,6 @@ msg_format_checkers = [
 ]
 
 
-file_black_list = ["./quantum/plugins/cisco/tests/unit",
-                   "./quantum/tests/unit",
+file_black_list = ["./quantum/tests/unit",
                    "./quantum/openstack",
                    "./quantum/plugins/bigswitch/tests"]
