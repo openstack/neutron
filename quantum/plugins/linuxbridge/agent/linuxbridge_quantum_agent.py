f03d91b850b1ef5976c110e5b14cdd0a9ef21e19
Tomasz Paszkowski <tpaszkowski@suse.com>
2013-06-26 08:40:46 -0400

    Add support for moving ip/addresss/default gateway
    Add support from moving ip/addresss/default gateway from physical interface
    which is going to be attached to bridge to bridge device itself.
    
    Fixes: bug #1192122
    
    Change-Id: I8a8bf5edc17a87dcb747626dd1d6817ac48cb20d

--- quantum/plugins/linuxbridge/agent/linuxbridge_quantum_agent.py
+++ quantum/plugins/linuxbridge/agent/linuxbridge_quantum_agent.py
@@ -140,7 +140,8 @@ class LinuxBridgeManager:
         """Create a vlan and bridge unless they already exist."""
         interface = self.ensure_vlan(physical_interface, vlan_id)
         bridge_name = self.get_bridge_name(network_id)
-        if self.ensure_bridge(bridge_name, interface):
+        ips, gateway = self.get_interface_details(interface)
+        if self.ensure_bridge(bridge_name, interface, ips, gateway):
             return interface
 
     def get_interface_details(self, interface):
