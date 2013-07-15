c2427c277368109b1ab747c3447e8c95f954733a
Sylvain Afchain <sylvain.afchain@enovance.com>
2013-07-15 10:42:12 +0200

    Add decorator helping to log method calls.
    Fixes bug 1201328
    
    Change-Id: I5c3a00c737903aa59cc6a2a248ee61aa53e54fbe

---
+++ neutron/common/log.py
@@ -0,0 +1,34 @@
+# Copyright (C) 2013 eNovance SAS <licensing@enovance.com>
+#
+# Author: Sylvain Afchain <sylvain.afchain@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""Log helper functions."""
+
+from neutron.openstack.common import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+def log(method):
+    """Decorator helping to log method calls."""
+    def wrapper(*args, **kwargs):
+        instance = args[0]
+        data = {"class_name": instance.__class__.__name__,
+                "method_name": method.__name__,
+                "args": args[1:], "kwargs": kwargs}
+        LOG.debug(_('%(class_name)s method %(method_name)s'
+                    'called with arguments %(args)s %(kwargs)s '), data)
+        return method(*args, **kwargs)
+    return wrapper
