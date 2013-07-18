a775ab3b61122993fbbecabc57d09f38abb03570
Luke Gorrie <luke@snabb.co>
2013-07-18 09:25:48 +0000

    ML2 Mechanism Driver for Tail-f Network Control System (NCS)
    Define a new ML2 Mechanism Driver that replicates Neutron network/port
    configuration changes to NCS: http://www.tail-f.com/network-control-system/
    
    Configuration is sent using a HTTP/JSON interface.
    
    Implements blueprint tailf-ncs
    
    Change-Id: I1f73fa3f2e4eec8e5a0f2865aec2d934e25c76d1

---
+++ neutron/tests/unit/ml2/test_mechanism_ncs.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2013 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron.plugins.ml2 import config as config
+from neutron.tests.unit import test_db_plugin as test_plugin
+
+PLUGIN_NAME = 'neutron.plugins.ml2.plugin.Ml2Plugin'
+
+
+class NCSTestCase(test_plugin.NeutronDbPluginV2TestCase):
+
+    def setUp(self):
+        # Enable the test mechanism driver to ensure that
+        # we can successfully call through to all mechanism
+        # driver apis.
+        config.cfg.CONF.set_override('mechanism_drivers',
+                                     ['logger', 'ncs'],
+                                     'ml2')
+        self.addCleanup(config.cfg.CONF.reset)
+        super(NCSTestCase, self).setUp(PLUGIN_NAME)
+        self.port_create_status = 'DOWN'
+
+
+class NCSMechanismTestBasicGet(test_plugin.TestBasicGet, NCSTestCase):
+    pass
+
+
+class NCSMechanismTestNetworksV2(test_plugin.TestNetworksV2, NCSTestCase):
+    pass
+
+
+class NCSMechanismTestPortsV2(test_plugin.TestPortsV2, NCSTestCase):
+    pass
