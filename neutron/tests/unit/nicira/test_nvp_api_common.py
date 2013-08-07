9aeb5c2b57c20b751b0197954f334bc15948cb6e
Aaron Rosen <arosen@nicira.com>
2013-08-07 11:34:46 -0700

    Correct NVP license header files
    Note: Pointed out by Akihiro but pushed by me as I was the one who
    push these files originally so I have to change the license.
    
    Fixes bug: 1209359
    
    Change-Id: I57d0fc588983e1934ad9321d54e642b72bdbdc9c

--- neutron/tests/unit/nicira/test_nvp_api_common.py
+++ neutron/tests/unit/nicira/test_nvp_api_common.py
@@ -1,9 +1,19 @@
-# Copyright (C) 2009-2011 Nicira Networks, Inc. All Rights Reserved.
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2011 Nicira, Inc.
+# All Rights Reserved
 #
-# This software is provided only under the terms and conditions of a written
-# license agreement with Nicira. If no such agreement applies to you, you are
-# not authorized to use this software. Contact Nicira to obtain an appropriate
-# license: www.nicira.com.
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
 
 # System
 import httplib
