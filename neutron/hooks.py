e1165ce1180bba1a11098cfacfe3c722a084dbcf
alexpilotti <ap@pilotti.it>
2013-08-15 19:49:31 +0300

    Fixes Windows setup dependency bug
    Fixes bug: #1212385
    
    The neutron project includes a setup hook that allows to dinamically
    include setup dependencies.
    
    Due to recent changes in the pbr project, the hook raises an exception
    on Windows, which ends the setup process.
    
    This fix solves the issue by adapting the hook to the recent pbr changes.
    
    Change-Id: I4b00d74d23a7167a10ba86458943ff16ca51b0db

--- neutron/hooks.py
+++ neutron/hooks.py
@@ -21,10 +21,11 @@ import sys
 def setup_hook(config):
     """Filter config parsed from a setup.cfg to inject our defaults."""
     metadata = config['metadata']
+    requires = metadata.get('requires_dist', '').split('\n')
     if sys.platform == 'win32':
-        requires = metadata.get('requires_dist', list()).split('\n')
         requires.append('pywin32')
         requires.append('wmi')
-        requires.remove('pyudev')
-        metadata['requires_dist'] = "\n".join(requires)
+    elif sys.platform.startswith('linux'):
+        requires.append('pyudev')
+    metadata['requires_dist'] = "\n".join(requires)
     config['metadata'] = metadata
