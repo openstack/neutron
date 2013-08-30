0b703922e380438452b7fddf8ddff9392cb0d1cd
Francois Eleouet <f.eleouet@gmail.com>
2013-08-30 22:30:04 +0200

    Add missing match in OVS agent tunnel mac leaning
    Learned flows introduced in commit a369f9e39691c01a4e4f7f8668cb37fc17ba03b3
    to learn remote mac addresses on tunnels currently only match local VLAN,
    but not destination mac address, as a consequence, connectivity may be
    erratic as soon as more than two agents are used
    
    Change-Id: I467e47d0dd2c1e5aca12a4e240e348116d530d4f
    Closes-Bug: #1219074

--- neutron/plugins/openvswitch/agent/ovs_neutron_agent.py
+++ neutron/plugins/openvswitch/agent/ovs_neutron_agent.py
@@ -610,6 +610,7 @@ class OVSNeutronAgent(sg_rpc.SecurityGroupAgentRpcCallbackMixin):
                         "priority=1,"
                         "hard_timeout=300,"
                         "NXM_OF_VLAN_TCI[0..11],"
+                        "NXM_OF_ETH_DST[]=NXM_OF_ETH_SRC[],"
                         "load:0->NXM_OF_VLAN_TCI[],"
                         "load:NXM_NX_TUN_ID[]->NXM_NX_TUN_ID[],"
                         "output:NXM_OF_IN_PORT[]" %
