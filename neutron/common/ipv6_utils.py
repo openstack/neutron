7e47a3e43692930b07c90b05a777e66333916bf3
Dazhao <dzyu@cn.ibm.com>
2013-11-13 18:24:32 +0800

    Calculate stateless IPv6 address
    In order to support stateless IPv6, neutron should calculate IPv6 address base
    on IPv6 prefix and MAC address via EUI-64 specification.  This change adds
    common library to calculate IPv6 address.
    
    Partially-implements bp ipv6-two-attributes
    Co-Authored-By: Xu Han Peng <xuhanp@cn.ibm.com>
    
    Change-Id: I68ccbc42388ec760d6fead242e080822ca753913

---
+++ neutron/common/ipv6_utils.py
@@ -0,0 +1,39 @@
+# Copyright 2013 IBM Corp.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+IPv6-related utilities and helper functions.
+"""
+
+import netaddr
+
+
+def get_ipv6_addr_by_EUI64(prefix, mac):
+    # Check if the prefix is IPv4 address
+    isIPv4 = netaddr.valid_ipv4(prefix)
+    if isIPv4:
+        msg = _("Unable to generate IP address by EUI64 for IPv4 prefix")
+        raise TypeError(msg)
+    try:
+        eui64 = int(netaddr.EUI(mac).eui64())
+        prefix = netaddr.IPNetwork(prefix)
+        return netaddr.IPAddress(prefix.first + eui64 ^ (1 << 57))
+    except netaddr.AddrFormatError:
+        raise TypeError(_('Bad prefix or mac format for generating IPv6 '
+                          'address by EUI-64: %(prefix)s, %(mac)s:')
+                        % {'prefix': prefix, 'mac': mac})
+    except TypeError:
+        raise TypeError(_('Bad prefix type for generate IPv6 address by '
+                          'EUI-64: %s') % prefix)
