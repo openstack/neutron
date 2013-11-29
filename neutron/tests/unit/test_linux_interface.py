5c43242a13f21aaeaa9499fd9f07e36d23fc5e17
Hui HX Xiang <xianghui@cn.ibm.com>
2013-11-28 20:38:10 -0800

    Sync dhcp_agent.ini with the codes
    Add an option item into dhcp_agent.ini to sync with the codes.
    * Add ovs_integration_bridge in /etc/dhcp_agent.ini
    * Add corresponding unit test
    
    Change-Id: Ia78e7ca6c1fcbeca3e962f870a393e44183d719c
    closes-Bug: #1256197

--- neutron/tests/unit/test_linux_interface.py
+++ neutron/tests/unit/test_linux_interface.py
@@ -110,6 +110,31 @@ class TestOVSInterfaceDriver(TestBase):
     def test_plug_alt_bridge(self):
         self._test_plug(bridge='br-foo')
 
+    def test_plug_configured_bridge(self):
+        br = 'br-v'
+        self.conf.set_override('ovs_use_veth', False)
+        self.conf.set_override('ovs_integration_bridge', br)
+        self.assertEqual(self.conf.ovs_integration_bridge, br)
+
+        def device_exists(dev, root_helper=None, namespace=None):
+            return dev == br
+
+        ovs = interface.OVSInterfaceDriver(self.conf)
+        with mock.patch.object(ovs, '_ovs_add_port') as add_port:
+            self.device_exists.side_effect = device_exists
+            ovs.plug('01234567-1234-1234-99',
+                     'port-1234',
+                     'tap0',
+                     'aa:bb:cc:dd:ee:ff',
+                     bridge=None,
+                     namespace=None)
+
+        add_port.assert_called_once_with('br-v',
+                                         'tap0',
+                                         'port-1234',
+                                         'aa:bb:cc:dd:ee:ff',
+                                         internal=True)
+
     def _test_plug(self, additional_expectation=[], bridge=None,
                    namespace=None):
 
