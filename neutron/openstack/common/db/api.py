9a20c0819074f17e3c607522aee4020b811e00e3
Julia Varlamova <jvarlamova@mirantis.com>
2014-02-11 18:04:01 +0400

    Sync latest oslo.db code into neutron
    Changes that were ported from oslo:
    
    b4f72b2 Don't raise MySQL 2013 'Lost connection' errors
    271adfb Format sql in db.sqlalchemy.session docstring
    0334cb3 Handle exception messages with six.text_type
    eff69ce Drop dependency on log from oslo db code
    7a11a04 Automatic retry db.api query if db connection lost
    11f2add Clean up docstring in db.sqlalchemy.session
    1b5147f Only enable MySQL TRADITIONAL mode if we're running against MySQL
    39e1c5c Move db tests base.py to common code
    986dafd Fix parsing of UC errors in sqlite 3.7.16+/3.8.2+
    9a203e6 Use dialect rather than a particular DB API driver
    1779029 Move helper DB functions to db.sqlalchemy.utils
    bcf6d5e Small edits on help strings
    ae01e9a Transition from migrate to alembic
    70ebb19 Fix mocking of utcnow() for model datetime cols
    7aa94df Add a db check for CHARSET=utf8
    aff0171 Remove "vim: tabstop=4 shiftwidth=4 softtabstop=4" from headers
    fa0f36f Fix database connection string is secret
    8575d87 Removed copyright from empty files
    d08d27f Fix the obsolete exception message
    8b2b0b7 Use hacking import_exceptions for gettextutils._
    9bc593e Add docstring for exception handlers of session
    855644a Removal of _REPOSITORY global variable.
    ea6caf9 Remove string.lowercase usage
    a33989e Remove eventlet tpool from common db.api
    e40903b Database hook enabling traditional mode at MySQL
    f2115a0 Replace xrange in for loop with range
    c802fa6 SQLAlchemy error patterns improved
    1c1f199 Remove unused import
    6d0a6c3 Correct invalid docstrings
    135dd00 Remove start index 0 in range()
    28f8fd5 Make _extra_keys a property of ModelBase
    45658e2 Fix violations of H302:import only modules
    bb4d7a2 Enables db2 server disconnects to be handled pessimistically
    915f8ab db.sqlalchemy.session add [sql].idle_timeout
    e6494c2 Use six.iteritems to make dict work on Python2/3
    48cfb7b Drop dependency on processutils from oslo db code
    4c47d3e Fix locking in migration tests
    c2ee282 Incorporating MIT licensed code
    c5a1088 Typos fix in db and periodic_task module
    fb0e86a Use six.moves.configparser instead of ConfigParser
    1dd4971 fix typo in db session docstring
    8a01dd8 The ability to run tests at various backend
    0fe4e28 Use log.warning() instead of log.warn() in oslo.db
    12bcdb7 Remove vim header
    4c22556 Use py3kcompat urlutils functions instead of urlparse
    ca7a2ab Don't use deprecated module commands
    6603e8f Remove sqlalchemy-migrate 0.7.3 patching
    274c7e2 Drop dependency on lockutils from oslo db code
    97d8cf4 Remove lazy loading of database backend
    2251cb5 Do not name variables as builtins
    3acd57c Add db2 communication error code when check the db connection
    c2dcf6e Add [sql].connection as deprecated opt for db
    001729d Modify SQLA session due to dispose of eventlet
    c2dcf6e Add [sql].connection as deprecated opt for db
    001729d Modify SQLA session due to dispose of eventlet
    4de827a Clean up db.sqla.Models.extra_keys interface
    347f29e Use functools.wrap() instead of custom implementation
    771d843 Move base migration test classes to common code
    9721129 exception: remove
    56ff3b3 Use single meta when change column type
    3f2f70e Helper function to sanitize db url credentials
    df3f2ba BaseException.message is deprecated since Python 2.6
    c76be5b Add function drop_unique_constraint()
    d4d8126 Change sqlalchemy/utils.py mode back to 644
    cf41936 Move sqlalchemy migration from Nova
    5758360 Raise ValueError if sort_dir is unknown
    31c1995 python3: Add python3 compatibility support
    3972c3f Migrate sqlalchemy utils from Nova
    1a2df89 Enable H302 hacking check
    3f503fa Add a monkey-patching util for sqlalchemy-migrate
    7ba5f4b Don't use mixture of cfg.Opt() deprecated args
    
    Change-Id: Ifddb91dc2c6b80668a07267f92f13074b974776b

--- neutron/openstack/common/db/api.py
+++ neutron/openstack/common/db/api.py
@@ -1,5 +1,3 @@
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
-
 # Copyright (c) 2013 Rackspace Hosting
 # All Rights Reserved.
 #
@@ -21,27 +19,21 @@ Supported configuration options:
 
 The following two parameters are in the 'database' group:
 `backend`: DB backend name or full module path to DB backend module.
-`use_tpool`: Enable thread pooling of DB API calls.
 
 A DB backend module should implement a method named 'get_backend' which
 takes no arguments.  The method can return any object that implements DB
 API methods.
-
-*NOTE*: There are bugs in eventlet when using tpool combined with
-threading locks. The python logging module happens to use such locks.  To
-work around this issue, be sure to specify thread=False with
-eventlet.monkey_patch().
-
-A bug for eventlet has been filed here:
-
-https://bitbucket.org/eventlet/eventlet/issue/137/
 """
+
 import functools
+import logging
+import time
 
 from oslo.config import cfg
 
+from neutron.openstack.common.db import exception
+from neutron.openstack.common.gettextutils import _  # noqa
 from neutron.openstack.common import importutils
-from neutron.openstack.common import lockutils
 
 
 db_opts = [
@@ -50,57 +42,95 @@ db_opts = [
                deprecated_name='db_backend',
                deprecated_group='DEFAULT',
                help='The backend to use for db'),
-    cfg.BoolOpt('use_tpool',
+    cfg.BoolOpt('use_db_reconnect',
                 default=False,
-                deprecated_name='dbapi_use_tpool',
-                deprecated_group='DEFAULT',
-                help='Enable the experimental use of thread pooling for '
-                     'all DB API calls')
+                help='Enable the experimental use of database reconnect '
+                     'on connection lost'),
+    cfg.IntOpt('db_retry_interval',
+               default=1,
+               help='seconds between db connection retries'),
+    cfg.BoolOpt('db_inc_retry_interval',
+                default=True,
+                help='Whether to increase interval between db connection '
+                     'retries, up to db_max_retry_interval'),
+    cfg.IntOpt('db_max_retry_interval',
+               default=10,
+               help='max seconds between db connection retries, if '
+                    'db_inc_retry_interval is enabled'),
+    cfg.IntOpt('db_max_retries',
+               default=20,
+               help='maximum db connection retries before error is raised. '
+                    '(setting -1 implies an infinite retry count)'),
 ]
 
 CONF = cfg.CONF
 CONF.register_opts(db_opts, 'database')
+
+LOG = logging.getLogger(__name__)
+
+
+def safe_for_db_retry(f):
+    """Enable db-retry for decorated function, if config option enabled."""
+    f.__dict__['enable_retry'] = True
+    return f
+
+
+def _wrap_db_retry(f):
+    """Retry db.api methods, if DBConnectionError() raised
+
+    Retry decorated db.api methods. If we enabled `use_db_reconnect`
+    in config, this decorator will be applied to all db.api functions,
+    marked with @safe_for_db_retry decorator.
+    Decorator catchs DBConnectionError() and retries function in a
+    loop until it succeeds, or until maximum retries count will be reached.
+    """
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        next_interval = CONF.database.db_retry_interval
+        remaining = CONF.database.db_max_retries
+
+        while True:
+            try:
+                return f(*args, **kwargs)
+            except exception.DBConnectionError as e:
+                if remaining == 0:
+                    LOG.exception(_('DB exceeded retry limit.'))
+                    raise exception.DBError(e)
+                if remaining != -1:
+                    remaining -= 1
+                    LOG.exception(_('DB connection error.'))
+                # NOTE(vsergeyev): We are using patched time module, so this
+                #                  effectively yields the execution context to
+                #                  another green thread.
+                time.sleep(next_interval)
+                if CONF.database.db_inc_retry_interval:
+                    next_interval = min(
+                        next_interval * 2,
+                        CONF.database.db_max_retry_interval
+                    )
+    return wrapper
 
 
 class DBAPI(object):
     def __init__(self, backend_mapping=None):
         if backend_mapping is None:
             backend_mapping = {}
-        self.__backend = None
-        self.__backend_mapping = backend_mapping
-
-    @lockutils.synchronized('dbapi_backend', 'neutron-')
-    def __get_backend(self):
-        """Get the actual backend.  May be a module or an instance of
-        a class.  Doesn't matter to us.  We do this synchronized as it's
-        possible multiple greenthreads started very quickly trying to do
-        DB calls and eventlet can switch threads before self.__backend gets
-        assigned.
-        """
-        if self.__backend:
-            # Another thread assigned it
-            return self.__backend
         backend_name = CONF.database.backend
-        self.__use_tpool = CONF.database.use_tpool
-        if self.__use_tpool:
-            from eventlet import tpool
-            self.__tpool = tpool
         # Import the untranslated name if we don't have a
         # mapping.
-        backend_path = self.__backend_mapping.get(backend_name,
-                                                  backend_name)
+        backend_path = backend_mapping.get(backend_name, backend_name)
         backend_mod = importutils.import_module(backend_path)
         self.__backend = backend_mod.get_backend()
-        return self.__backend
 
     def __getattr__(self, key):
-        backend = self.__backend or self.__get_backend()
-        attr = getattr(backend, key)
-        if not self.__use_tpool or not hasattr(attr, '__call__'):
+        attr = getattr(self.__backend, key)
+
+        if not hasattr(attr, '__call__'):
             return attr
+        # NOTE(vsergeyev): If `use_db_reconnect` option is set to True, retry
+        #                  DB API methods, decorated with @safe_for_db_retry
+        #                  on disconnect.
+        if CONF.database.use_db_reconnect and hasattr(attr, 'enable_retry'):
+            attr = _wrap_db_retry(attr)
 
-        def tpool_wrapper(*args, **kwargs):
-            return self.__tpool.execute(attr, *args, **kwargs)
-
-        functools.update_wrapper(tpool_wrapper, attr)
-        return tpool_wrapper
+        return attr
