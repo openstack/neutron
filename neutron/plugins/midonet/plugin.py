0045eee48f9d646d8ae99500b910d25edcb80a6d
Joe Mills <joe@midokura.com>
2014-02-12 10:19:27 +0000

    Add the agent extension alias to Midonet plugin.
    * Add the agent extension alias to the Midonet plugin.
    * This alias is needed to support the listing of network agents.
    
    Signed-off-by: Joe Mills <joe@midokura.com>
    
    Fixes MN-1210

--- neutron/plugins/midonet/plugin.py
+++ neutron/plugins/midonet/plugin.py
@@ -236,7 +236,7 @@ class MidonetPluginV2(db_base_plugin_v2.NeutronDbPluginV2,
                       securitygroups_db.SecurityGroupDbMixin):
 
     supported_extension_aliases = ['ext_gw_mode', 'external-net', 'router',
-                                   'security-group']
+                                   'agent', 'security-group']
 
     __native_bulk_support = True
 
