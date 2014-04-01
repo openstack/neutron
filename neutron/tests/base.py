f59e5fff820c1cf6900100d058622b8b1cbb03ca
Joe Gordon <joe.gordon0@gmail.com>
2014-04-01 15:06:36 -0700

    Use a temp dir for CONF.state_path
    external_pids will defaults to $state_path(/var/lib/neutron)/external/pids
    which isn't accessible when running unit tests so use a tempdir for
    CONF.state_path. This bug arose when oslo.config 1.3.0 came out which
    fixed bug #1282250 (I4324bda6f3de3cdf05f8eb3cf16052c04018dec8) and
    caused this issue.
    
    Change-Id: I253c5ea85c51eff94399da0b9462d10a9c509a0f
    Closes-Bug: #1301027

--- neutron/tests/base.py
+++ neutron/tests/base.py
@@ -105,6 +105,9 @@ class BaseTestCase(testtools.TestCase):
         self.useFixture(fixtures.NestedTempfile())
         self.useFixture(fixtures.TempHomeDir())
 
+        self.temp_dir = self.useFixture(fixtures.TempDir()).path
+        cfg.CONF.set_override('state_path', self.temp_dir)
+
         self.addCleanup(mock.patch.stopall)
         self.addCleanup(CONF.reset)
 
