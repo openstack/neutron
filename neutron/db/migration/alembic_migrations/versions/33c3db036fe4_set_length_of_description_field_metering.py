c6a8f25f49f84142ad92cf03583047ee3cc21bc3
Ann Kamyshnikova <akamyshnikova@mirantis.com>
2014-04-16 15:17:02 +0400

    Fix migration that breaks Grenade jobs
    Migration 33c3db036fe4_set_length_of_description_field_metering
    needs to be changed as it is a reason of Grenade job's fail.
    
    Closes-bug: #1307344
    
    Change-Id: Ia57d9a60847e4a34977e40fb8b09181bdbd9c39b

--- neutron/db/migration/alembic_migrations/versions/33c3db036fe4_set_length_of_description_field_metering.py
+++ neutron/db/migration/alembic_migrations/versions/33c3db036fe4_set_length_of_description_field_metering.py
@@ -41,6 +41,12 @@ def upgrade(active_plugins=None, options=None):
     if not migration.should_run(active_plugins, migration_for_plugins):
         return
 
+    op.execute("CREATE TABLE IF NOT EXISTS meteringlabels( "
+               "tenant_id VARCHAR(255) NULL, "
+               "id VARCHAR(36) PRIMARY KEY NOT NULL, "
+               "name VARCHAR(255) NULL, "
+               "description VARCHAR(255) NULL)")
+
     op.alter_column('meteringlabels', 'description', type_=sa.String(1024),
                     existing_nullable=True)
 
