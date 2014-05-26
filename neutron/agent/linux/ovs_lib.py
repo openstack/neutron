be340d1bab015c47650687f97393c6c9015fb537
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2014-05-26 22:45:25 +0200

    fix openvswitch requirement check
    With VXLAN enabled on openvswitch, neutron checks
    module version of the openvswitch kernel module.
    If the pattern to extract the version matches twice
    (eg. for path and version) the agent dies.
    
    This patch ensures, that only the version is checked
    against the pattern.
    
    Change-Id: I879624f6b0936cab59e02958ae3a89950df773bb
    Closes-Bug: #1322139

--- neutron/agent/linux/ovs_lib.py
+++ neutron/agent/linux/ovs_lib.py
@@ -476,7 +476,7 @@ def get_installed_ovs_usr_version(root_helper):
 
 
 def get_installed_ovs_klm_version():
-    args = ["modinfo", "openvswitch"]
+    args = ["modinfo", "-F vermagic", "openvswitch"]
     try:
         cmd = utils.execute(args)
         for line in cmd.split('\n'):
