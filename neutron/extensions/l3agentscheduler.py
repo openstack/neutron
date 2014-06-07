18dfd363c320ba67567fd55a2d47ffa6ea6c7bd2
Maruti <maruti.kamat@hp.com>
2014-06-07 05:42:45 -0700

    Fixed audit notifications for l3-agent-router ops
    l3-agent-router-add and l3-agent-router-remove do not
    generate audit notifications which are used for security
    compliance.
    CRUD operations of core network resources are
    handled by neutron/api/v2/base.py. In base.py, each of
    create(), update(), delete() methods makes calls to
    oslo.messaging.Notifier.info() to generate these notifications.
    In the proposed fix, it is fixed in a similar fashion in
    extensions/l3agentscheduler.py inside create() and delete()
    methods by introducing info() method calls inside them.
    
    Change-Id: I1354e51e4af24eda128c042926765683018b320b
    Closes-Bug: 1317000

--- neutron/extensions/l3agentscheduler.py
+++ neutron/extensions/l3agentscheduler.py
@@ -22,6 +22,7 @@ from neutron.api.v2 import base
 from neutron.api.v2 import resource
 from neutron.common import constants
 from neutron.common import exceptions
+from neutron.common import rpc as n_rpc
 from neutron.extensions import agent
 from neutron import manager
 from neutron.openstack.common import log as logging
@@ -63,18 +64,23 @@ class RouterSchedulerController(wsgi.Controller):
         policy.enforce(request.context,
                        "create_%s" % L3_ROUTER,
                        {})
-        return plugin.add_router_to_l3_agent(
-            request.context,
-            kwargs['agent_id'],
-            body['router_id'])
+        agent_id = kwargs['agent_id']
+        router_id = body['router_id']
+        result = plugin.add_router_to_l3_agent(request.context, agent_id,
+                                               router_id)
+        notify(request.context, 'l3_agent.router.add', router_id, agent_id)
+        return result
 
     def delete(self, request, id, **kwargs):
         plugin = self.get_plugin()
         policy.enforce(request.context,
                        "delete_%s" % L3_ROUTER,
                        {})
-        return plugin.remove_router_from_l3_agent(
-            request.context, kwargs['agent_id'], id)
+        agent_id = kwargs['agent_id']
+        result = plugin.remove_router_from_l3_agent(request.context, agent_id,
+                                                    id)
+        notify(request.context, 'l3_agent.router.remove', id, agent_id)
+        return result
 
 
 class L3AgentsHostingRouterController(wsgi.Controller):
@@ -192,3 +198,9 @@ class L3AgentSchedulerPluginBase(object):
     @abc.abstractmethod
     def list_l3_agents_hosting_router(self, context, router_id):
         pass
+
+
+def notify(context, action, router_id, agent_id):
+    info = {'id': agent_id, 'router_id': router_id}
+    notifier = n_rpc.get_notifier('router')
+    notifier.info(context, action, {'agent': info})
