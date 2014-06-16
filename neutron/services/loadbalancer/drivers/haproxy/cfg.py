f27e5244adcac9a12c04605fbf5c49a086d5fce5
Vishal Agarwal <vishala@hp.com>
2014-06-16 21:31:15 +0530

    Fix pool statistics for LBaaS Haproxy driver
    Total connections was mapped to none of the counters
    of haproxy. Active connections was mapped to active request
    counter(qcur)) of haproxy which in low load situations
    always remains zero.
    
    Patch maps connections API counters to haproxy session counters
    and fixes unit test accordingly.
    
    Change-Id: I0ef4f76a75340232eb11a9b95453769ecd89d13a
    Closes-Bug: #1319361

--- neutron/services/loadbalancer/drivers/haproxy/cfg.py
+++ neutron/services/loadbalancer/drivers/haproxy/cfg.py
@@ -37,10 +37,11 @@ BALANCE_MAP = {
 }
 
 STATS_MAP = {
-    constants.STATS_ACTIVE_CONNECTIONS: 'qcur',
-    constants.STATS_MAX_CONNECTIONS: 'qmax',
+    constants.STATS_ACTIVE_CONNECTIONS: 'scur',
+    constants.STATS_MAX_CONNECTIONS: 'smax',
     constants.STATS_CURRENT_SESSIONS: 'scur',
     constants.STATS_MAX_SESSIONS: 'smax',
+    constants.STATS_TOTAL_CONNECTIONS: 'stot',
     constants.STATS_TOTAL_SESSIONS: 'stot',
     constants.STATS_IN_BYTES: 'bin',
     constants.STATS_OUT_BYTES: 'bout',
