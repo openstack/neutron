09856a0cf63052045b3d9f4c8d2a5aa920c7c935
Jakub Libosvar <libosvar@redhat.com>
2014-06-17 17:20:25 +0200

    Notify systemd when starting Neutron server
    systemd is notified only when Neutron is started with multiple api
    workers. This patch adds systemd.notify_once() call when starting
    Neutron server with api_workers=0
    
    Change-Id: I95581dc5dffde6168c68d9f04cf8bdfcdb773ba8
    Closes-Bug: #1330532

--- neutron/wsgi.py
+++ neutron/wsgi.py
@@ -45,6 +45,7 @@ from neutron.openstack.common import gettextutils
 from neutron.openstack.common import jsonutils
 from neutron.openstack.common import log as logging
 from neutron.openstack.common import service as common_service
+from neutron.openstack.common import systemd
 
 socket_opts = [
     cfg.IntOpt('backlog',
@@ -210,6 +211,7 @@ class Server(object):
             # For the case where only one process is required.
             self._server = self.pool.spawn(self._run, application,
                                            self._socket)
+            systemd.notify_once()
         else:
             # Minimize the cost of checking for child exit by extending the
             # wait interval past the default of 0.01s.
