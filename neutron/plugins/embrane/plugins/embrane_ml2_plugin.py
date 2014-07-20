6186e0cabe5ba854cddd68351e314933f826ee33
Ignacio Scopetta <ignacio@embrane.com>
2014-07-19 23:37:22 -0700

    Remove ovs dependency in embrane plugin
    This patch changes the dependency of the embrane plugin from ovs to ml2.
    Existing users of the old entry point will be upgraded on a case-by-case
    base as needed, therefore no generic upgrade procedure is required.
    
    Change-Id: Id192fc32bd6ba7a8926eff1190070b8df5327129
    Closes-Bug: #1343024

--- neutron/plugins/embrane/plugins/embrane_ml2_plugin.py
+++ neutron/plugins/embrane/plugins/embrane_ml2_plugin.py
@@ -1,4 +1,4 @@
-# Copyright 2013 Embrane, Inc.
+# Copyright 2014 Embrane, Inc.
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -14,23 +14,31 @@
 #    under the License.
 #
 # @author: Ivar Lazzaro, Embrane, Inc.
+# @author: Ignacio Scopetta, Embrane, Inc.
 
+from neutron.db import extraroute_db
+from neutron.db import l3_dvr_db
+from neutron.db import l3_gwmode_db
 from neutron.plugins.embrane import base_plugin as base
-from neutron.plugins.embrane.l2base.openvswitch import openvswitch_support
-from neutron.plugins.openvswitch import ovs_neutron_plugin as l2
+from neutron.plugins.embrane.l2base.ml2 import ml2_support
+from neutron.plugins.ml2 import plugin as l2
 
 
-class EmbraneOvsPlugin(base.EmbranePlugin, l2.OVSNeutronPluginV2):
-    '''EmbraneOvsPlugin.
+class EmbraneMl2Plugin(base.EmbranePlugin, l2.Ml2Plugin,
+                       l3_dvr_db.L3_NAT_with_dvr_db_mixin,
+                       l3_gwmode_db.L3_NAT_db_mixin,
+                       extraroute_db.ExtraRoute_db_mixin):
+    '''EmbraneMl2Plugin.
 
-    This plugin uses OpenVSwitch specific L2 plugin for providing L2 networks
+    This plugin uses Modular Layer 2 plugin for providing L2 networks
     and the base EmbranePlugin for L3.
 
     '''
-    _plugin_support = openvswitch_support.OpenvswitchSupport()
+    _plugin_support = ml2_support.Ml2Support()
 
     def __init__(self):
         '''First run plugin specific initialization, then Embrane's.'''
-        self._supported_extension_aliases.remove("l3_agent_scheduler")
-        l2.OVSNeutronPluginV2.__init__(self)
+        self._supported_extension_aliases.extend(["router", "extraroute",
+                                                  "ext-gw-mode"])
+        l2.Ml2Plugin.__init__(self)
         self._run_embrane_config()
