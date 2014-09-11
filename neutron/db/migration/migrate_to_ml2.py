3116ffafdca72e9eef70f4eaf90626982ae51d4c
Bernhard M. Wiedemann <bwiedemann@suse.de>
2014-09-11 11:09:21 +0200

    use TRUE in SQL for boolean var
    The "allocated" variable was defined to be a boolean type
    and in postgresql this type is enforced,
    while in mysql this just maps to tinyint and accepts both numbers and bools
    
    Closes-Bug: #1368251
    Change-Id: If324c8b83e490e150085d044ac61360b647522ac
    (cherry picked from commit b030300b1687b853a95b6bd2e706045f549d1e50)

--- neutron/db/migration/migrate_to_ml2.py
+++ neutron/db/migration/migrate_to_ml2.py
@@ -170,7 +170,7 @@ class BaseMigrateToMl2_Icehouse(object):
           INSERT INTO ml2_vlan_allocations
             SELECT physical_network, vlan_id, allocated
               FROM %(source_table)s
-              WHERE allocated = 1
+              WHERE allocated = TRUE
         """) % {'source_table': self.vlan_allocation_table_name})
 
     def get_port_segment_map(self, engine):
@@ -389,7 +389,7 @@ class MigrateOpenvswitchToMl2_Icehouse(BaseMigrateToMl2_Icehouse):
               INSERT INTO ml2_gre_allocations
                 SELECT tunnel_id as gre_id, allocated
                   FROM ovs_tunnel_allocations
-                  WHERE allocated = 1
+                  WHERE allocated = TRUE
             """)
             engine.execute("""
               INSERT INTO ml2_gre_endpoints
@@ -403,7 +403,7 @@ class MigrateOpenvswitchToMl2_Icehouse(BaseMigrateToMl2_Icehouse):
               INSERT INTO ml2_vxlan_allocations
                 SELECT tunnel_id as vxlan_vni, allocated
                   FROM ovs_tunnel_allocations
-                  WHERE allocated = 1
+                  WHERE allocated = TRUE
             """)
             engine.execute(sa.text("""
               INSERT INTO ml2_vxlan_endpoints
