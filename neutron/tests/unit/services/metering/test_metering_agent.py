openstack/neutron

commit bdcc5a46d7776f50d5ae372798a913349629d2f6

Drop RpcProxy usage from metering_agent
Drop one more use of the RpcProxy compatibility class in favor of
direct use of oslo.messaging APIs.

Part of blueprint drop-rpc-compat.

Change-Id: I3c68aa246d88447f793aa9890f10f5043a41338d

neutron/services/metering/agents/metering_agent.py
@@ -19,6 +19,7 @@ import eventlet
 eventlet.monkey_patch()
 
 from oslo.config import cfg
+from oslo import messaging
 
 from neutron.agent.common import config
 from neutron.agent import rpc as agent_rpc
@@ -40,21 +41,17 @@ from neutron import service as neutron_service
 LOG = logging.getLogger(__name__)
 
 
-class MeteringPluginRpc(n_rpc.RpcProxy):
-
-    BASE_RPC_API_VERSION = '1.0'
+class MeteringPluginRpc(object):
 
     def __init__(self, host):
-        super(MeteringPluginRpc,
-              self).__init__(topic=topics.METERING_AGENT,
-                             default_version=self.BASE_RPC_API_VERSION)
+        target = messaging.Target(topic=topics.METERING_PLUGIN, version='1.0')
+        self.client = n_rpc.get_client(target)
 
     def _get_sync_data_metering(self, context):
         try:
-            return self.call(context,
-                             self.make_msg('get_sync_data_metering',
-                                           host=self.host),
-                             topic=topics.METERING_PLUGIN)
+            cctxt = self.client.prepare()
+            return cctxt.call(context, 'get_sync_data_metering',
+                              host=self.host)
         except Exception:
             LOG.exception(_("Failed synchronizing routers"))
 

commit 688b3794d2746d1c313119458d8548c1ef777aa4

MeteringPluginRpc: Fix crash in periodic_task
Restore __init__ chain which has been broken by
commit bdcc5a46d7776f50d5ae372798a913349629d2f6 .

Closes-Bug: #1396893
Change-Id: Ia1f0357b6726ac79a22ed1666eecebac33689aa1

neutron/services/metering/agents/metering_agent.py
@@ -45,6 +45,11 @@ LOG = logging.getLogger(__name__)
 class MeteringPluginRpc(object):
 
     def __init__(self, host):
+        # NOTE(yamamoto): super.__init__() call here is not only for
+        # aesthetics.  Because of multiple inheritances in MeteringAgent,
+        # it's actually necessary to initialize parent classes of
+        # manager.Manager correctly.
+        super(MeteringPluginRpc, self).__init__()
         target = messaging.Target(topic=topics.METERING_PLUGIN, version='1.0')
         self.client = n_rpc.get_client(target)
 

neutron/tests/unit/services/metering/test_metering_agent.py
@@ -158,3 +158,9 @@ class TestMeteringDriver(base.BaseTestCase):
                                              {'driver': self.noop_driver,
                                               'func':
                                               'add_metering_label'})
+
+    def test_init_chain(self):
+        with mock.patch('neutron.openstack.common.'
+                        'periodic_task.PeriodicTasks.__init__') as init:
+            metering_agent.MeteringAgent('my agent', cfg.CONF)
+        init.assert_called_once_with()