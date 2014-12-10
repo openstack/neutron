d264666bba0d633f2c6be423bf9f7e08db4ff289
Paul Michali <pcm@cisco.com>
2014-12-10 18:50:56 -0500

    L3 Agent restructure - observer hierarchy
    This commit creates the basic observer hierarchy for advanced
    services, as part of a multi-step refactoring effort of the L3 agent.
    
    The change set has these modifications:
    
    - Device drivers for VPN and FW services are loaded.
    - AdvancedService child instances are created for VPN and FW.
    - L3EventObservers is created by the L3 agent and the VPN and FW
      service objects are registered for notifications of events.
    - VPN device driver event handlers moved to VPN service instance.
    - VPN device driver callbacks to VPN agent, moved to VPN service.
      to service from the VPN device driver.
    - Test cases updated and moved related to these changes.
    - UT updated to test new methods and refactoring changes.
    
    Future commits will massage the event notification points in the
    L3 agent, and implement handlers.
    
    Please keep these things in mind, when reviewing:
    - The goal is to refactor the code and not 'improve/change'
      functionality (other than broken tests).
    - This is one step of a series of steps to move to the 'final'
      restructuring.
    - Because we're incrementally changing the code, there may be
      temporary changes to allow existing code to continue to work.
    
    Co-Authored-By: Assaf Muller
    
    Change-Id: I674c72e37b56aa1f729110310e6f697297c47c09
    Partially-implements: blueprint restructure-l3-agent

---
+++ neutron/agent/l3/event_observers.py
@@ -0,0 +1,35 @@
+# Copyright 2014 OpenStack Foundation
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+class L3EventObservers(object):
+
+    """Manages observers for L3 agent events."""
+
+    def __init__(self):
+        self.observers = set()
+
+    def add(self, observer):
+        """Add a listener for L3 agent notifications."""
+        self.observers.add(observer)
+
+    def notify(self, l3_event_action, *args, **kwargs):
+        """Give interested parties a chance to act on event.
+
+        NOTE: Preserves existing behavior for error propagation.
+        """
+        method_name = l3_event_action.__name__
+        for observer in self.observers:
+            getattr(observer, method_name)(*args, **kwargs)
