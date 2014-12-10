65b8daffd1915ff2dbef4461763065ebe441a6f8
Matthew Treinish <mtreinish@kortar.org>
2014-12-10 13:21:28 -0500

    Switch to using subunit-trace from tempest-lib
    This commit removes the local copy of subunit-trace and instead uses
    the version packaged with tempest-lib.
    
    Change-Id: Ia7b57c6fc092514d9fbe4e71f580a4b189dc68b0

--- tools/pretty_tox.sh
+++ tools/pretty_tox.sh
@@ -3,4 +3,4 @@
 TESTRARGS=$1
 
 exec 3>&1
-status=$(exec 4>&1 >&3; ( python -m neutron.openstack.common.lockutils python setup.py testr --slowest --testr-args="--subunit $TESTRARGS"; echo $? >&4 ) | $(dirname $0)/subunit-trace.py -f) && exit $status
+status=$(exec 4>&1 >&3; ( python -m neutron.openstack.common.lockutils python setup.py testr --slowest --testr-args="--subunit $TESTRARGS"; echo $? >&4 ) | subunit-trace -f) && exit $status
