96c34a780fd3f504ea1bef671cc3ed1d9f6bc2ef
Doug Wiegley <dougw@a10networks.com>
2014-12-12 19:20:47 -0700

    Minor lbaasv2 things from the feature branch, needed in neutron
    - Minor neutron.conf fix from feature branch until config is moved
    - Some extra constants for extension
    - Jinja template entry in services.conf
    
    Change-Id: I4a15d0ac422457d2286d4b24d9f5067047e81563
    Partially-Implements: blueprint services-split
    Co-Authored-By: Brandon Logan <brandon.logan@rackspace.com>

--- neutron/plugins/common/constants.py
+++ neutron/plugins/common/constants.py
@@ -17,6 +17,7 @@
 CORE = "CORE"
 DUMMY = "DUMMY"
 LOADBALANCER = "LOADBALANCER"
+LOADBALANCERV2 = "LOADBALANCERV2"
 FIREWALL = "FIREWALL"
 VPN = "VPN"
 METERING = "METERING"
@@ -27,6 +28,7 @@ L3_ROUTER_NAT = "L3_ROUTER_NAT"
 EXT_TO_SERVICE_MAPPING = {
     'dummy': DUMMY,
     'lbaas': LOADBALANCER,
+    'lbaasv2': LOADBALANCERV2,
     'fwaas': FIREWALL,
     'vpnaas': VPN,
     'metering': METERING,
@@ -35,12 +37,13 @@ EXT_TO_SERVICE_MAPPING = {
 
 # TODO(salvatore-orlando): Move these (or derive them) from conf file
 ALLOWED_SERVICES = [CORE, DUMMY, LOADBALANCER, FIREWALL, VPN, METERING,
-                    L3_ROUTER_NAT]
+                    L3_ROUTER_NAT, LOADBALANCERV2]
 
 COMMON_PREFIXES = {
     CORE: "",
     DUMMY: "/dummy_svc",
     LOADBALANCER: "/lb",
+    LOADBALANCERV2: "/lbaas",
     FIREWALL: "/fw",
     VPN: "/vpn",
     METERING: "/metering",
