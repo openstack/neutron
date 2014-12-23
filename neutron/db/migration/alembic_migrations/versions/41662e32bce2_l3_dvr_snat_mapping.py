6ee81d35fc269863f1fe08c57f3672fdc48197cf
rajeev <rajeev.grover@hp.com>
2014-12-23 13:49:19 -0500

    HA for DVR - schema migration and change
    To support HA for DVR SNAT, default SNAT has to be schedulable
    on multiple L3 agents. The csnat_l3_agent_bindings table is being
    modified to include l3_agent_id in the primary key.
    The migration script and Class definition update is included in
    this patch. For modularity and code management, HA/DVR methods
    that would make use of this change will be included in a different
    patch.
    
    Partial-bug: #1365473
    Change-Id: Idfe93cace0c1b633be6e786206fbec6e1f3c13cd

---
+++ neutron/db/migration/alembic_migrations/versions/41662e32bce2_l3_dvr_snat_mapping.py
@@ -0,0 +1,64 @@
+# Copyright 2014 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+"""L3 DVR SNAT mapping
+
+Revision ID: 41662e32bce2
+Revises: 4dbe243cd84d
+Create Date: 2014-12-22 16:48:56.922833
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '41662e32bce2'
+down_revision = '4dbe243cd84d'
+
+from alembic import op
+from sqlalchemy.engine import reflection
+
+from neutron.db import migration
+
+
+TABLE_NAME = 'csnat_l3_agent_bindings'
+
+
+def upgrade():
+    inspector = reflection.Inspector.from_engine(op.get_bind())
+    prev_pk_const = inspector.get_pk_constraint(TABLE_NAME)
+    prev_pk_name = prev_pk_const.get('name')
+
+    with migration.remove_fks_from_table(TABLE_NAME):
+        op.drop_constraint(name=prev_pk_name,
+                           table_name=TABLE_NAME,
+                           type_='primary')
+
+        op.create_primary_key(name=None,
+                              table_name=TABLE_NAME,
+                              cols=['router_id', 'l3_agent_id'])
+
+
+def downgrade():
+    inspector = reflection.Inspector.from_engine(op.get_bind())
+    prev_pk_const = inspector.get_pk_constraint(TABLE_NAME)
+    prev_pk_name = prev_pk_const.get('name')
+
+    with migration.remove_fks_from_table(TABLE_NAME):
+        op.drop_constraint(name=prev_pk_name,
+                           table_name=TABLE_NAME,
+                           type_='primary')
+
+        op.create_primary_key(name=None,
+                              table_name=TABLE_NAME,
+                              cols=['router_id'])
