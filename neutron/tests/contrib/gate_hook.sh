8d848353fb1979d3c9cb7e5970073dcf28d60516
armando-migliaccio <armamig@gmail.com>
2015-01-06 11:03:14 -0800

    Drop functional/contrib directory
    This has been replaced by tests/contrib and the infra change [1]
    is now in force and this copy is no longer necessary.
    
    [1] https://review.openstack.org/#/c/142603/
    
    Change-Id: Idbe0814e6b258d2f394894c1923f459cc1bdd4d2

--- neutron/tests/contrib/gate_hook.sh
+++ neutron/tests/contrib/gate_hook.sh
@@ -11,7 +11,7 @@ then
     # Add a rootwrap filter to support test-only
     # configuration (e.g. a KillFilter for processes that
     # use the python installed in a tox env).
-    FUNC_FILTER=$BASE/new/neutron/neutron/tests/functional/contrib/filters.template
+    FUNC_FILTER=$BASE/new/neutron/neutron/tests/contrib/filters.template
     sed -e "s+\$BASE_PATH+$BASE/new/neutron/.tox/dsvm-functional+" \
         $FUNC_FILTER | sudo tee /etc/neutron/rootwrap.d/functional.filters > /dev/null
 
