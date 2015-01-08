766c2738ae16ebbae37f26b17e261f0112616bb5
Itsuro Oda <oda@valinux.co.jp>
2015-01-09 08:47:56 +0900

    Enable services on agents with admin_state_up False
    Previously when admin_state_up of an agent is turned to False,
    all services on it will be disabled.
    This fix makes existing services on agents with admin_state_up
    False keep available.
    To keep current behavior available the following configuration
    parameter added.
    
    * enable_services_on_agents_with_admin_state_down
    
    If the parameter is True, existing services on agents with admin_state_up
    False keep available. No more service will be scheduled to the agent
    automatically. But adding a service to the agent manually is available.
    i.e. admin_state_up: False means to stop automatic scheduling under the
    parameter is True.
    The default of the parameter is False (current behavior).
    
    Change-Id: Ifba606a5c1f3f07d717c7695a7a64e16238c2057
    Closes-Bug: #1408488

--- neutron/api/rpc/agentnotifiers/dhcp_rpc_agent_api.py
+++ neutron/api/rpc/agentnotifiers/dhcp_rpc_agent_api.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from oslo_config import cfg
 from oslo_log import log as logging
 import oslo_messaging
 
@@ -75,9 +76,11 @@ class DhcpAgentNotifyAPI(object):
         return new_agents + existing_agents
 
     def _get_enabled_agents(self, context, network, agents, method, payload):
-        """Get the list of agents whose admin_state is UP."""
+        """Get the list of agents who can provide services."""
         network_id = network['id']
-        enabled_agents = [x for x in agents if x.admin_state_up]
+        enabled_agents = agents
+        if not cfg.CONF.enable_services_on_agents_with_admin_state_down:
+            enabled_agents = [x for x in agents if x.admin_state_up]
         active_agents = [x for x in agents if x.is_active]
         len_enabled_agents = len(enabled_agents)
         len_active_agents = len(active_agents)
