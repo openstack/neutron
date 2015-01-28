29aa644eb33cbf6bd54935fd0d0f3283872f72cc
Doug Wiegley <dougw@a10networks.com>
2015-01-27 20:17:00 -0700

    Mechanisms to move extensions and config into service repos
    - Extensions will automatically be loaded from service repos in addition
      to neutron proper, but neutron proper will take precedence.
    - Config entries for service repos will be read out of neutron-{service}.conf
      first, and then neutron.conf. After Kilo, they will be read only from
      neutron-{service}.conf.
    - Service providers for drivers will be collected from all neutron conf files.
    
    This is review 1 of 3.  The second set will be in the server repos, moving
    the extensions.  The third will be in neutron, removing the service exts.
    
    Change-Id: I16b5e5b2bb70717166da14faa975fa2ab9129049
    Partially-Implements: blueprint services-split

---
+++ neutron/common/repos.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2015, A10 Networks
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import ConfigParser
+import importlib
+import os
+
+from neutron.openstack.common import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+class NeutronModules(object):
+
+    MODULES = [
+        'neutron_fwaas',
+        'neutron_lbaas',
+        'neutron_vpnaas'
+    ]
+
+    def __init__(self):
+        self.repos = {}
+        for repo in self.MODULES:
+            self.repos[repo] = {}
+            self.repos[repo]['mod'] = self._import_or_none(repo)
+            self.repos[repo]['ini'] = None
+
+    def _import_or_none(self, module):
+            try:
+                return importlib.import_module(module)
+            except ImportError:
+                return None
+
+    def installed_list(self):
+        z = filter(lambda k: self.repos[k]['mod'] is not None, self.repos)
+        LOG.debug("NeutronModules related repos installed = %s", z)
+        return z
+
+    def module(self, module):
+        return self.repos[module]['mod']
+
+    # Return an INI parser for the child module. oslo.conf is a bit too
+    # magical in its INI loading, and in one notable case, we need to merge
+    # together the [service_providers] section for across at least four
+    # repositories.
+    def ini(self, module):
+        if self.repos[module]['ini'] is None:
+            ini = ConfigParser.SafeConfigParser()
+
+            ini_path = '/etc/neutron/%s.conf' % module
+            if os.path.exists(ini_path):
+                ini.read(ini_path)
+
+            self.repos[module]['ini'] = ini
+
+        return self.repos[module]['ini']
