9bb7eb5e1baed71e95f772474df9c6376563225b
Jakub Libosvar <libosvar@redhat.com>
2015-02-03 14:33:39 +0100

    Don't crash when adding duplicate gre allocation
    This patch catches DBDuplicateError when initializing ML2 GRE type
    driver and allocation already exists in DB. Because current allocations
    are queried and then only those that doesn't exist in database are
    added, DBDuplicateError should never occur.
    
    But the race can happen when running multiple neutron-servers and one
    of servers adds allocations between allocations are queried and added.
    
    Closes-bug: 1417560
    
    Conflicts:
    	neutron/tests/unit/ml2/test_type_gre.py
    
    Change-Id: I427b7020d61b0d2c06292ff2804ba1f4483696c6
    (cherry picked from commit 50b469fae2dbd1517d856509f37328394e0697e4)

--- neutron/tests/unit/ml2/test_type_gre.py
+++ neutron/tests/unit/ml2/test_type_gre.py
@@ -15,6 +15,11 @@
 
 import mock
 
+from oslo.db import exception as db_exc
+from sqlalchemy.orm import exc as sa_exc
+import testtools
+
+from neutron.db import api as db_api
 from neutron.plugins.common import constants as p_const
 from neutron.plugins.ml2.drivers import type_gre
 from neutron.tests.unit.ml2 import test_type_vxlan
@@ -23,6 +28,16 @@ from neutron.tests.unit import testlib_api
 
 TUNNEL_IP_ONE = "10.10.10.10"
 TUNNEL_IP_TWO = "10.10.10.20"
+
+
+def _add_allocation(session, gre_id, allocated=False):
+    allocation = type_gre.GreAllocation(gre_id=gre_id, allocated=allocated)
+    allocation.save(session)
+
+
+def _get_allocation(session, gre_id):
+    return session.query(type_gre.GreAllocation).filter_by(
+        gre_id=gre_id).one()
 
 
 class GreTypeTest(test_type_vxlan.TunnelTypeTestMixin,
@@ -47,6 +62,32 @@ class GreTypeTest(test_type_vxlan.TunnelTypeTestMixin,
         with mock.patch.object(type_gre.LOG, 'warning') as log_warn:
             self.driver.add_endpoint(TUNNEL_IP_ONE)
         log_warn.assert_called_once_with(mock.ANY, TUNNEL_IP_ONE)
+
+    def test_sync_allocations_entry_added_during_session(self):
+        with mock.patch.object(self.driver, '_add_allocation',
+                               side_effect=db_exc.DBDuplicateEntry) as (
+                mock_add_allocation):
+            self.driver.sync_allocations()
+            self.assertTrue(mock_add_allocation.called)
+
+    def test__add_allocation_not_existing(self):
+        session = db_api.get_session()
+        _add_allocation(session, gre_id=1)
+        self.driver._add_allocation(session, set([1, 2]))
+        _get_allocation(session, 2)
+
+    def test__add_allocation_existing_allocated_is_kept(self):
+        session = db_api.get_session()
+        _add_allocation(session, gre_id=1, allocated=True)
+        self.driver._add_allocation(session, set([2]))
+        _get_allocation(session, 1)
+
+    def test__add_allocation_existing_not_allocated_is_removed(self):
+        session = db_api.get_session()
+        _add_allocation(session, gre_id=1)
+        self.driver._add_allocation(session, set([2]))
+        with testtools.ExpectedException(sa_exc.NoResultFound):
+            _get_allocation(session, 1)
 
 
 class GreTypeMultiRangeTest(test_type_vxlan.TunnelTypeMultiRangeTestMixin,
