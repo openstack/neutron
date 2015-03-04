857322c30ecfed11e770f0aea7b234f8ffdc0db1
Ann Kamyshnikova <akamyshnikova@mirantis.com>
2015-03-04 15:50:07 +0300

    Use accessors instead of private attributes for Ml2 plugin
    Start using accessors instead of private attributes of PortContext
    and NetworkContext where it is possible.
    
    Closes-bug: #1424587
    
    Change-Id: Ie3d963ae6cee9782d65ba683936072602c78ebaa

--- neutron/plugins/ml2/managers.py
+++ neutron/plugins/ml2/managers.py
@@ -595,7 +595,7 @@ class MechanismManager(stevedore.named.NamedExtensionManager):
 
     def _bind_port_level(self, context, level, segments_to_bind):
         binding = context._binding
-        port_id = context._port['id']
+        port_id = context.current['id']
         LOG.debug("Attempting to bind port %(port)s on host %(host)s "
                   "at level %(level)s using segments %(segments)s",
                   {'port': port_id,
@@ -652,7 +652,7 @@ class MechanismManager(stevedore.named.NamedExtensionManager):
                               driver.name)
         binding.vif_type = portbindings.VIF_TYPE_BINDING_FAILED
         LOG.error(_LE("Failed to bind port %(port)s on host %(host)s"),
-                  {'port': context._port['id'],
+                  {'port': context.current['id'],
                    'host': binding.host})
 
     def _check_driver_to_bind(self, driver, segments_to_bind, binding_levels):
