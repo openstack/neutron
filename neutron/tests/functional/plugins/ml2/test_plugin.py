63fe3a418c685ca90077f1dd4c35fd9ccf586fca
Ihar Hrachyshka <ihrachys@redhat.com>
2015-03-09 18:05:18 +0100

    Add the rebinding chance in _bind_port_if_needed
    Make function _bind_port_if_needed to bind at least one time when the port's
    binding status passed in is already in binding_failed.
    
    This is the second attempt to introduce the patch (the first one was
    reverted due to regression that broke Ironic), now with proper
    notification sent even when binding attempt failed.
    
    The patch also fixes several cases when we attempted to notify with a
    binding context that was not committed into database.
    
    The patch changes _attempt_binding to call _commit_port_binding only
    with the binding final state:
    1. Successful binding: will just call _commit_port_binding.
    2. Unsuccessful binding: will call _commit_port_binding at the final
    attempt to bind the port.
    This is in order to refrain from reverts, with will really complicate
    things even  more.
    
    Co-Authored-By: Yalei Wang <yalei.wang@intel.com>
    Co-Authored-By: Nir Magnezi <nmagnezi@redhat.com>
    Co-Authored-By: John Schwarz <jschwarz@redhat.com>
    Change-Id: I437290affd8eb87177d0626bf7935a165859cbdd
    Closes-Bug: #1399249

---
+++ neutron/tests/functional/plugins/ml2/test_plugin.py
@@ -0,0 +1,71 @@
+# Copyright (c) 2016 Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron.common import constants
+from neutron import context
+from neutron.db import agents_db
+from neutron.extensions import portbindings
+from neutron.tests.common import helpers
+from neutron.tests.unit.plugins.ml2 import base as ml2_test_base
+
+
+DEVICE_OWNER_COMPUTE = constants.DEVICE_OWNER_COMPUTE_PREFIX + 'fake'
+
+
+class TestMl2PortBinding(ml2_test_base.ML2TestFramework,
+                         agents_db.AgentDbMixin):
+    def setUp(self):
+        super(TestMl2PortBinding, self).setUp()
+        self.admin_context = context.get_admin_context()
+        self.host_args = {portbindings.HOST_ID: helpers.HOST,
+                          'admin_state_up': True}
+
+    def test_port_bind_successfully(self):
+        helpers.register_ovs_agent(host=helpers.HOST)
+        with self.network() as network:
+            with self.subnet(network=network) as subnet:
+                with self.port(
+                        subnet=subnet, device_owner=DEVICE_OWNER_COMPUTE,
+                        arg_list=(portbindings.HOST_ID, 'admin_state_up',),
+                        **self.host_args) as port:
+                    # Note: Port creation invokes _bind_port_if_needed(),
+                    # therefore it is all we need in order to test a successful
+                    # binding
+                    self.assertEqual(port['port']['binding:vif_type'],
+                                     portbindings.VIF_TYPE_OVS)
+
+    def test_port_bind_retry(self):
+        agent = helpers.register_ovs_agent(host=helpers.HOST)
+        helpers.kill_agent(agent_id=agent.id)
+        with self.network() as network:
+            with self.subnet(network=network) as subnet:
+                with self.port(
+                        subnet=subnet, device_owner=DEVICE_OWNER_COMPUTE,
+                        arg_list=(portbindings.HOST_ID, 'admin_state_up',),
+                        **self.host_args) as port:
+                    # Since the agent is dead, expect binding to fail
+                    self.assertEqual(port['port']['binding:vif_type'],
+                                     portbindings.VIF_TYPE_BINDING_FAILED)
+                    helpers.revive_agent(agent.id)
+                    # When an agent starts, The RPC call get_device_details()
+                    # will invoke get_bound_port_context() which eventually use
+                    # _bind_port_if_needed()
+                    bound_context = self.plugin.get_bound_port_context(
+                        self.admin_context, port['port']['id'], helpers.HOST)
+                    # Since the agent is back online, expect binding to succeed
+                    self.assertEqual(bound_context.vif_type,
+                                     portbindings.VIF_TYPE_OVS)
+                    self.assertEqual(bound_context.current['binding:vif_type'],
+                                     portbindings.VIF_TYPE_OVS)
