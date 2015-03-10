1508213c382e0451caa956f166c670db5d68ad86
Rich Curran <rcurran@cisco.com>
2015-03-10 10:55:19 -0400

    ML2 cisco_nexus MD: sync config and models with vendor repo
    Updates required to the upstream neutron ML2 cisco nexus MD code to
    support stackforge repo changes.
    
    Original development done under cisco-openstack repo. File history for
    these changes can be found under the staging/junoplus branch.
    
    Change-Id: Idc9da84cefa4131a84b911f77d99a3b1647103a8
    Closes-Bug: 1429879

--- neutron/plugins/ml2/drivers/cisco/nexus/nexus_models_v2.py
+++ neutron/plugins/ml2/drivers/cisco/nexus/nexus_models_v2.py
@@ -17,6 +17,7 @@
 import sqlalchemy as sa
 
 from neutron.db import model_base
+from neutron.db import models_v2
 
 
 class NexusPortBinding(model_base.BASEV2):
@@ -27,19 +28,63 @@ class NexusPortBinding(model_base.BASEV2):
     binding_id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     port_id = sa.Column(sa.String(255))
     vlan_id = sa.Column(sa.Integer, nullable=False)
+    vni = sa.Column(sa.Integer)
     switch_ip = sa.Column(sa.String(255))
     instance_id = sa.Column(sa.String(255))
+    is_provider_vlan = sa.Column(sa.Boolean(), nullable=False, default=False,
+                           server_default=sa.sql.false())
 
     def __repr__(self):
         """Just the binding, without the id key."""
-        return ("<NexusPortBinding(%s,%s,%s,%s)>" %
-                (self.port_id, self.vlan_id, self.switch_ip, self.instance_id))
+        return ("<NexusPortBinding(%s,%s,%s,%s, %s, %s)>" %
+                (self.port_id, self.vlan_id, self.vni, self.switch_ip,
+                 self.instance_id,
+                 'True' if self.is_provider_vlan else 'False'))
 
     def __eq__(self, other):
         """Compare only the binding, without the id key."""
         return (
             self.port_id == other.port_id and
             self.vlan_id == other.vlan_id and
+            self.vni == other.vni and
             self.switch_ip == other.switch_ip and
-            self.instance_id == other.instance_id
+            self.instance_id == other.instance_id and
+            self.is_provider_vlan == other.is_provider_vlan
         )
+
+
+class NexusNVEBinding(model_base.BASEV2):
+    """Represents Network Virtualization Endpoint configuration."""
+
+    __tablename__ = "cisco_ml2_nexus_nve"
+
+    vni = sa.Column(sa.Integer, primary_key=True, nullable=False)
+    device_id = sa.Column(sa.String(255), primary_key=True)
+    switch_ip = sa.Column(sa.String(255), primary_key=True)
+    mcast_group = sa.Column(sa.String(255))
+
+    def __repr__(self):
+        return ("<NexusNVEBinding(%s,%s,%s,%s)>" %
+                (self.vni, self.switch_ip, self.device_id, self.mcast_group))
+
+
+class NexusVxlanAllocation(model_base.BASEV2):
+
+    __tablename__ = 'ml2_nexus_vxlan_allocations'
+
+    vxlan_vni = sa.Column(sa.Integer, nullable=False, primary_key=True,
+                          autoincrement=False)
+    allocated = sa.Column(sa.Boolean, nullable=False, default=False,
+                          server_default=sa.sql.false())
+
+
+class NexusMcastGroup(model_base.BASEV2, models_v2.HasId):
+
+    __tablename__ = 'ml2_nexus_vxlan_mcast_groups'
+
+    mcast_group = sa.Column(sa.String(64), nullable=False)
+    associated_vni = sa.Column(sa.Integer,
+                               sa.ForeignKey(
+                                   'ml2_nexus_vxlan_allocations.vxlan_vni',
+                                   ondelete="CASCADE"),
+                               nullable=False)
