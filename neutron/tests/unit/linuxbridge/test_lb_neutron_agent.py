openstack/neutron

commit b7a56fd1b44649daa1f768157e68a135b9e01dd1

Default the Linuxbridge agent to enabling VXLAN
Linux kernels from 3.7 and up have supported VXLAN with Linuxbridge.
I think it's time we default to enabling this support in the
Linuxbridge agent in Neutron as well.

DocImpact

Change-Id: I8a99c973b4816e3a2845cdb8abd71d6250bc4800

etc/neutron/plugins/linuxbridge/linuxbridge_conf.ini
@@ -34,8 +34,8 @@
 [vxlan]
 # (BoolOpt) enable VXLAN on the agent
 # VXLAN support can be enabled when agent is managed by ml2 plugin using
-# linuxbridge mechanism driver. Useless if set while using linuxbridge plugin.
-# enable_vxlan = False
+# linuxbridge mechanism driver.
+# enable_vxlan = True
 #
 # (IntOpt) use specific TTL for vxlan interface protocol packets
 # ttl =

neutron/plugins/linuxbridge/common/config.py
@@ -32,7 +32,7 @@ vlan_opts = [
 ]
 
 vxlan_opts = [
-    cfg.BoolOpt('enable_vxlan', default=False,
+    cfg.BoolOpt('enable_vxlan', default=True,
                 help=_("Enable VXLAN on the agent. Can be enabled when "
                        "agent is managed by ml2 plugin using linuxbridge "
                        "mechanism driver")),

commit ce0b13982158d813024cad1fae7f495e3545b7ee

linuxbridge UT: Mock get_interface_by_ip
To avoid querying devices on the system running UT,
which might or might not work.

The regression was introduced by
commit b7a56fd1b44649daa1f768157e68a135b9e01dd1 .

Closes-Bug: #1430098
Change-Id: I2dcdf4b9516e0a90dffee26684fdce7f0ce43cd8

neutron/tests/unit/linuxbridge/test_lb_neutron_agent.py
@@ -47,8 +47,10 @@ class TestLinuxBridge(base.BaseTestCase):
         super(TestLinuxBridge, self).setUp()
         interface_mappings = {'physnet1': 'eth1'}
 
-        self.linux_bridge = linuxbridge_neutron_agent.LinuxBridgeManager(
-            interface_mappings)
+        with mock.patch.object(linuxbridge_neutron_agent.LinuxBridgeManager,
+                               'get_interface_by_ip', return_value=None):
+            self.linux_bridge = linuxbridge_neutron_agent.LinuxBridgeManager(
+                interface_mappings)
 
     def test_ensure_physical_in_bridge_invalid(self):
         result = self.linux_bridge.ensure_physical_in_bridge('network_id',
@@ -104,8 +106,10 @@ class TestLinuxBridgeAgent(base.BaseTestCase):
                                     'get_interface_mac')
         self.get_mac = self.get_mac_p.start()
         self.get_mac.return_value = '00:00:00:00:00:01'
-        self.agent = linuxbridge_neutron_agent.LinuxBridgeNeutronAgentRPC({},
-                                                                          0)
+        with mock.patch.object(linuxbridge_neutron_agent.LinuxBridgeManager,
+                               'get_interface_by_ip', return_value=None):
+            self.agent = linuxbridge_neutron_agent.LinuxBridgeNeutronAgentRPC(
+                {}, 0)
 
     def test_treat_devices_removed_with_existed_device(self):
         agent = self.agent
@@ -339,8 +343,10 @@ class TestLinuxBridgeManager(base.BaseTestCase):
         super(TestLinuxBridgeManager, self).setUp()
         self.interface_mappings = {'physnet1': 'eth1'}
 
-        self.lbm = linuxbridge_neutron_agent.LinuxBridgeManager(
-            self.interface_mappings)
+        with mock.patch.object(linuxbridge_neutron_agent.LinuxBridgeManager,
+                               'get_interface_by_ip', return_value=None):
+            self.lbm = linuxbridge_neutron_agent.LinuxBridgeManager(
+                self.interface_mappings)
 
     def test_interface_exists_on_bridge(self):
         with mock.patch.object(os, 'listdir') as listdir_fn:
@@ -752,8 +758,10 @@ class TestLinuxBridgeManager(base.BaseTestCase):
 
     def test_delete_vxlan_bridge_no_int_mappings(self):
         interface_mappings = {}
-        lbm = linuxbridge_neutron_agent.LinuxBridgeManager(
-            interface_mappings)
+        with mock.patch.object(linuxbridge_neutron_agent.LinuxBridgeManager,
+                               'get_interface_by_ip', return_value=None):
+            lbm = linuxbridge_neutron_agent.LinuxBridgeManager(
+                interface_mappings)
 
         with contextlib.nested(
             mock.patch.object(ip_lib, "device_exists"),