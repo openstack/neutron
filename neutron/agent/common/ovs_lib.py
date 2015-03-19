9e7f484adc199b424bb9a5390c8cf3ced0f77278
Terry Wilson <twilson@redhat.com>
2015-03-19 12:43:21 -0500

    OVS_LIB API addition - change bridge controller connection-mode
    Add an API to change controller connection mode to 'out-of-band', feature
    which might be useful for many projects using Openflow controller with OVS
    
    Change-Id: If93f6858f4eed05f5f1d9bdb1667838d80c490cd
    Closes-Bug: #1433208

--- neutron/agent/common/ovs_lib.py
+++ neutron/agent/common/ovs_lib.py
@@ -22,6 +22,7 @@ from oslo_log import log as logging
 from oslo_utils import excutils
 import retrying
 import six
+import uuid
 
 from neutron.agent.common import utils
 from neutron.agent.linux import ip_lib
@@ -427,6 +428,20 @@ class OVSBridge(BaseOVS):
         else:
             msg = _('Unable to determine mac address for %s') % self.br_name
             raise Exception(msg)
+
+    def set_controllers_connection_mode(self, connection_mode):
+        """Set bridge controllers connection mode.
+
+        :param connection_mode: "out-of-band" or "in-band"
+        """
+        attr = [('connection_mode', connection_mode)]
+        controllers = self.db_get_val('Bridge', self.br_name, 'controller')
+        controllers = [controllers] if isinstance(
+            controllers, uuid.UUID) else controllers
+        with self.ovsdb.transaction(check_error=True) as txn:
+            for controller_uuid in controllers:
+                txn.add(self.ovsdb.db_set('Controller',
+                                          controller_uuid, *attr))
 
     def __enter__(self):
         self.create()
