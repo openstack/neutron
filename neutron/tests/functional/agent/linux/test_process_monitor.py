openstack/neutron

commit eba4c2941ee60409d5e481347d9a856947dbf216

Use common agent.linux.utils.ensure_dir method
We repeated os.makedirs(dir, 0o755) in several places. We should use
common neutron.agent.linux.utils.ensure_dir. Unit tests are also added.

Change-Id: Iaeae5ff7dc6676420c000d6501f69a5997ad4b6c
Closes-bug: 1419042

neutron/tests/unit/agent/linux/test_utils.py
@@ -11,6 +11,8 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+import os
+
 import mock
 import testtools
 
@@ -203,3 +205,19 @@ class TestPathUtilities(base.BaseTestCase):
         self.assertFalse(utils.cmdlines_are_equal(
             ['ping', '8.8.8.8'],
             ['/usr/bin/ping6', '8.8.8.8']))
+
+
+class TestBaseOSUtils(base.BaseTestCase):
+    @mock.patch.object(os.path, 'isdir', return_value=False)
+    @mock.patch.object(os, 'makedirs')
+    def test_ensure_dir_not_exist(self, makedirs, isdir):
+        utils.ensure_dir('/the')
+        isdir.assert_called_once_with('/the')
+        makedirs.assert_called_once_with('/the', 0o755)
+
+    @mock.patch.object(os.path, 'isdir', return_value=True)
+    @mock.patch.object(os, 'makedirs')
+    def test_ensure_dir_exist(self, makedirs, isdir):
+        utils.ensure_dir('/the')
+        isdir.assert_called_once_with('/the')
+        self.assertFalse(makedirs.called)

neutron/agent/dhcp/agent.py
@@ -24,6 +24,7 @@ from oslo_utils import importutils
 
 from neutron.agent.linux import dhcp
 from neutron.agent.linux import external_process
+from neutron.agent.linux import utils as linux_utils
 from neutron.agent.metadata import driver as metadata_driver
 from neutron.agent import rpc as agent_rpc
 from neutron.common import constants
@@ -62,8 +63,7 @@ class DhcpAgent(manager.Manager):
                                         ctx, self.conf.use_namespaces)
         # create dhcp dir to store dhcp info
         dhcp_dir = os.path.dirname("/%s/dhcp/" % self.conf.state_path)
-        if not os.path.isdir(dhcp_dir):
-            os.makedirs(dhcp_dir, 0o755)
+        linux_utils.ensure_dir(dhcp_dir)
         self.dhcp_version = self.dhcp_driver_cls.check_version()
         self._populate_networks_cache()
         self._process_monitor = external_process.ProcessMonitor(

neutron/agent/l3/ha.py
@@ -18,6 +18,7 @@ import os
 from oslo_config import cfg
 
 from neutron.agent.linux import keepalived
+from neutron.agent.linux import utils
 from neutron.common import constants as l3_constants
 from neutron.i18n import _LE
 from neutron.openstack.common import log as logging
@@ -51,8 +52,7 @@ class AgentMixin(object):
 
     def _init_ha_conf_path(self):
         ha_full_path = os.path.dirname("/%s/" % self.conf.ha_confs_path)
-        if not os.path.isdir(ha_full_path):
-            os.makedirs(ha_full_path, 0o755)
+        utils.ensure_dir(ha_full_path)
 
     def process_ha_router_added(self, ri):
         ha_port = ri.router.get(l3_constants.HA_INTERFACE_KEY)

neutron/agent/linux/dhcp.py
@@ -170,7 +170,7 @@ class DhcpLocalProcess(DhcpBase):
                                                version, plugin)
         self.confs_dir = self.get_confs_dir(conf)
         self.network_conf_dir = os.path.join(self.confs_dir, network.id)
-        self._ensure_network_conf_dir()
+        utils.ensure_dir(self.network_conf_dir)
 
     @staticmethod
     def get_confs_dir(conf):
@@ -179,11 +179,6 @@ class DhcpLocalProcess(DhcpBase):
     def get_conf_file_name(self, kind):
         """Returns the file name for a given kind of config file."""
         return os.path.join(self.network_conf_dir, kind)
-
-    def _ensure_network_conf_dir(self):
-        """Ensures the directory for configuration files is created."""
-        if not os.path.isdir(self.network_conf_dir):
-            os.makedirs(self.network_conf_dir, 0o755)
 
     def _remove_config_files(self):
         shutil.rmtree(self.network_conf_dir, ignore_errors=True)
@@ -200,7 +195,7 @@ class DhcpLocalProcess(DhcpBase):
         if self.active:
             self.restart()
         elif self._enable_dhcp():
-            self._ensure_network_conf_dir()
+            utils.ensure_dir(self.network_conf_dir)
             interface_name = self.device_manager.setup(self.network)
             self.interface_name = interface_name
             self.spawn_process()

neutron/agent/linux/keepalived.py
@@ -351,8 +351,8 @@ class KeepalivedNotifierMixin(object):
 
     def _get_full_config_file_path(self, filename, ensure_conf_dir=True):
         conf_dir = self.get_conf_dir()
-        if ensure_conf_dir and not os.path.isdir(conf_dir):
-            os.makedirs(conf_dir, 0o755)
+        if ensure_conf_dir:
+            utils.ensure_dir(conf_dir)
         return os.path.join(conf_dir, filename)
 
 

neutron/agent/metadata/agent.py
@@ -26,6 +26,7 @@ from oslo_utils import excutils
 import six.moves.urllib.parse as urlparse
 import webob
 
+from neutron.agent.linux import utils as linux_utils
 from neutron.agent import rpc as agent_rpc
 from neutron.common import constants as n_const
 from neutron.common import rpc as n_rpc
@@ -311,7 +312,7 @@ class UnixDomainMetadataProxy(object):
                     if not os.path.exists(cfg.CONF.metadata_proxy_socket):
                         ctxt.reraise = False
         else:
-            os.makedirs(dirname, 0o755)
+            linux_utils.ensure_dir(dirname)
 
         self._init_state_reporting()
 

neutron/tests/unit/test_l3_agent.py
@@ -198,8 +198,9 @@ class BasicRouterOperationsFramework(base.BaseTestCase):
             'neutron.agent.linux.ip_lib.device_exists')
         self.device_exists = self.device_exists_p.start()
 
-        mock.patch('neutron.agent.l3.ha.AgentMixin'
-                   '._init_ha_conf_path').start()
+        self.ensure_dir = mock.patch('neutron.agent.linux.utils'
+                                     '.ensure_dir').start()
+
         mock.patch('neutron.agent.linux.keepalived.KeepalivedNotifierMixin'
                    '._get_full_config_file_path').start()
 
@@ -307,6 +308,11 @@ class BasicRouterOperationsFramework(base.BaseTestCase):
 
 
 class TestBasicRouterOperations(BasicRouterOperationsFramework):
+    def test_init_ha_conf(self):
+        with mock.patch('os.path.dirname', return_value='/etc/ha/'):
+            l3_agent.L3NATAgent(HOSTNAME, self.conf)
+            self.ensure_dir.assert_called_once_with('/etc/ha/')
+
     def test_periodic_sync_routers_task_raise_exception(self):
         agent = l3_agent.L3NATAgent(HOSTNAME, self.conf)
         self.plugin_api.get_routers.side_effect = ValueError

neutron/tests/unit/test_linux_dhcp.py
@@ -23,6 +23,7 @@ from neutron.agent.common import config
 from neutron.agent.dhcp import config as dhcp_config
 from neutron.agent.linux import dhcp
 from neutron.agent.linux import external_process
+from neutron.agent.linux import utils
 from neutron.common import config as base_config
 from neutron.common import constants
 from neutron.openstack.common import log as logging
@@ -674,15 +675,11 @@ class TestDhcpLocalProcess(TestBase):
         lp = LocalChild(self.conf, FakeV4Network())
         self.assertEqual(lp.get_conf_file_name('dev'), tpl)
 
-    def test_ensure_network_conf_dir(self):
+    @mock.patch.object(utils, 'ensure_dir')
+    def test_ensure_dir_called(self, ensure_dir):
         LocalChild(self.conf, FakeV4Network())
-        self.makedirs.assert_called_once_with(
-            '/dhcp/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', mock.ANY)
-
-    def test_ensure_network_conf_existing_dir(self):
-        self.isdir.return_value = True
-        LocalChild(self.conf, FakeV4Network())
-        self.assertFalse(self.makedirs.called)
+        ensure_dir.assert_called_once_with(
+            '/dhcp/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa')
 
     def test_enable_already_active(self):
         with mock.patch.object(LocalChild, 'active') as patched:
@@ -693,16 +690,15 @@ class TestDhcpLocalProcess(TestBase):
             self.assertEqual(lp.called, ['restart'])
             self.assertFalse(self.mock_mgr.return_value.setup.called)
 
-    def test_enable(self):
+    @mock.patch.object(utils, 'ensure_dir')
+    def test_enable(self, ensure_dir):
         attrs_to_mock = dict(
             [(a, mock.DEFAULT) for a in
-                ['active', 'get_conf_file_name', 'interface_name',
-                 '_ensure_network_conf_dir']]
+                ['active', 'interface_name']]
         )
 
         with mock.patch.multiple(LocalChild, **attrs_to_mock) as mocks:
             mocks['active'].__get__ = mock.Mock(return_value=False)
-            mocks['get_conf_file_name'].return_value = '/dir'
             mocks['interface_name'].__set__ = mock.Mock()
             lp = LocalChild(self.conf,
                             FakeDualNetwork())
@@ -713,7 +709,8 @@ class TestDhcpLocalProcess(TestBase):
                  mock.call().setup(mock.ANY)])
             self.assertEqual(lp.called, ['spawn'])
             self.assertTrue(mocks['interface_name'].__set__.called)
-            self.assertTrue(mocks['_ensure_network_conf_dir'].called)
+            ensure_dir.assert_called_with(
+                '/dhcp/cccccccc-cccc-cccc-cccc-cccccccccccc')
 
     def _assert_disabled(self, lp):
         self.assertTrue(lp.process_monitor.unregister.called)

commit 31631e82bbf974c50fb913dafe0ad86e2c0e6a8b

Fix intermittent UT failures in test_utils
Change eba4c2941ee introduced these tests. However they are not that useful as they
simply mimick the code, without really ensuring that the behavior is expected, so
they provide negative value ([1]), plus, they fail randomly.

This patch removes them in favor of a more useful functional check.

[1] http://googletesting.blogspot.com/2015/01/testing-on-toilet-change-detector-tests.html

Closes-bug: #1441347

Change-Id: I8a321995295deef7f6d30be303486be491e2771f

neutron/tests/functional/agent/linux/test_process_monitor.py
@@ -12,6 +12,8 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import os
+
 from oslo_config import cfg
 from six import moves
 
@@ -77,6 +79,10 @@ class BaseTestProcessMonitor(base.BaseTestCase):
     def wait_for_all_children_respawned(self):
         def all_children_active():
             return all(pm.active for pm in self._child_processes)
+
+        for pm in self._child_processes:
+            directory = os.path.dirname(pm.get_pid_file_name())
+            self.assertEqual(0o755, os.stat(directory).st_mode & 0o777)
 
         # we need to allow extra_time for the check process to happen
         # and properly execute action over the gone processes under

neutron/tests/unit/agent/linux/test_utils.py
@@ -12,8 +12,6 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import os
-
 import mock
 import socket
 import testtools
@@ -226,20 +224,6 @@ class TestBaseOSUtils(base.BaseTestCase):
     EUNAME = 'user'
     EGID = 456
     EGNAME = 'group'
-
-    @mock.patch.object(os.path, 'isdir', return_value=False)
-    @mock.patch.object(os, 'makedirs')
-    def test_ensure_dir_not_exist(self, makedirs, isdir):
-        utils.ensure_dir('/the')
-        isdir.assert_called_once_with('/the')
-        makedirs.assert_called_once_with('/the', 0o755)
-
-    @mock.patch.object(os.path, 'isdir', return_value=True)
-    @mock.patch.object(os, 'makedirs')
-    def test_ensure_dir_exist(self, makedirs, isdir):
-        utils.ensure_dir('/the')
-        isdir.assert_called_once_with('/the')
-        self.assertFalse(makedirs.called)
 
     @mock.patch('os.geteuid', return_value=EUID)
     @mock.patch('pwd.getpwuid', return_value=FakeUser(EUNAME))