d89ee0b995259216cf4fdef6ad1afe315e3f549f
Salvatore Orlando <salv.orlando@gmail.com>
2015-05-18 14:51:05 -0700

    Remove a unused Context class
    This class in neutron.tests.unit.plugins.opencontrail.test_contrail.plugin
    is not used anywhere and has no future development purpose.
    
    Change-Id: Ibf149c5392b97f2aa33ccfc97c8ad6377f34bfee

--- neutron/tests/unit/plugins/opencontrail/test_contrail_plugin.py
+++ neutron/tests/unit/plugins/opencontrail/test_contrail_plugin.py
@@ -14,9 +14,6 @@
 # limitations under the License.
 
 
-import datetime
-import uuid
-
 import mock
 import netaddr
 from oslo_config import cfg
@@ -180,21 +177,6 @@ class FakeServer(db_base_plugin_v2.NeutronDbPluginV2,
 
 
 FAKE_SERVER = FakeServer()
-
-
-class Context(object):
-    def __init__(self, tenant_id=''):
-        self.read_only = False
-        self.show_deleted = False
-        self.roles = [u'admin', u'KeystoneServiceAdmin', u'KeystoneAdmin']
-        self._read_deleted = 'no'
-        self.timestamp = datetime.datetime.now()
-        self.auth_token = None
-        self._session = None
-        self._is_admin = True
-        self.admin = uuid.uuid4().hex.decode()
-        self.request_id = 'req-' + str(uuid.uuid4())
-        self.tenant = tenant_id
 
 
 class KeyStoneInfo(object):
