9b29f62491ec0e1cb27601e6ad53cf152de5eb66
Moshe Levi <moshele@mellanox.com>
2015-05-31 13:27:56 +0300

    Refactor mlnx mechanism driver to support infiniband only
    The old mlnx mechanism driver was used for SR-IOV with Ethernet
    and infiniband support but the PCI allocation wasn't done by nova.
    Juno introduced sriovnicswitch mechanism driver for SR-IOV with
    Ethernet support. Mellanox recommends on using it.
    The updated mlnx mechanism driver supports SR-IOV infiniband.
    
    * support only port vnic_type 'direct'
    * update vif_type name to ib_hostdev (the ib_hostdev is generic vif_type
      for SR-IOV infiniband see https://review.openstack.org/#/c/187052/)
    
    Closes-Bug: #1460430
    DocImpact
    
    Change-Id: Ia822b492afdfeb1aecf373d5a8cdb72174512884

--- neutron/tests/unit/plugins/ml2/_test_mech_agent.py
+++ neutron/tests/unit/plugins/ml2/_test_mech_agent.py
@@ -146,6 +146,7 @@ class AgentMechanismBaseTestCase(base.BaseTestCase):
     AGENTS = None
     AGENTS_DEAD = None
     AGENTS_BAD = None
+    VNIC_TYPE = portbindings.VNIC_NORMAL
 
     def _check_unbound(self, context):
         self.assertIsNone(context._bound_segment_id)
@@ -177,7 +178,8 @@ class AgentMechanismGenericTestCase(AgentMechanismBaseTestCase):
     def test_unknown_type(self):
         context = FakePortContext(self.AGENT_TYPE,
                                   self.AGENTS,
-                                  self.UNKNOWN_TYPE_SEGMENTS)
+                                  self.UNKNOWN_TYPE_SEGMENTS,
+                                  vnic_type=self.VNIC_TYPE)
         self.driver.bind_port(context)
         self._check_unbound(context)
 
@@ -191,14 +193,16 @@ class AgentMechanismLocalTestCase(AgentMechanismBaseTestCase):
     def test_type_local(self):
         context = FakePortContext(self.AGENT_TYPE,
                                   self.AGENTS,
-                                  self.LOCAL_SEGMENTS)
+                                  self.LOCAL_SEGMENTS,
+                                  vnic_type=self.VNIC_TYPE)
         self.driver.bind_port(context)
         self._check_bound(context, self.LOCAL_SEGMENTS[1])
 
     def test_type_local_dead(self):
         context = FakePortContext(self.AGENT_TYPE,
                                   self.AGENTS_DEAD,
-                                  self.LOCAL_SEGMENTS)
+                                  self.LOCAL_SEGMENTS,
+                                  vnic_type=self.VNIC_TYPE)
         self.driver.bind_port(context)
         self._check_unbound(context)
 
@@ -213,14 +217,16 @@ class AgentMechanismFlatTestCase(AgentMechanismBaseTestCase):
     def test_type_flat(self):
         context = FakePortContext(self.AGENT_TYPE,
                                   self.AGENTS,
-                                  self.FLAT_SEGMENTS)
+                                  self.FLAT_SEGMENTS,
+                                  vnic_type=self.VNIC_TYPE)
         self.driver.bind_port(context)
         self._check_bound(context, self.FLAT_SEGMENTS[1])
 
     def test_type_flat_bad(self):
         context = FakePortContext(self.AGENT_TYPE,
                                   self.AGENTS_BAD,
-                                  self.FLAT_SEGMENTS)
+                                  self.FLAT_SEGMENTS,
+                                  vnic_type=self.VNIC_TYPE)
         self.driver.bind_port(context)
         self._check_unbound(context)
 
@@ -236,14 +242,16 @@ class AgentMechanismVlanTestCase(AgentMechanismBaseTestCase):
     def test_type_vlan(self):
         context = FakePortContext(self.AGENT_TYPE,
                                   self.AGENTS,
-                                  self.VLAN_SEGMENTS)
+                                  self.VLAN_SEGMENTS,
+                                  vnic_type=self.VNIC_TYPE)
         self.driver.bind_port(context)
         self._check_bound(context, self.VLAN_SEGMENTS[1])
 
     def test_type_vlan_bad(self):
         context = FakePortContext(self.AGENT_TYPE,
                                   self.AGENTS_BAD,
-                                  self.VLAN_SEGMENTS)
+                                  self.VLAN_SEGMENTS,
+                                  vnic_type=self.VNIC_TYPE)
         self.driver.bind_port(context)
         self._check_unbound(context)
 
