608c282285380704e70228ef549646a61492d2bd
Aman Kumar <amank@hp.com>
2015-06-02 23:49:40 -0700

    Make MockFixedIntervalLoopingCall class as a helper class
    MockFixedIntervalLoopingCall class is used in both the classes
    TestOvsNeutronAgent and TestOvsDvrNeutronAgent.
    
    This patch removes the MockFixedIntervalLoopingCall from both the
    places and make it as a helper class in test_ovs_neutron_agent.py.
    So that it will be used everywhere in test_ovs_neutron_agent.py.
    
    Change-Id: I5e0803436e91b4f20ec03209e9e799a68df4eaf4

--- neutron/tests/unit/plugins/openvswitch/agent/test_ovs_neutron_agent.py
+++ neutron/tests/unit/plugins/openvswitch/agent/test_ovs_neutron_agent.py
@@ -45,6 +45,14 @@ class FakeVif(object):
     port_name = 'name'
 
 
+class MockFixedIntervalLoopingCall(object):
+    def __init__(self, f):
+        self.f = f
+
+    def start(self, interval=0):
+        self.f()
+
+
 class CreateAgentConfigMap(ovs_test_base.OVSAgentConfigTestBase):
 
     def test_create_agent_config_map_succeeds(self):
@@ -105,13 +113,6 @@ class TestOvsNeutronAgent(object):
         cfg.CONF.set_default('quitting_rpc_timeout', 10, 'AGENT')
         cfg.CONF.set_default('prevent_arp_spoofing', False, 'AGENT')
         kwargs = self.mod_agent.create_agent_config_map(cfg.CONF)
-
-        class MockFixedIntervalLoopingCall(object):
-            def __init__(self, f):
-                self.f = f
-
-            def start(self, interval=0):
-                self.f()
 
         with mock.patch.object(self.mod_agent.OVSNeutronAgent,
                                'setup_integration_br'),\
@@ -1228,13 +1229,6 @@ class TestOvsDvrNeutronAgent(object):
                              'neutron.agent.firewall.NoopFirewallDriver',
                              group='SECURITYGROUP')
         kwargs = self.mod_agent.create_agent_config_map(cfg.CONF)
-
-        class MockFixedIntervalLoopingCall(object):
-            def __init__(self, f):
-                self.f = f
-
-            def start(self, interval=0):
-                self.f()
 
         with mock.patch.object(self.mod_agent.OVSNeutronAgent,
                                'setup_integration_br'),\
