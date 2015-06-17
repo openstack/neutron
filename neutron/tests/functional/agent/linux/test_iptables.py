b9e9cfb08bf0609dcfea46403c510607e858926a
Jakub Libosvar <libosvar@redhat.com>
2015-06-17 13:10:13 +0000

    Move NetcatTester to common/net_helpers
    The NetcatTester is a testing tool that can be used also in fullstack
    tests so I think it should go there to avoid imports in fullstack tests
    from functional.
    
    Tests for original helpers module was removed.
    
    Change-Id: I7229eba1dbc2ca3d524a1a021256b6202f4aecee

--- neutron/tests/functional/agent/linux/test_iptables.py
+++ neutron/tests/functional/agent/linux/test_iptables.py
@@ -24,7 +24,6 @@ from neutron.tests.common import machine_fixtures
 from neutron.tests.common import net_helpers
 from neutron.tests.functional.agent.linux import base as linux_base
 from neutron.tests.functional.agent.linux.bin import ipt_binname
-from neutron.tests.functional.agent.linux import helpers
 from neutron.tests.functional import base as functional_base
 
 
@@ -44,8 +43,8 @@ class IptablesManagerTestCase(functional_base.BaseSudoTestCase):
         self.client_fw, self.server_fw = self.create_firewalls()
         # The port is used in isolated namespace that precludes possibility of
         # port conflicts
-        self.port = helpers.get_free_namespace_port(constants.PROTO_NAME_TCP,
-                                                    self.server.namespace)
+        self.port = net_helpers.get_free_namespace_port(
+            constants.PROTO_NAME_TCP, self.server.namespace)
 
     def create_firewalls(self):
         client_iptables = iptables_manager.IptablesManager(
@@ -80,7 +79,7 @@ class IptablesManagerTestCase(functional_base.BaseSudoTestCase):
         return chain, rule
 
     def _test_with_nc(self, fw_manager, direction, port, protocol):
-        netcat = helpers.NetcatTester(
+        netcat = net_helpers.NetcatTester(
             self.client.namespace, self.server.namespace,
             self.server.ip, self.port, protocol)
         self.addCleanup(netcat.stop_processes)
@@ -121,35 +120,35 @@ class IptablesManagerTestCase(functional_base.BaseSudoTestCase):
 
     def test_tcp_input_port(self):
         self._test_with_nc(self.server_fw, 'ingress', self.port,
-                           protocol=helpers.NetcatTester.TCP)
+                           protocol=net_helpers.NetcatTester.TCP)
 
     def test_tcp_output_port(self):
         self._test_with_nc(self.client_fw, 'egress', self.port,
-                           protocol=helpers.NetcatTester.TCP)
+                           protocol=net_helpers.NetcatTester.TCP)
 
     def test_tcp_input(self):
         self._test_with_nc(self.server_fw, 'ingress', port=None,
-                           protocol=helpers.NetcatTester.TCP)
+                           protocol=net_helpers.NetcatTester.TCP)
 
     def test_tcp_output(self):
         self._test_with_nc(self.client_fw, 'egress', port=None,
-                           protocol=helpers.NetcatTester.TCP)
+                           protocol=net_helpers.NetcatTester.TCP)
 
     def test_udp_input_port(self):
         self._test_with_nc(self.server_fw, 'ingress', self.port,
-                           protocol=helpers.NetcatTester.UDP)
+                           protocol=net_helpers.NetcatTester.UDP)
 
     def test_udp_output_port(self):
         self._test_with_nc(self.client_fw, 'egress', self.port,
-                           protocol=helpers.NetcatTester.UDP)
+                           protocol=net_helpers.NetcatTester.UDP)
 
     def test_udp_input(self):
         self._test_with_nc(self.server_fw, 'ingress', port=None,
-                           protocol=helpers.NetcatTester.UDP)
+                           protocol=net_helpers.NetcatTester.UDP)
 
     def test_udp_output(self):
         self._test_with_nc(self.client_fw, 'egress', port=None,
-                           protocol=helpers.NetcatTester.UDP)
+                           protocol=net_helpers.NetcatTester.UDP)
 
 
 class IptablesManagerNonRootTestCase(base.BaseTestCase):
