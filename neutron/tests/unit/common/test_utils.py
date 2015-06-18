a4b17cbb80b48c8e4bcd7cc005139f8e66066d68
Cyril Roelandt <cyril@redhat.com>
2015-06-18 11:52:28 +0000

    Python3: do not use im_self/im_func/func_closure
    One should use __self__, __func__ and __closure__ instead, as they work
    with both Python 2 and 3.
    
    Change-Id: I2b2847cfd5b4fa70e45387ff369240227ce9e526
    Blueprint: neutron-python3

--- neutron/tests/unit/common/test_utils.py
+++ neutron/tests/unit/common/test_utils.py
@@ -418,7 +418,7 @@ class TestCachingDecorator(base.BaseTestCase):
         self.func_name = '%(module)s._CachingDecorator.func' % {
             'module': self.__module__
         }
-        self.not_cached = self.decor.func.func.im_self._not_cached
+        self.not_cached = self.decor.func.func.__self__._not_cached
 
     def test_cache_miss(self):
         expected_key = (self.func_name, 1, 2, ('foo', 'bar'))
