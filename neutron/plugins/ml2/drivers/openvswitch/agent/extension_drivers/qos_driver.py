3cbe446cb6db0ee18c928bb3ec1ff688db5febd4
Gal Sagie <gal.sagie@huawei.com>
2015-07-14 17:37:08 +0300

    Add unit tests and fixes for OVS Agent QoS Extension Driver
    Add basic unit tests and fix some of the issues while doing
    the tests
    
    blueprint ml2-ovs-qos-with-bwlimiting
    
    Change-Id: I3962dd2c0e1273905781faf3f5c51886dea21cd4

--- neutron/plugins/ml2/drivers/openvswitch/agent/extension_drivers/qos_driver.py
+++ neutron/plugins/ml2/drivers/openvswitch/agent/extension_drivers/qos_driver.py
@@ -28,7 +28,7 @@ class QosOVSAgentDriver(qos_agent.QosAgentDriver):
         super(QosOVSAgentDriver, self).__init__()
         # TODO(QoS) check if we can get this configuration
         #  as constructor arguments
-        self.br_int_name = cfg.CONF.ovs_integration_bridge
+        self.br_int_name = cfg.CONF.OVS.integration_bridge
         self.br_int = None
         self.handlers = {}
 
@@ -53,9 +53,9 @@ class QosOVSAgentDriver(qos_agent.QosAgentDriver):
 
     def _handle_rules(self, action, port, rules):
         for rule in rules:
-            handler = self.handlers.get((action, rule.get_type()))
+            handler = self.handlers.get((action, rule.get('type')))
             if handler is not None:
-                handler(port, rules)
+                handler(port, rule)
 
     def _update_bw_limit_rule(self, port, rule):
         port_name = port.get('name')
@@ -71,6 +71,9 @@ class QosOVSAgentDriver(qos_agent.QosAgentDriver):
                                                  max_kbps,
                                                  max_burst_kbps)
 
-    def _delete_bw_limit_rule(self, port):
+    def _delete_bw_limit_rule(self, port, rule):
         port_name = port.get('name')
-        self.br_int.del_qos_bw_limit_for_port(port_name)
+        current_max_kbps, current_max_burst = (
+            self.br_int.get_qos_bw_limit_for_port(port_name))
+        if current_max_kbps is not None or current_max_burst is not None:
+            self.br_int.del_qos_bw_limit_for_port(port_name)
