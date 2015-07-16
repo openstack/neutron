92c49549bdaa4a6d9e9ff6e9dd2cf9f3cf3ca07c
Jarod Du <ydu@lenovo.com>
2015-07-16 05:19:22 -0700

    Add Lenovo ML2 Mechanism Driver support for Lenovo switches

---
+++ neutron/plugins/ml2/drivers/lenovo/mech_lenovo_nos.py
@@ -0,0 +1,24 @@
+# Copyright 2013 OpenStack Foundation
+# All rights reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+ML2 Mechanism Driver for Lenovo NOS platforms.
+"""
+
+from networking_lenovo.ml2 import mech_lenovo_nos
+
+
+class LenovoNOSMechanismDriver(mech_lenovo_nos.LenovoNOSMechanismDriver):
+    pass
