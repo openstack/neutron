2ba2456f9dae2a4cf30804a562c08832c24b6231
Salvatore Orlando <salv.orlando@gmail.com>
2015-07-24 04:32:56 -0700

    Pecan WSGI: prevent plugins from opening AMQP connections
    The Pecan WSGI server is supposed to server REST requests only.
    As several plugins initialize their RPC interfaces upon initialization,
    this results in establishing undesired connections to the AMQP server.
    
    This patch does not alter plugin behavior or initialization process,
    but ensures that the setup_rpc operation performed by a plugin has no
    effect when executed in the Pecan WSGI server.
    
    This patch also performs some refactoring for server launchers.
    
    Change-Id: I56384f5f964ea90d72babf911aa5639989e9c3d8

---
+++ neutron/server/wsgi_eventlet.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import eventlet
+
+from oslo_log import log
+
+from neutron.i18n import _LI
+from neutron import server
+from neutron import service
+
+LOG = log.getLogger(__name__)
+
+
+def _eventlet_wsgi_server():
+    pool = eventlet.GreenPool()
+
+    neutron_api = service.serve_wsgi(service.NeutronApiService)
+    api_thread = pool.spawn(neutron_api.wait)
+
+    try:
+        neutron_rpc = service.serve_rpc()
+    except NotImplementedError:
+        LOG.info(_LI("RPC was already started in parent process by "
+                     "plugin."))
+    else:
+        rpc_thread = pool.spawn(neutron_rpc.wait)
+
+        # api and rpc should die together.  When one dies, kill the other.
+        rpc_thread.link(lambda gt: api_thread.kill())
+        api_thread.link(lambda gt: rpc_thread.kill())
+
+    pool.waitall()
+
+
+def main():
+    server.boot_server(_eventlet_wsgi_server)
