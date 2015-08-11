25420bb1bbccb037866872efdf1e5d3458d286a6
Doug Wiegley <dougw@a10networks.com>
2015-08-11 15:46:31 -0600

    Setup reference service providers for API test runs
    Partial-bug: #1483266
    Change-Id: Ia979fa2fdf25f3c1dbe9e7160d6dde2df51c305e

--- neutron/tests/contrib/gate_hook.sh
+++ neutron/tests/contrib/gate_hook.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
-
 set -ex
-
 
 VENV=${1:-"dsvm-functional"}
 
+GATE_DEST=$BASE/new
+DEVSTACK_PATH=$GATE_DEST/devstack
 
 if [ "$VENV" == "dsvm-functional" ] || [ "$VENV" == "dsvm-fullstack" ]
 then
     # The following need to be set before sourcing
     # configure_for_func_testing.
-    GATE_DEST=$BASE/new
     GATE_STACK_USER=stack
     NEUTRON_PATH=$GATE_DEST/neutron
     PROJECT_NAME=neutron
-    DEVSTACK_PATH=$GATE_DEST/devstack
     IS_GATE=True
 
     source $NEUTRON_PATH/tools/configure_for_func_testing.sh
@@ -26,10 +24,22 @@ then
     configure_host_for_func_testing
 elif [ "$VENV" == "api" ]
 then
-    if [[ -z "$DEVSTACK_LOCAL_CONFIG" ]]; then
-        export DEVSTACK_LOCAL_CONFIG="enable_plugin neutron-vpnaas git://git.openstack.org/openstack/neutron-vpnaas"
-    else
-        export DEVSTACK_LOCAL_CONFIG+=$'\n'"enable_plugin neutron-vpnaas git://git.openstack.org/openstack/neutron-vpnaas"
-    fi
+    cat > $DEVSTACK_PATH/local.conf <<EOF
+[[post-config|/etc/neutron/neutron_lbaas.conf]]
+
+[service_providers]
+service_provider=LOADBALANCER:Haproxy:neutron_lbaas.services.loadbalancer.drivers.haproxy.plugin_driver.HaproxyOnHostPluginDriver:default
+
+[[post-config|/etc/neutron/neutron_vpnaas.conf]]
+
+[service_providers]
+service_provider=VPN:openswan:neutron_vpnaas.services.vpn.service_drivers.ipsec.IPsecVPNDriver:default
+
+EOF
+
+    export DEVSTACK_LOCAL_CONFIG+="
+enable_plugin neutron-vpnaas git://git.openstack.org/openstack/neutron-vpnaas
+"
+
     $BASE/new/devstack-gate/devstack-vm-gate.sh
 fi
