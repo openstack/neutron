2fb89ceb6dd2edff50cd96ca5680a7f306655b04
Ihar Hrachyshka <ihrachys@redhat.com>
2015-09-11 14:46:51 -0400

    ovs: don't use ARP responder for IPv6 addresses
    ARP does not support IPv6 addresses, so when we try to apply the flow, it
    fails, with all other flows deferred for the same transaction. It results in
    random flow breakages, depending on the order of the bad flow in the
    transaction.
    
    Conflicts:
    	neutron/plugins/openvswitch/agent/ovs_neutron_agent.py
    	neutron/tests/unit/openvswitch/test_ovs_neutron_agent.py
    
    Change-Id: I0ecf167653e5a7d0916e091e05050406a026a1e2
    Co-Authored-By: Thomas Carroll <Thomas.Carroll@pnnl.gov>
    Closes-Bug: #1477253
    (cherry picked from commit a19e64c9d95781982d28113c667dbc90d0ea11eb)
    (cherry picked from commit 5923c223e670d799c43e77262bc0b29b84a067c7)

--- neutron/plugins/openvswitch/agent/ovs_neutron_agent.py
+++ neutron/plugins/openvswitch/agent/ovs_neutron_agent.py
@@ -432,8 +432,11 @@ class OVSNeutronAgent(n_rpc.RpcCallback,
         if not self.arp_responder_enabled:
             return
 
-        mac = netaddr.EUI(mac_address, dialect=netaddr.mac_unix)
         ip = netaddr.IPAddress(ip_address)
+        if ip.version == 6:
+            return
+
+        mac = netaddr.EUI(mac_address, dialect=netaddr.mac_unix)
 
         if action == 'add':
             actions = constants.ARP_RESPONDER_ACTIONS % {'mac': mac, 'ip': ip}
