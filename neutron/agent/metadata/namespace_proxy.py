13b6d76da361178ea0b84baf4831a4c7f53e29c7
Ihar Hrachyshka <ihrachys@redhat.com>
2015-10-01 17:13:25 +0200

    metadata: don't crash proxy on non-unicode user data
    We attempt to log every successful metadata response with LOG.debug. But
    as per oslo.log docs [1], we should make sure that what we pass into the
    library is unicode.
    
    Http.request returns a tuple of Response object and a string, which is
    bytes in Python 2.x [2].
    
    That's why we need to convert the response content to unicode before
    passing it into oslo.log.
    
    To achieve it, we utilize encodeutils.safe_decode with 'replace' errors
    handling strategy, so that we don't get exceptions on input that does
    not conform unicode.
    
    For the unit test case, we pass a string that is not expected to convert
    to unicode with errors='strict' strategy or similar, and check that we
    still don't crash.
    
    While at it, we remove a check for the number of log calls being
    triggered, because it's something that we should avoid validating in
    test cases, and it cannot trigger a real bug. The mock that was used to
    count the number would also hide the bug that we try to reproduce.
    
    Note that the bug does not require debug to be set because the crash
    occurs before oslo.log machinery decides it should not log the message.
    
    [1]: http://docs.openstack.org/developer/oslo.log/usage.html#no-more-implicit-conversion-to-unicode-str
    [2]: http://bitworking.org/projects/httplib2/doc/html/libhttplib2.html#httplib2.Http.request
    
    Closes-Bug: #1501772
    Change-Id: I6a32c40ff117fae43913386134c8981539697ce8
    (cherry picked from commit 80e3d9be4923ecad17377b1d15c8392b8a43dac6)

--- neutron/agent/metadata/namespace_proxy.py
+++ neutron/agent/metadata/namespace_proxy.py
@@ -15,6 +15,7 @@
 import httplib2
 from oslo_config import cfg
 from oslo_log import log as logging
+from oslo_utils import encodeutils
 import six
 import six.moves.urllib.parse as urlparse
 import webob
@@ -88,7 +89,7 @@ class NetworkMetadataProxyHandler(object):
 
         if resp.status == 200:
             LOG.debug(resp)
-            LOG.debug(content)
+            LOG.debug(encodeutils.safe_decode(content, errors='replace'))
             response = webob.Response()
             response.status = resp.status
             response.headers['Content-Type'] = resp['content-type']
