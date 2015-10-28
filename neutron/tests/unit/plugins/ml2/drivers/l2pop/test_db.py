2a27d7d25eb6718c8c97ece42d9fc53d57ebb817
Hong Hui Xiao <xiaohhui@cn.ibm.com>
2015-10-28 03:28:27 -0400

    Datapath on L2pop only for agents with tunneling-ip
    This patch is a regression issue for patch[1].
    
    Only those agents which expose tunneling-ip will be considered in
    determining data-path tunnels in deployments with l2pop ON.
    
    [1] https://review.openstack.org/#/c/236970/
    
    Change-Id: I7c3b911d5e7448b4e8dee15bb50df33a6e9d5cfe
    Closes-Bug: #1407959
    (cherry picked from commit 311c9d9d929b4a53d2f54824e599c12f653b77f6)

--- neutron/tests/unit/plugins/ml2/drivers/l2pop/test_db.py
+++ neutron/tests/unit/plugins/ml2/drivers/l2pop/test_db.py
@@ -14,7 +14,10 @@
 
 from neutron.common import constants
 from neutron import context
+from neutron.db import models_v2
+from neutron.extensions import portbindings
 from neutron.plugins.ml2.drivers.l2pop import db as l2pop_db
+from neutron.plugins.ml2 import models
 from neutron.tests.common import helpers
 from neutron.tests.unit import testlib_api
 
@@ -23,6 +26,7 @@ class TestL2PopulationDBTestCase(testlib_api.SqlTestCase):
     def setUp(self):
         super(TestL2PopulationDBTestCase, self).setUp()
         self.db_mixin = l2pop_db.L2populationDbMixin()
+        self.ctx = context.get_admin_context()
 
     def test_get_agent_by_host(self):
         # Register a L2 agent + A bunch of other agents on the same host
@@ -30,7 +34,7 @@ class TestL2PopulationDBTestCase(testlib_api.SqlTestCase):
         helpers.register_dhcp_agent()
         helpers.register_ovs_agent()
         agent = self.db_mixin.get_agent_by_host(
-            context.get_admin_context().session, helpers.HOST)
+            self.ctx.session, helpers.HOST)
         self.assertEqual(constants.AGENT_TYPE_OVS, agent.agent_type)
 
     def test_get_agent_by_host_no_candidate(self):
@@ -38,5 +42,73 @@ class TestL2PopulationDBTestCase(testlib_api.SqlTestCase):
         helpers.register_l3_agent()
         helpers.register_dhcp_agent()
         agent = self.db_mixin.get_agent_by_host(
-            context.get_admin_context().session, helpers.HOST)
+            self.ctx.session, helpers.HOST)
         self.assertIsNone(agent)
+
+    def _setup_port_binding(self, network_id='network_id', dvr=True):
+        with self.ctx.session.begin(subtransactions=True):
+            self.ctx.session.add(models_v2.Network(id=network_id))
+            device_owner = constants.DEVICE_OWNER_DVR_INTERFACE if dvr else ''
+            self.ctx.session.add(models_v2.Port(
+                id='port_id',
+                network_id=network_id,
+                mac_address='00:11:22:33:44:55',
+                admin_state_up=True,
+                status=constants.PORT_STATUS_ACTIVE,
+                device_id='',
+                device_owner=device_owner))
+            port_binding_cls = (models.DVRPortBinding if dvr
+                                else models.PortBinding)
+            binding_kwarg = {
+                'port_id': 'port_id',
+                'host': helpers.HOST,
+                'vif_type': portbindings.VIF_TYPE_UNBOUND,
+                'vnic_type': portbindings.VNIC_NORMAL
+            }
+            if dvr:
+                binding_kwarg['router_id'] = 'router_id'
+                binding_kwarg['status'] = constants.PORT_STATUS_DOWN
+
+            self.ctx.session.add(port_binding_cls(**binding_kwarg))
+
+    def test_get_dvr_active_network_ports(self):
+        self._setup_port_binding()
+        # Register a L2 agent + A bunch of other agents on the same host
+        helpers.register_l3_agent()
+        helpers.register_dhcp_agent()
+        helpers.register_ovs_agent()
+        tunnel_network_ports = self.db_mixin.get_dvr_active_network_ports(
+            self.ctx.session, 'network_id')
+        self.assertEqual(1, len(tunnel_network_ports))
+        _, agent = tunnel_network_ports[0]
+        self.assertEqual(constants.AGENT_TYPE_OVS, agent.agent_type)
+
+    def test_get_dvr_active_network_ports_no_candidate(self):
+        self._setup_port_binding()
+        # Register a bunch of non-L2 agents on the same host
+        helpers.register_l3_agent()
+        helpers.register_dhcp_agent()
+        tunnel_network_ports = self.db_mixin.get_dvr_active_network_ports(
+            self.ctx.session, 'network_id')
+        self.assertEqual(0, len(tunnel_network_ports))
+
+    def test_get_nondvr_active_network_ports(self):
+        self._setup_port_binding(dvr=False)
+        # Register a L2 agent + A bunch of other agents on the same host
+        helpers.register_l3_agent()
+        helpers.register_dhcp_agent()
+        helpers.register_ovs_agent()
+        fdb_network_ports = self.db_mixin.get_nondvr_active_network_ports(
+            self.ctx.session, 'network_id')
+        self.assertEqual(1, len(fdb_network_ports))
+        _, agent = fdb_network_ports[0]
+        self.assertEqual(constants.AGENT_TYPE_OVS, agent.agent_type)
+
+    def test_get_nondvr_active_network_ports_no_candidate(self):
+        self._setup_port_binding(dvr=False)
+        # Register a bunch of non-L2 agents on the same host
+        helpers.register_l3_agent()
+        helpers.register_dhcp_agent()
+        fdb_network_ports = self.db_mixin.get_nondvr_active_network_ports(
+            self.ctx.session, 'network_id')
+        self.assertEqual(0, len(fdb_network_ports))
