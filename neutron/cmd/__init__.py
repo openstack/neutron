a63c413f05c4b7ad8d622c8fe8b215531d38bf2b
Ihar Hrachyshka <ihrachys@redhat.com>
2015-11-26 17:17:43 +0100

    Add Guru Meditation Reports support to Neutron services
    This new feature allows services to dump valuable debug info to stderr
    on SIGUSR2 signal.
    
    Change-Id: If17b57f687558e23b830eae5ee1f8900356355e4
    Implements: blueprint guru-meditation-report

---
+++ neutron/cmd/__init__.py
@@ -0,0 +1,20 @@
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo_reports import guru_meditation_report as gmr
+
+from neutron import version
+
+_version_string = version.version_info.release_string()
+gmr.TextGuruMeditation.setup_autorun(version=_version_string)
