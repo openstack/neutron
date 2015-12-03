1382da468faa454cb7d48365c1960bbe10f506a7
Andreas Scheuring <andreas.scheuring@de.ibm.com>
2015-12-03 14:54:39 +0100

    Correct return values for bridge sysctl calls
    This fixes an issue where the lb agent did not plug the
    dhcp tap device into the bridge when having vlan networking
    set up.  Caused by setting of disable_ipv6 value.
    
    Conflicts:
    	neutron/agent/linux/bridge_lib.py
    	neutron/tests/functional/agent/linux/test_bridge_lib.py
    	neutron/tests/unit/agent/linux/test_bridge_lib.py
    
    Closes-Bug: #1520618
    Change-Id: I0d21fad3a676d1fdd30501ea6a295f1e9b207a3a
    Co-Authored-By: Brian Haley <brian.haley@hpe.com>
    (cherry picked from commit cac2436f298491dbca2c932c80bdf3a64ac39ee6)

--- neutron/tests/unit/agent/linux/test_bridge_lib.py
+++ neutron/tests/unit/agent/linux/test_bridge_lib.py
@@ -36,6 +36,11 @@ class BridgeLibTest(base.BaseTestCase):
         self.execute.assert_called_once_with(cmd, run_as_root=True)
         self.execute.reset_mock()
 
+    def _verify_bridge_mock_check_exit_code(self, cmd):
+        self.execute.assert_called_once_with(cmd, run_as_root=True,
+                                             check_exit_code=True)
+        self.execute.reset_mock()
+
     def _test_br(self, namespace=None):
         br = bridge_lib.BridgeDevice.addbr(self._BR_NAME, namespace)
         self.assertEqual(namespace, br.namespace)
@@ -49,7 +54,7 @@ class BridgeLibTest(base.BaseTestCase):
 
         br.disable_ipv6()
         cmd = 'net.ipv6.conf.%s.disable_ipv6=1' % self._BR_NAME
-        self._verify_bridge_mock(['sysctl', '-w', cmd])
+        self._verify_bridge_mock_check_exit_code(['sysctl', '-w', cmd])
 
         br.addif(self._IF_NAME)
         self._verify_bridge_mock(
