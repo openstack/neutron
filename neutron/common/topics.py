7f9e654843db5b515fca9e33c76ee8c7edd76d93
Paul Michali <pc@michali.net>
2015-12-07 15:05:35 -0500

    Removing adv svcs dependencies on neutron
    After upstreaming Idcfeee6ddc2982fff49f44064c918bfd4627be12 and
    I62bfd6b34c381427425ca2a656e5782087de3a3b, we can remove items
    from Neutron.
    
    Specifically, LOADBALANCER_PLUGIN and LOADBALANCER_AGENT are not
    used now. FIREWALL_PLUGIN is used in one module, so moved there.
    The LOADBALANCER constant is only used in test files, but it is
    used in multiple files, so leaving it. LOADBALANCERV2 is only used
    in the header it is defined, so leaving it as well.
    
    In the FW and LB repos, there are no longer dependency on these literals.
    
    Change-Id: I7f0b972e1ae7ef435a3612e239dad7b2c04baea2
    Implements: blueprint neutron-lib

--- neutron/common/topics.py
+++ neutron/common/topics.py
@@ -30,14 +30,11 @@ PLUGIN = 'q-plugin'
 L3PLUGIN = 'q-l3-plugin'
 REPORTS = 'q-reports-plugin'
 DHCP = 'q-dhcp-notifer'
-FIREWALL_PLUGIN = 'q-firewall-plugin'
 METERING_PLUGIN = 'q-metering-plugin'
-LOADBALANCER_PLUGIN = 'n-lbaas-plugin'
 
 L3_AGENT = 'l3_agent'
 DHCP_AGENT = 'dhcp_agent'
 METERING_AGENT = 'metering_agent'
-LOADBALANCER_AGENT = 'n-lbaas_agent'
 
 RESOURCE_TOPIC_PATTERN = "neutron-vo-%(resource_type)s-%(version)s"
 
