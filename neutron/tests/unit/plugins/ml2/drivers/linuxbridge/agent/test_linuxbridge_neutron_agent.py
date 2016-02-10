bbd881f3a970143e1954cb277e5235dddd26c5d0
Mark McClain <mark@mcclain.xyz>
2016-02-10 13:28:21 -0500

    add arp_responder flag to linuxbridge agent
    When the ARP responder is enabled, secondary IP addresses explicitly
    allowed by via the allowed-address-pairs extensions do not resolve.
    This change adds the ability to enable the local ARP responder similar
    to the feature in the OVS agent.  This change disables local ARP
    responses by default, so ARP traffic will be sent over the overlay.
    
    DocImpact
    UpgradeImpact
    
    Change-Id: I5da4afa44fc94032880ea59ec574df504470fb4a
    Closes-Bug: 1445089

--- neutron/tests/unit/plugins/ml2/drivers/linuxbridge/agent/test_linuxbridge_neutron_agent.py
+++ neutron/tests/unit/plugins/ml2/drivers/linuxbridge/agent/test_linuxbridge_neutron_agent.py
@@ -333,7 +333,7 @@ class TestLinuxBridgeManager(base.BaseTestCase):
                 self.assertIsNone(self.lbm.ensure_vlan("eth0", "1"))
                 self.assertEqual(3, exec_fn.call_count)
 
-    def test_ensure_vxlan(self):
+    def test_ensure_vxlan(self, expected_proxy=False):
         seg_id = "12345678"
         self.lbm.local_int = 'eth0'
         self.lbm.vxlan_mode = lconst.VXLAN_MCAST
@@ -355,7 +355,11 @@ class TestLinuxBridgeManager(base.BaseTestCase):
                 add_vxlan_fn.assert_called_with("vxlan-" + seg_id, seg_id,
                                                 group="224.0.0.1",
                                                 dev=self.lbm.local_int,
-                                                proxy=True)
+                                                proxy=expected_proxy)
+
+    def test_ensure_vxlan_arp_responder_enabled(self):
+        cfg.CONF.set_override('arp_responder', True, 'VXLAN')
+        self.test_ensure_vxlan(expected_proxy=True)
 
     def test_update_interface_ip_details(self):
         gwdict = dict(gateway='1.1.1.1',
