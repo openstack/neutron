67b621de73339d28dedbea7700897784dd4edd6a
Brent Eagles <beagles@redhat.com>
2016-02-10 11:17:41 -0330

    Data models for vlan-aware-vms
    This patch introduces the following:
    
    - data models and related schema migrations
    - first stub at DB operations
    - trunk module structure
    
    This is a tepid attempt to land the first functional code
    for this sorely needed feature.
    
    Partially-implements: blueprint vlan-aware-vms
    
    Change-Id: I84b2a7604b6a282ec7cb953482bf7b567ae2344d

---
+++ neutron/db/migration/alembic_migrations/versions/newton/expand/5abc0278ca73_add_support_for_vlan_trunking.py
@@ -0,0 +1,49 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+revision = '5abc0278ca73'
+down_revision = '45f8dd33480b'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table('trunks',
+        sa.Column('tenant_id', sa.String(length=255), nullable=True,
+                  index=True),
+        sa.Column('id', sa.String(length=36), nullable=False),
+        sa.Column('port_id', sa.String(length=36), nullable=False),
+        sa.Column('standard_attr_id', sa.BigInteger(), nullable=False),
+        sa.ForeignKeyConstraint(['port_id'], ['ports.id'],
+                                ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(['standard_attr_id'],
+                                ['standardattributes.id'],
+                                ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('id'),
+        sa.UniqueConstraint('port_id'),
+        sa.UniqueConstraint('standard_attr_id')
+    )
+    op.create_table('subports',
+        sa.Column('port_id', sa.String(length=36), nullable=False),
+        sa.Column('trunk_id', sa.String(length=36), nullable=False),
+        sa.Column('segmentation_type', sa.String(length=32), nullable=False),
+        sa.Column('segmentation_id', sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(['port_id'], ['ports.id'], ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(['trunk_id'], ['trunks.id'],
+                                ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('port_id', 'trunk_id'),
+        sa.UniqueConstraint('port_id'),
+        sa.UniqueConstraint('trunk_id', 'segmentation_type', 'segmentation_id',
+            name='uniq_subport0trunk_id0segmentation_type0segmentation_id')
+    )
