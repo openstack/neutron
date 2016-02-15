d596ceef3bce77ac3cc4821bae046509af69b996
Gary Kotton <gkotton@vmware.com>
2016-02-15 00:49:14 -0800

    Remove deprecation warnings
        ../neutron/extensions/bgp.py:113: DeprecationWarning: Using
        function/method 'instance.ugettext()' is deprecated: Builtin
        _ translation function is deprecated in OpenStack; use the
        function from _i18n module for your project.
    
          message = _("BGP speaker %(id)s could not be found.")
    
    TrivialFix
    
    Change-Id: I63dc5199c7560329e8df55a0fbf7855e92e49c31

--- neutron/extensions/bgp.py
+++ neutron/extensions/bgp.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 #
 
+from neutron._i18n import _
 from neutron.api import extensions
 from neutron.api.v2 import attributes as attr
 from neutron.api.v2 import resource_helper as rh
