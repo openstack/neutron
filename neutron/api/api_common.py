49609f36f5b4b48f7d6cf2c5bbae65eab1a59876
Gary Kotton <gkotton@vmware.com>
2016-06-13 06:43:26 -0700

    Remove deprecated class NeutronController
    Commit 9b8bf7d25c4dda41b78946ce7a3c0ab63602e834 marked
    NeutronController as deprecated.
    
    This code is no longer used in Neutron and there is no need for
    the class nor the unit tests for it.
    
    TrivialFix
    
    Closes-bug: #1625140
    
    Change-Id: Ie02b6f1a45c3ea32a1116a48946c59673e2d2472

--- neutron/api/api_common.py
+++ neutron/api/api_common.py
@@ -15,7 +15,6 @@
 
 import functools
 
-from debtcollector import removals
 import netaddr
 from neutron_lib import exceptions
 from oslo_config import cfg
@@ -341,46 +340,6 @@ class SortingNativeHelper(SortingHelper):
 
 class NoSortingHelper(SortingHelper):
     pass
-
-
-@removals.removed_class('NeutronController',
-                        version='newton',
-                        removal_version='ocata')
-class NeutronController(object):
-    """Base controller class for Neutron API."""
-    # _resource_name will be redefined in sub concrete controller
-    _resource_name = None
-
-    def __init__(self, plugin):
-        self._plugin = plugin
-        super(NeutronController, self).__init__()
-
-    def _prepare_request_body(self, body, params):
-        """Verifies required parameters are in request body.
-
-        Sets default value for missing optional parameters.
-        Body argument must be the deserialized body.
-        """
-        try:
-            if body is None:
-                # Initialize empty resource for setting default value
-                body = {self._resource_name: {}}
-            data = body[self._resource_name]
-        except KeyError:
-            # raise if _resource_name is not in req body.
-            raise exc.HTTPBadRequest(_("Unable to find '%s' in request body") %
-                                     self._resource_name)
-        for param in params:
-            param_name = param['param-name']
-            param_value = data.get(param_name)
-            # If the parameter wasn't found and it was required, return 400
-            if param_value is None and param['required']:
-                msg = (_("Failed to parse request. "
-                         "Parameter '%s' not specified") % param_name)
-                LOG.error(msg)
-                raise exc.HTTPBadRequest(msg)
-            data[param_name] = param_value or param.get('default-value')
-        return body
 
 
 def convert_exception_to_http_exc(e, faults, language):
