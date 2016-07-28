bfed1675f7819c47211d11e5fa7fd7c447b55db4
Armando Migliaccio <armamig@gmail.com>
2016-07-28 09:41:41 -0700

    Remove local subports validator
    neutron-lib 0.3.0 is released and it includes the required validator.
    
    Change-Id: I4b252ff0f4d8a4d7c8ca54f5704d81fb94dff31e

--- neutron/extensions/trunk.py
+++ neutron/extensions/trunk.py
@@ -13,74 +13,11 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import collections
-
-from oslo_log import log as logging
-
 from neutron_lib.api import converters
-from neutron_lib.api import validators
 
-from neutron._i18n import _
 from neutron.api import extensions
 from neutron.api.v2 import attributes as attr
 from neutron.api.v2 import resource_helper
-
-LOG = logging.getLogger(__name__)
-
-
-# TODO(armax): this validator was introduced in neutron-lib in
-# https://review.openstack.org/#/c/319386/; remove it as soon
-# as there is a new release.
-def validate_subports(data, valid_values=None):
-    if not isinstance(data, list):
-        msg = _("Invalid data format for subports: '%s' is not a list") % data
-        LOG.debug(msg)
-        return msg
-
-    subport_ids = set()
-    segmentations = collections.defaultdict(set)
-    for subport in data:
-        if not isinstance(subport, dict):
-            msg = _("Invalid data format for subport: "
-                    "'%s' is not a dict") % subport
-            LOG.debug(msg)
-            return msg
-
-        # Expect a non duplicated and valid port_id for the subport
-        if 'port_id' not in subport:
-            msg = _("A valid port UUID must be specified")
-            LOG.debug(msg)
-            return msg
-        elif validators.validate_uuid(subport["port_id"]):
-            msg = _("Invalid UUID for subport: '%s'") % subport["port_id"]
-            return msg
-        elif subport["port_id"] in subport_ids:
-            msg = _("Non unique UUID for subport: '%s'") % subport["port_id"]
-            return msg
-        subport_ids.add(subport["port_id"])
-
-        # Validate that both segmentation id and segmentation type are
-        # specified, and that the client does not duplicate segmentation
-        # ids
-        segmentation_id = subport.get("segmentation_id")
-        segmentation_type = subport.get("segmentation_type")
-        if (not segmentation_id or not segmentation_type) and len(subport) > 1:
-            msg = _("Invalid subport details '%s': missing segmentation "
-                    "information. Must specify both segmentation_id and "
-                    "segmentation_type") % subport
-            LOG.debug(msg)
-            return msg
-        if segmentation_id in segmentations.get(segmentation_type, []):
-            msg = _("Segmentation ID '%(seg_id)s' for '%(subport)s' is not "
-                    "unique") % {"seg_id": segmentation_id,
-                                 "subport": subport["port_id"]}
-            LOG.debug(msg)
-            return msg
-        if segmentation_id:
-            segmentations[segmentation_type].add(segmentation_id)
-
-
-validators.validators['type:subports'] = validate_subports
 
 
 RESOURCE_ATTRIBUTE_MAP = {
