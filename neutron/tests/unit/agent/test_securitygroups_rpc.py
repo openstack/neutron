42b209a6f9918c9fe8a96462b85278569a1cc85d
Hong Hui Xiao <honghui_xiao@yeah.net>
2016-08-20 08:09:54 -0400

    Fix security_groups_provider_updated for linuxbridge
    According to [1], security_groups_provider_updated will get port ids
    to update the related security groups.
    
    However, in the firewall of l2 agent, ports are identified by 'device'
    attribute. This is fine for ovs agent, because, for ovs agent, port id
    is the same as 'device' attribute.
    
    But for other agent, like linuxbridge agent, the 'device' attribute of
    port will be the device name. This will make l2 agent can't find port
    and fail to update the security group of port. The info message at [2]
    can be observed.
    
    The fix is to convert port id list to 'device' list.
    
    [1] http://git.openstack.org/cgit/openstack/neutron/tree/neutron/db/securitygroups_rpc_base.py#n157
    [2] http://git.openstack.org/cgit/openstack/neutron/tree/neutron/agent/linux/iptables_firewall.py#n177
    
    Change-Id: Idf9bd7942ef2063cfbc5e9eeead8d49d09883ea8
    Closes-Bug: #1555027

--- neutron/tests/unit/agent/test_securitygroups_rpc.py
+++ neutron/tests/unit/agent/test_securitygroups_rpc.py
@@ -1220,7 +1220,7 @@ class SecurityGroupAgentRpcTestCase(BaseSecurityGroupAgentRpcTestCase):
         self.agent.refresh_firewall = mock.Mock()
         self.agent.security_groups_provider_updated(None)
         self.agent.refresh_firewall.assert_has_calls(
-            [mock.call.refresh_firewall(None)])
+            [mock.call.refresh_firewall()])
 
     def test_refresh_firewall(self):
         self.agent.prepare_devices_filter(['fake_port_id'])
@@ -1344,7 +1344,7 @@ class SecurityGroupAgentEnhancedRpcTestCase(
         self.agent.refresh_firewall = mock.Mock()
         self.agent.security_groups_provider_updated(None)
         self.agent.refresh_firewall.assert_has_calls(
-            [mock.call.refresh_firewall(None)])
+            [mock.call.refresh_firewall()])
 
     def test_refresh_firewall_enhanced_rpc(self):
         self.agent.prepare_devices_filter(['fake_port_id'])
@@ -1489,8 +1489,15 @@ class SecurityGroupAgentRpcWithDeferredRefreshTestCase(
         self.assertTrue(self.agent.global_refresh_firewall)
 
     def test_security_groups_provider_updated_devices_specified(self):
+        self.agent.firewall.ports = {
+            'fake_device_1': {
+                'id': 'fake_port_id_1',
+                'device': 'fake_device_1'},
+            'fake_device_2': {
+                'id': 'fake_port_id_2',
+                'device': 'fake_device_2'}}
         self.agent.security_groups_provider_updated(
-            ['fake_device_1', 'fake_device_2'])
+            ['fake_port_id_1', 'fake_port_id_2'])
         self.assertFalse(self.agent.global_refresh_firewall)
         self.assertIn('fake_device_1', self.agent.devices_to_refilter)
         self.assertIn('fake_device_2', self.agent.devices_to_refilter)
