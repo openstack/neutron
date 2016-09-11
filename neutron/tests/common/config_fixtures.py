bdfa3c9400afa67053cb20490bce73cdb828b063
lilintan <lintan.li@easystack.cn>
2016-09-11 16:25:15 +0800

    Use ConfigParser instead of SafeConfigParser
    SafeConfigParser supports interpolation on top of ConfigParser in
    Python 2, and SafeConfigParser is deprecated in Python 3.2 and log
    warning like " DeprecationWarning: The SafeConfigParser class has
    been renamed to ConfigParser in Python 3.2. This alias will be removed
    in future versions. Use ConfigParser directly instead." So we can use
    ConfigParser if we don't need interpolation.
    
    Closes-Bug: #1618666
    Change-Id: Ie5d38b86bb2ba3c83c7eb541de0389c6e4dcf654

--- neutron/tests/common/config_fixtures.py
+++ neutron/tests/common/config_fixtures.py
@@ -60,7 +60,7 @@ class ConfigFileFixture(fixtures.Fixture):
             f.flush()
 
     def dict_to_config_parser(self, config_dict):
-        config_parser = six.moves.configparser.SafeConfigParser()
+        config_parser = six.moves.configparser.ConfigParser()
         for section, section_dict in six.iteritems(config_dict):
             if section != 'DEFAULT':
                 config_parser.add_section(section)
