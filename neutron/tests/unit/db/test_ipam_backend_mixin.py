748faa0579056c0157d038e539610f386dd64abe
Kevin Benton <kevin@benton.pub>
2016-09-14 20:37:56 -0700

    Revert "Don't allocate IP on port update when existing subnet specified"
    This reverts commit f07c07b16fb0858c45f6cef135a8d8c07a16c505.
    This broke the ability to add fixed IPs to a port based on
    subnet_id.
    
    API test to prevent regression in child patch.
    
    Change-Id: Ia13abea59431744ce7a0270f480f4bf61a7161e0
    Closes-Bug: #1623800

--- neutron/tests/unit/db/test_ipam_backend_mixin.py
+++ neutron/tests/unit/db/test_ipam_backend_mixin.py
@@ -167,51 +167,6 @@ class TestIpamBackendMixin(base.BaseTestCase):
         self._mock_slaac_subnet_on()
         self._test_get_changed_ips_for_port_no_ip_address()
 
-    def test__get_changed_ips_for_port_subnet_id_no_ip(self):
-        # If a subnet is specified without an IP address only allocate a new
-        # address if one doesn't exist
-        self._mock_slaac_subnet_off()
-        new_ips = [{'subnet_id': 'id-3'}]
-        original_ips = [{'subnet_id': 'id-3', 'ip_address': '4.3.2.1'}]
-
-        expected_change = self.mixin.Changes(
-            add=[],
-            original=[{'subnet_id': 'id-3', 'ip_address': '4.3.2.1'}],
-            remove=[])
-        self._test_get_changed_ips_for_port(expected_change, original_ips,
-                                            new_ips, self.owner_non_router)
-
-    def test__get_changed_ips_for_port_subnet_id_no_ip_ipv6(self):
-        # If a subnet is specified without an IP address only allocate a new
-        # address if one doesn't exist
-        self._mock_slaac_subnet_off()
-        new_ips = [{'subnet_id': 'id-3'}]
-        original_ips = [{'subnet_id': 'id-3', 'ip_address': '2001:db8::8'}]
-
-        expected_change = self.mixin.Changes(
-            add=[],
-            original=[{'subnet_id': 'id-3', 'ip_address': '2001:db8::8'}],
-            remove=[])
-        self._test_get_changed_ips_for_port(expected_change, original_ips,
-                                            new_ips, self.owner_non_router)
-
-    def test__get_changed_ips_for_port_subnet_id_no_ip_eui64(self):
-        # If a subnet is specified without an IP address allocate a new address
-        # if the address is eui-64. This supports changing prefix when prefix
-        # delegation is in use.
-        self._mock_slaac_subnet_off()
-        new_ips = [{'subnet_id': 'id-3'}]
-        original_ips = [{'subnet_id': 'id-3',
-                         'ip_address': '2001::eeb1:d7ff:fe2c:9c5f'}]
-
-        expected_change = self.mixin.Changes(
-            add=[{'subnet_id': 'id-3'}],
-            original=[],
-            remove=[{'subnet_id': 'id-3',
-                     'ip_address': '2001::eeb1:d7ff:fe2c:9c5f'}])
-        self._test_get_changed_ips_for_port(expected_change, original_ips,
-                                            new_ips, self.owner_non_router)
-
     def test__is_ip_required_by_subnet_for_router_port(self):
         # Owner -> router:
         # _get_subnet should not be called,
