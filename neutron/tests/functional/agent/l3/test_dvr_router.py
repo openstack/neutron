d40322c7d4aa1dd6d595dfe415278c9f252f4da2
Swaminathan Vasudevan <swaminathan.vasudevan@hpe.com>
2016-10-07 10:30:40 -0700

    DVR: Fix race condition in creation of fip gateway
    In large-scale environments, we have seen a router update
    arrive for one tenant while we are still creating the
    router for a different tenant and initializing the shared
    floating IP gateway port.  Sometimes these updates can
    get scheduled simultaneously, with the second running
    before we are done creating all the resources in the
    first, causing an exception when trying to set the
    default route since either the interface or IP address
    does not exist yet.
    
    Add a lock to better synchronize these functions so
    a create can finish before an update can be done.
    
    If it still fails, we will throw an exception so that
    the namespace will be cleaned-up and the update can be
    re-scheduled for the next iteration.
    
    Closes-Bug: #1631513
    Change-Id: Ia8c92cea2f8798582c39ad3450ab3b3c45a356f7

--- neutron/tests/functional/agent/l3/test_dvr_router.py
+++ neutron/tests/functional/agent/l3/test_dvr_router.py
@@ -28,6 +28,7 @@ from neutron.agent.l3 import namespaces
 from neutron.agent.linux import ip_lib
 from neutron.agent.linux import iptables_manager
 from neutron.common import constants as n_const
+from neutron.common import exceptions as n_exc
 from neutron.common import utils
 from neutron.extensions import portbindings
 from neutron.tests.common import l3_test_common
@@ -110,6 +111,89 @@ class TestDvrRouter(framework.L3AgentTestFramework):
         self.assertEqual(3, default_rules_list_count)
         self.assertEqual(2, interface_rules_list_count)
 
+    def test_dvr_update_gateway_port_with_no_gw_port_in_namespace(self):
+        self.agent.conf.agent_mode = 'dvr'
+
+        # Create the router with external net
+        router_info = self.generate_dvr_router_info()
+        external_gw_port = router_info['gw_port']
+        ext_net_id = router_info['_floatingips'][0]['floating_network_id']
+        self.mock_plugin_api.get_external_network_id.return_value = ext_net_id
+        router = self.manage_router(self.agent, router_info)
+        fg_port = router.fip_ns.agent_gateway_port
+        fg_port_name = router.fip_ns.get_ext_device_name(fg_port['id'])
+        fg_device = ip_lib.IPDevice(fg_port_name,
+                                    namespace=router.fip_ns.name)
+        # Now validate if the gateway is properly configured.
+        self.assertIn('gateway', fg_device.route.get_gateway())
+        self._validate_fips_for_external_network(
+            router, router.fip_ns.get_name())
+        # Now delete the fg- port that was created
+        ext_net_bridge = self.agent.conf.external_network_bridge
+        router.fip_ns.driver.unplug(fg_port_name,
+                                    bridge=ext_net_bridge,
+                                    namespace=router.fip_ns.name,
+                                    prefix=dvr_fip_ns.FIP_EXT_DEV_PREFIX)
+        # Now check if the fg- port is missing.
+        self.assertFalse(fg_device.exists())
+        # Now change the gateway ip for the router and do an update.
+        router.ex_gw_port = copy.deepcopy(router.ex_gw_port)
+        new_fg_port = copy.deepcopy(fg_port)
+        for subnet in new_fg_port['subnets']:
+            subnet['gateway_ip'] = '19.4.4.2'
+        router.router[n_const.FLOATINGIP_AGENT_INTF_KEY] = [new_fg_port]
+        self.assertRaises(n_exc.FloatingIpSetupException,
+                          self.manage_router,
+                          self.agent,
+                          router.router)
+        router = self.manage_router(self.agent, router.router)
+        self.assertTrue(fg_device.exists())
+        self.assertEqual({'gateway': u'19.4.4.2'},
+                         fg_device.route.get_gateway())
+        self._validate_fips_for_external_network(
+            router, router.fip_ns.get_name())
+        self._delete_router(self.agent, router.router_id)
+        self._assert_fip_namespace_deleted(external_gw_port)
+
+    @mock.patch.object(dvr_fip_ns.FipNamespace, 'subscribe')
+    def test_dvr_process_fips_with_no_gw_port_in_namespace(
+        self, fip_subscribe):
+        self.agent.conf.agent_mode = 'dvr'
+
+        # Create the router with external net
+        router_info = self.generate_dvr_router_info()
+        external_gw_port = router_info['gw_port']
+        ext_net_id = router_info['_floatingips'][0]['floating_network_id']
+        self.mock_plugin_api.get_external_network_id.return_value = ext_net_id
+
+        # Create the fip namespace up front
+        dvr_fip_ns.FipNamespace(ext_net_id,
+                                self.agent.conf,
+                                self.agent.driver,
+                                self.agent.use_ipv6).create()
+        # Create the router with the fip, this shouldn't allow the
+        # update_gateway_port to be called without the fg- port
+        fip_subscribe.return_value = False
+        # This will raise the exception and will also clear
+        # subscription for the ext_net_id
+        self.assertRaises(n_exc.FloatingIpSetupException,
+                          self.manage_router,
+                          self.agent,
+                          router_info)
+        fip_subscribe.return_value = True
+        # Now update the router again
+        router = self.manage_router(self.agent, router_info)
+        fg_port = router.fip_ns.agent_gateway_port
+        fg_port_name = router.fip_ns.get_ext_device_name(fg_port['id'])
+        fg_device = ip_lib.IPDevice(fg_port_name,
+                                    namespace=router.fip_ns.name)
+        # Now validate if the gateway is properly configured.
+        self.assertIn('gateway', fg_device.route.get_gateway())
+        self._validate_fips_for_external_network(
+            router, router.fip_ns.get_name())
+        self._delete_router(self.agent, router.router_id)
+        self._assert_fip_namespace_deleted(external_gw_port)
+
     def test_dvr_router_fips_stale_gw_port(self):
         self.agent.conf.agent_mode = 'dvr'
 
@@ -148,7 +232,7 @@ class TestDvrRouter(framework.L3AgentTestFramework):
                            'prefixlen': prefixlen}],
             'id': framework._uuid(),
             'device_id': framework._uuid()}
-        stale_fip_ns.create_gateway_port(stale_agent_gw_port)
+        stale_fip_ns.create_or_update_gateway_port(stale_agent_gw_port)
 
         stale_dev_exists = self.device_exists_with_ips_and_mac(
                 stale_agent_gw_port,
