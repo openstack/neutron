fef33d7857039a874e4db7fb4c6c654167be0c79
Gary Kotton <gkotton@vmware.com>
2016-10-09 05:58:59 -0700

    ipv6_utils: delete get_ipv6_addr_by_EUI64
    Commit 7913798a2cc6717fa3e586b044801e7266b3ffc3 in Newton. We now
    delete the method.
    
    TrivialFix
    
    Change-Id: Ied18b631f3693ee81cd1d3f8fef4b54ca3536ab7

--- neutron/tests/unit/common/test_ipv6_utils.py
+++ neutron/tests/unit/common/test_ipv6_utils.py
@@ -22,39 +22,6 @@ from neutron.tests import base
 from neutron.tests import tools
 
 
-class IPv6byEUI64TestCase(base.BaseTestCase):
-    """Unit tests for generate IPv6 by EUI-64 operations."""
-
-    def test_generate_IPv6_by_EUI64(self):
-        addr = ipv6_utils.get_ipv6_addr_by_EUI64('2001:db8::',
-                                                 '00:16:3e:33:44:55')
-        self.assertEqual('2001:db8::216:3eff:fe33:4455', addr.format())
-
-    def test_generate_IPv6_with_IPv4_prefix(self):
-        ipv4_prefix = '10.0.8'
-        mac = '00:16:3e:33:44:55'
-        self.assertRaises(TypeError, lambda:
-                          ipv6_utils.get_ipv6_addr_by_EUI64(ipv4_prefix, mac))
-
-    def test_generate_IPv6_with_bad_mac(self):
-        bad_mac = '00:16:3e:33:44:5Z'
-        prefix = '2001:db8::'
-        self.assertRaises(TypeError, lambda:
-                          ipv6_utils.get_ipv6_addr_by_EUI64(prefix, bad_mac))
-
-    def test_generate_IPv6_with_bad_prefix(self):
-        mac = '00:16:3e:33:44:55'
-        bad_prefix = 'bb'
-        self.assertRaises(TypeError, lambda:
-                          ipv6_utils.get_ipv6_addr_by_EUI64(bad_prefix, mac))
-
-    def test_generate_IPv6_with_error_prefix_type(self):
-        mac = '00:16:3e:33:44:55'
-        prefix = 123
-        self.assertRaises(TypeError, lambda:
-                          ipv6_utils.get_ipv6_addr_by_EUI64(prefix, mac))
-
-
 class TestIsEnabledAndBindByDefault(base.BaseTestCase):
 
     def setUp(self):
@@ -133,12 +100,6 @@ class TestIsEui64Address(base.BaseTestCase):
         for ip in ips:
             self.assertEqual(expected, ipv6_utils.is_eui64_address(ip),
                              "Error on %s" % ip)
-
-    def test_valid_eui64_addresses(self):
-        ips = ('fffe::0cad:12ff:fe44:5566',
-               ipv6_utils.get_ipv6_addr_by_EUI64('2001:db8::',
-                                                 '00:16:3e:33:44:55'))
-        self._test_eui_64(ips, True)
 
     def test_invalid_eui64_addresses(self):
         ips = ('192.168.1.1',
