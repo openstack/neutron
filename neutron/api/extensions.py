fa04ab0fcf7de7acd51ddfb1284d5a32d23f4e8b
Shaohe Feng <shaohe.feng@intel.com>
2016-11-08 04:38:54 +0800

    tiny refine: capitalize mod_name with string capitalize function
    string capitalize can return a copy of the string S with only its first
    character capitalized.
    
    Change-Id: If76b63e0b6c1c9ab957afdf66d3e7c76367afb2c

--- neutron/api/extensions.py
+++ neutron/api/extensions.py
@@ -455,7 +455,7 @@ class ExtensionManager(object):
                 ext_path = os.path.join(path, f)
                 if file_ext.lower() == '.py' and not mod_name.startswith('_'):
                     mod = imp.load_source(mod_name, ext_path)
-                    ext_name = mod_name[0].upper() + mod_name[1:]
+                    ext_name = mod_name.capitalize()
                     new_ext_class = getattr(mod, ext_name, None)
                     if not new_ext_class:
                         LOG.warning(_LW('Did not find expected name '
