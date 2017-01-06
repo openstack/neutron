ffc4489a585294a5a13765ab9520cfe9694f02c2
Kevin Benton <kevin@benton.pub>
2017-01-06 14:06:11 -0800

    Don't create default SG in transaction
    Creating default security groups should not be done inside
    another transaction because the creation code emits AFTER events.
    Handlers for the push notification code expect to be able to read
    from the DB during an after event using a new transaction, which will
    not work if they are inside of exising transactions.
    
    This patch adjusts the default security group creation logic to avoid
    using layered transactions for the default security group creation and
    adjusts ML2 to stop calling ensure_default_security_group inside of a
    port/network transaction.
    
    Partially-Implements: blueprint push-notifications
    Change-Id: Iaa83c8664d5bfde31fdcdd694f6f18d9ef9bf14a

--- neutron/tests/unit/plugins/ml2/test_security_group.py
+++ neutron/tests/unit/plugins/ml2/test_security_group.py
@@ -20,6 +20,9 @@ import mock
 from neutron_lib import constants as const
 from neutron_lib.plugins import directory
 
+from neutron.callbacks import events
+from neutron.callbacks import registry
+from neutron.callbacks import resources
 from neutron import context
 from neutron.extensions import securitygroup as ext_sg
 from neutron.tests import tools
@@ -147,6 +150,20 @@ class TestMl2SecurityGroups(Ml2SecurityGroupsTestCase,
                                            test_base._uuid()])
             # the or_ function should only have one argument
             or_mock.assert_called_once_with(mock.ANY)
+
+    def test_security_groups_created_outside_transaction(self):
+        def record_after_state(r, e, t, context, *args, **kwargs):
+            self.was_active = context.session.is_active
+
+        registry.subscribe(record_after_state, resources.SECURITY_GROUP,
+                           events.AFTER_CREATE)
+        with self.subnet() as s:
+            self.assertFalse(self.was_active)
+            self._delete(
+                'security-groups',
+                self._list('security-groups')['security_groups'][0]['id'])
+            with self.port(subnet=s):
+                self.assertFalse(self.was_active)
 
 
 class TestMl2SGServerRpcCallBack(
