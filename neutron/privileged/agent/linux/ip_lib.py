d7a6827e43a60a740a7d12c40fc1ead027046348
Brian Haley <brian.haley@hpe.com>
2017-01-10 17:44:58 -0500

    Change neighbour commands to use pyroute2
    Change ip_lib's IpNeighCommand class to use pyroute2
    for adding, deleting and dumping entries, rather than
    using 'ip neigh'.  This will increase performance when
    many ARP updates happen at once.
    
    Change-Id: Idd528c0b402d1c9fc4b030f2aaa6d641d86ec02a
    Partial-Bug: #1511134

--- neutron/privileged/agent/linux/ip_lib.py
+++ neutron/privileged/agent/linux/ip_lib.py
@@ -15,6 +15,7 @@ import socket
 
 import pyroute2
 from pyroute2.netlink import rtnl
+from pyroute2.netlink.rtnl import ndmsg
 
 from neutron._i18n import _
 from neutron import privileged
@@ -36,6 +37,10 @@ class NetworkNamespaceNotFound(RuntimeError):
     def __init__(self, netns_name):
         super(NetworkNamespaceNotFound, self).__init__(
             self.message % {'netns_name': netns_name})
+
+
+class NetworkInterfaceNotFound(RuntimeError):
+    pass
 
 
 @privileged.default.entrypoint
@@ -66,3 +71,100 @@ def get_routing_table(ip_version, namespace=None):
                    'scope': _get_scope_name(route['scope'])}
                   for route in ipdb_routes if route['family'] == family]
     return routes
+
+
+def _get_iproute(namespace):
+    # From iproute.py:
+    # `IPRoute` -- RTNL API to the current network namespace
+    # `NetNS` -- RTNL API to another network namespace
+    if namespace:
+        # do not try and create the namespace
+        return pyroute2.NetNS(namespace, flags=0)
+    else:
+        return pyroute2.IPRoute()
+
+
+def _run_iproute(command, device, namespace, **kwargs):
+    try:
+        with _get_iproute(namespace) as ip:
+            idx = ip.link_lookup(ifname=device)[0]
+            return ip.neigh(command, ifindex=idx, **kwargs)
+    except IndexError:
+        msg = _("Network interface %(device)s not found in namespace "
+                "%(namespace)s.") % {'device': device,
+                                     'namespace': namespace}
+        raise NetworkInterfaceNotFound(msg)
+    except OSError as e:
+        if e.errno == errno.ENOENT:
+            raise NetworkNamespaceNotFound(netns_name=namespace)
+        raise
+
+
+@privileged.default.entrypoint
+def add_neigh_entry(ip_version, ip_address, mac_address, device, namespace,
+                    **kwargs):
+    """Add a neighbour entry.
+
+    :param ip_address: IP address of entry to add
+    :param mac_address: MAC address of entry to add
+    :param device: Device name to use in adding entry
+    :param namespace: The name of the namespace in which to add the entry
+    """
+    family = _IP_VERSION_FAMILY_MAP[ip_version]
+    _run_iproute('replace',
+                 device,
+                 namespace,
+                 dst=ip_address,
+                 lladdr=mac_address,
+                 family=family,
+                 state=ndmsg.states['permanent'],
+                 **kwargs)
+
+
+@privileged.default.entrypoint
+def delete_neigh_entry(ip_version, ip_address, mac_address, device, namespace,
+                       **kwargs):
+    """Delete a neighbour entry.
+
+    :param ip_address: IP address of entry to delete
+    :param mac_address: MAC address of entry to delete
+    :param device: Device name to use in deleting entry
+    :param namespace: The name of the namespace in which to delete the entry
+    """
+    family = _IP_VERSION_FAMILY_MAP[ip_version]
+    _run_iproute('delete',
+                 device,
+                 namespace,
+                 dst=ip_address,
+                 lladdr=mac_address,
+                 family=family,
+                 **kwargs)
+
+
+@privileged.default.entrypoint
+def dump_neigh_entries(ip_version, device, namespace, **kwargs):
+    """Dump all neighbour entries.
+
+    :param ip_version: IP version of entries to show (4 or 6)
+    :param device: Device name to use in dumping entries
+    :param namespace: The name of the namespace in which to dump the entries
+    :param kwargs: Callers add any filters they use as kwargs
+    :return: a list of dictionaries, each representing a neighbour.
+    The dictionary format is: {'dst': ip_address,
+                               'lladdr': mac_address,
+                               'device': device_name}
+    """
+    family = _IP_VERSION_FAMILY_MAP[ip_version]
+    entries = []
+    dump = _run_iproute('dump',
+                        device,
+                        namespace,
+                        family=family,
+                        **kwargs)
+
+    for entry in dump:
+        attrs = dict(entry['attrs'])
+        entries += [{'dst': attrs['NDA_DST'],
+                     'lladdr': attrs.get('NDA_LLADDR'),
+                     'device': device}]
+    return entries
