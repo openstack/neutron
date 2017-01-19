fbf40fe1baac06ce570b660a0a4118e2030c668d
Hirofumi Ichihara <ichihara.hirofumi@lab.ntt.co.jp>
2017-01-19 13:52:39 +0900

    Enhance tag mechanism
    This patch enhances the tag mechanism for subnet, port, subnetpool,
    router resources. The tag-ext as new extension is added so that
    tag supports their resources.
    
    APIImpact: Adds tag support to subnet, port, subnetpool, router
    DocImpact: allow users to set tags on some resources
    
    Change-Id: I3ab8c2f47f283bee7219f39f20b07361b8e0c5f1
    Closes-Bug: #1661608
    (cherry picked from commit b56f008f3a01e5dbbf5b0744a9286a8302c3326a)

--- neutron/tests/unit/extensions/test_tag.py
+++ neutron/tests/unit/extensions/test_tag.py
@@ -10,56 +10,100 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import testscenarios
+
 from neutron.api import extensions
+from neutron.api.v2 import attributes
 from neutron.common import config
 import neutron.extensions
 from neutron.services.tag import tag_plugin
 from neutron.tests import fake_notifier
 from neutron.tests.unit.db import test_db_base_plugin_v2
+from neutron.tests.unit.extensions import test_l3
 
 
+load_tests = testscenarios.load_tests_apply_scenarios
 extensions_path = ':'.join(neutron.extensions.__path__)
 
 
-class TestTagApiBase(test_db_base_plugin_v2.NeutronDbPluginV2TestCase):
+class TestTagApiBase(test_db_base_plugin_v2.NeutronDbPluginV2TestCase,
+                     test_l3.L3NatTestCaseMixin):
+    scenarios = [
+        ('Network Tag Test',
+         dict(resource='networks',
+              member='network')),
+        ('Subnet Tag Test',
+         dict(resource='subnets',
+              member='subnet')),
+        ('Port Tag Test',
+         dict(resource='ports',
+              member='port')),
+        ('Subnetpool Tag Test',
+         dict(resource='subnetpools',
+              member='subnetpool')),
+        ('Router Tag Test',
+         dict(resource='routers',
+              member='router')),
+    ]
 
     def setUp(self):
-        service_plugins = {'TAG': "neutron.services.tag.tag_plugin.TagPlugin"}
+        service_plugins = {
+            'TAG': "neutron.services.tag.tag_plugin.TagPlugin",
+            'router':
+            "neutron.tests.unit.extensions.test_l3.TestL3NatServicePlugin"}
         super(TestTagApiBase, self).setUp(service_plugins=service_plugins)
         plugin = tag_plugin.TagPlugin()
+        l3_plugin = test_l3.TestL3NatServicePlugin()
         ext_mgr = extensions.PluginAwareExtensionManager(
-            extensions_path, {'TAG': plugin}
+            extensions_path, {'router': l3_plugin, 'TAG': plugin}
         )
+        ext_mgr.extend_resources("2.0", attributes.RESOURCE_ATTRIBUTE_MAP)
         app = config.load_paste_app('extensions_test_app')
         self.ext_api = extensions.ExtensionMiddleware(app, ext_mgr=ext_mgr)
 
-    def _get_resource_tags(self, resource_id):
-        res = self._show(self.resource, resource_id)
+    def _make_resource(self):
+        if self.resource == "networks":
+            res = self._make_network(self.fmt, 'net1', True)
+        elif self.resource == "subnets":
+            net = self._make_network(self.fmt, 'net1', True)
+            res = self._make_subnet(self.fmt, net, '10.0.0.1', '10.0.0.0/24')
+        elif self.resource == "ports":
+            net = self._make_network(self.fmt, 'net1', True)
+            res = self._make_port(self.fmt, net['network']['id'])
+        elif self.resource == "subnetpools":
+            res = self._make_subnetpool(self.fmt, ['10.0.0.0/8'],
+                                        name='my pool', tenant_id="tenant")
+        elif self.resource == "routers":
+            res = self._make_router(self.fmt, None)
+        return res[self.member]['id']
+
+    def _get_resource_tags(self):
+        res = self._show(self.resource, self.resource_id)
         return res[self.member]['tags']
 
-    def _put_tag(self, resource_id, tag):
-        req = self._req('PUT', self.resource, id=resource_id,
+    def _put_tag(self, tag):
+        req = self._req('PUT', self.resource, id=self.resource_id,
                         subresource='tags', sub_id=tag)
         return req.get_response(self.ext_api)
 
-    def _put_tags(self, resource_id, tags):
+    def _put_tags(self, tags):
         body = {'tags': tags}
-        req = self._req('PUT', self.resource, data=body, id=resource_id,
+        req = self._req('PUT', self.resource, data=body, id=self.resource_id,
                         subresource='tags')
         return req.get_response(self.ext_api)
 
-    def _get_tag(self, resource_id, tag):
-        req = self._req('GET', self.resource, id=resource_id,
+    def _get_tag(self, tag):
+        req = self._req('GET', self.resource, id=self.resource_id,
                         subresource='tags', sub_id=tag)
         return req.get_response(self.ext_api)
 
-    def _delete_tag(self, resource_id, tag):
-        req = self._req('DELETE', self.resource, id=resource_id,
+    def _delete_tag(self, tag):
+        req = self._req('DELETE', self.resource, id=self.resource_id,
                         subresource='tags', sub_id=tag)
         return req.get_response(self.ext_api)
 
-    def _delete_tags(self, resource_id):
-        req = self._req('DELETE', self.resource, id=resource_id,
+    def _delete_tags(self):
+        req = self._req('DELETE', self.resource, id=self.resource_id,
                         subresource='tags')
         return req.get_response(self.ext_api)
 
@@ -83,9 +127,7 @@ class TestTagApiBase(test_db_base_plugin_v2.NeutronDbPluginV2TestCase):
                                    not_tags=None, not_tags_any=None):
         params = self._make_query_string(tags, tags_any, not_tags,
                                          not_tags_any)
-        req = self._req('GET', self.resource, params=params)
-        res = req.get_response(self.api)
-        res = self.deserialize(self.fmt, res)
+        res = self._list(self.resource, query_params=params)
         return res[self.resource]
 
     def _test_notification_report(self, expect_notify):
@@ -96,168 +138,149 @@ class TestTagApiBase(test_db_base_plugin_v2.NeutronDbPluginV2TestCase):
         fake_notifier.reset()
 
 
-class TestNetworkTagApi(TestTagApiBase):
-    resource = 'networks'
-    member = 'network'
+class TestResourceTagApi(TestTagApiBase):
+
+    def setUp(self):
+        super(TestResourceTagApi, self).setUp()
+        self.resource_id = self._make_resource()
 
     def test_put_tag(self):
         expect_notify = set(['tag.create.start',
                              'tag.create.end'])
-        with self.network() as net:
-            net_id = net['network']['id']
-            res = self._put_tag(net_id, 'red')
-            self.assertEqual(201, res.status_int)
-            tags = self._get_resource_tags(net_id)
-            self._assertEqualTags(['red'], tags)
-            self._test_notification_report(expect_notify)
-            res = self._put_tag(net_id, 'blue')
-            self.assertEqual(201, res.status_int)
-            tags = self._get_resource_tags(net_id)
-            self._assertEqualTags(['red', 'blue'], tags)
-            self._test_notification_report(expect_notify)
+        res = self._put_tag('red')
+        self.assertEqual(201, res.status_int)
+        tags = self._get_resource_tags()
+        self._assertEqualTags(['red'], tags)
+        self._test_notification_report(expect_notify)
+        res = self._put_tag('blue')
+        self.assertEqual(201, res.status_int)
+        tags = self._get_resource_tags()
+        self._assertEqualTags(['red', 'blue'], tags)
+        self._test_notification_report(expect_notify)
 
     def test_put_tag_exists(self):
-        with self.network() as net:
-            net_id = net['network']['id']
-            res = self._put_tag(net_id, 'blue')
-            self.assertEqual(201, res.status_int)
-            res = self._put_tag(net_id, 'blue')
-            self.assertEqual(201, res.status_int)
+        res = self._put_tag('blue')
+        self.assertEqual(201, res.status_int)
+        res = self._put_tag('blue')
+        self.assertEqual(201, res.status_int)
 
     def test_put_tags(self):
         expect_notify = set(['tag.update.start',
                              'tag.update.end'])
-        with self.network() as net:
-            net_id = net['network']['id']
-            res = self._put_tags(net_id, ['red', 'green'])
-            self.assertEqual(200, res.status_int)
-            tags = self._get_resource_tags(net_id)
-            self._assertEqualTags(['red', 'green'], tags)
-            self._test_notification_report(expect_notify)
+        res = self._put_tags(['red', 'green'])
+        self.assertEqual(200, res.status_int)
+        tags = self._get_resource_tags()
+        self._assertEqualTags(['red', 'green'], tags)
+        self._test_notification_report(expect_notify)
 
     def test_put_tags_replace(self):
-        with self.network() as net:
-            net_id = net['network']['id']
-            res = self._put_tags(net_id, ['red', 'green'])
-            self.assertEqual(200, res.status_int)
-            tags = self._get_resource_tags(net_id)
-            self._assertEqualTags(['red', 'green'], tags)
-            res = self._put_tags(net_id, ['blue', 'red'])
-            self.assertEqual(200, res.status_int)
-            tags = self._get_resource_tags(net_id)
-            self._assertEqualTags(['blue', 'red'], tags)
+        res = self._put_tags(['red', 'green'])
+        self.assertEqual(200, res.status_int)
+        tags = self._get_resource_tags()
+        self._assertEqualTags(['red', 'green'], tags)
+        res = self._put_tags(['blue', 'red'])
+        self.assertEqual(200, res.status_int)
+        tags = self._get_resource_tags()
+        self._assertEqualTags(['blue', 'red'], tags)
 
     def test_get_tag(self):
-        with self.network() as net:
-            net_id = net['network']['id']
-            res = self._put_tag(net_id, 'red')
-            self.assertEqual(201, res.status_int)
-            res = self._get_tag(net_id, 'red')
-            self.assertEqual(204, res.status_int)
+        res = self._put_tag('red')
+        self.assertEqual(201, res.status_int)
+        res = self._get_tag('red')
+        self.assertEqual(204, res.status_int)
 
     def test_get_tag_notfound(self):
-        with self.network() as net:
-            net_id = net['network']['id']
-            res = self._put_tag(net_id, 'red')
-            self.assertEqual(201, res.status_int)
-            res = self._get_tag(net_id, 'green')
-            self.assertEqual(404, res.status_int)
+        res = self._put_tag('red')
+        self.assertEqual(201, res.status_int)
+        res = self._get_tag('green')
+        self.assertEqual(404, res.status_int)
 
     def test_delete_tag(self):
         expect_notify = set(['tag.delete.start',
                              'tag.delete.end'])
-        with self.network() as net:
-            net_id = net['network']['id']
-            res = self._put_tags(net_id, ['red', 'green'])
-            self.assertEqual(200, res.status_int)
-            res = self._delete_tag(net_id, 'red')
-            self.assertEqual(204, res.status_int)
-            tags = self._get_resource_tags(net_id)
-            self._assertEqualTags(['green'], tags)
-            self._test_notification_report(expect_notify)
+        res = self._put_tags(['red', 'green'])
+        self.assertEqual(200, res.status_int)
+        res = self._delete_tag('red')
+        self.assertEqual(204, res.status_int)
+        tags = self._get_resource_tags()
+        self._assertEqualTags(['green'], tags)
+        self._test_notification_report(expect_notify)
 
     def test_delete_tag_notfound(self):
-        with self.network() as net:
-            net_id = net['network']['id']
-            res = self._put_tags(net_id, ['red', 'green'])
-            self.assertEqual(200, res.status_int)
-            res = self._delete_tag(net_id, 'blue')
-            self.assertEqual(404, res.status_int)
+        res = self._put_tags(['red', 'green'])
+        self.assertEqual(200, res.status_int)
+        res = self._delete_tag('blue')
+        self.assertEqual(404, res.status_int)
 
     def test_delete_tags(self):
         expect_notify = set(['tag.delete_all.start',
                              'tag.delete_all.end'])
-        with self.network() as net:
-            net_id = net['network']['id']
-            res = self._put_tags(net_id, ['red', 'green'])
-            self.assertEqual(200, res.status_int)
-            res = self._delete_tags(net_id)
-            self.assertEqual(204, res.status_int)
-            tags = self._get_resource_tags(net_id)
-            self._assertEqualTags([], tags)
-            self._test_notification_report(expect_notify)
+        res = self._put_tags(['red', 'green'])
+        self.assertEqual(200, res.status_int)
+        res = self._delete_tags()
+        self.assertEqual(204, res.status_int)
+        tags = self._get_resource_tags()
+        self._assertEqualTags([], tags)
+        self._test_notification_report(expect_notify)
 
 
-class TestNetworkTagFilter(TestTagApiBase):
-    resource = 'networks'
-    member = 'network'
+class TestResourceTagFilter(TestTagApiBase):
 
     def setUp(self):
-        super(TestNetworkTagFilter, self).setUp()
-        self._prepare_network_tags()
+        super(TestResourceTagFilter, self).setUp()
+        self._prepare_resource_tags()
 
-    def _prepare_network_tags(self):
-        res = self._make_network(self.fmt, 'net1', True)
-        net1_id = res['network']['id']
-        res = self._make_network(self.fmt, 'net2', True)
-        net2_id = res['network']['id']
-        res = self._make_network(self.fmt, 'net3', True)
-        net3_id = res['network']['id']
-        res = self._make_network(self.fmt, 'net4', True)
-        net4_id = res['network']['id']
-        res = self._make_network(self.fmt, 'net5', True)
-        net5_id = res['network']['id']
+    def _make_tags(self, resource_id, tags):
+        body = {'tags': tags}
+        req = self._req('PUT', self.resource, data=body, id=resource_id,
+                        subresource='tags')
+        return req.get_response(self.ext_api)
 
-        self._put_tags(net1_id, ['red'])
-        self._put_tags(net2_id, ['red', 'blue'])
-        self._put_tags(net3_id, ['red', 'blue', 'green'])
-        self._put_tags(net4_id, ['green'])
-        # net5: no tags
-        tags = self._get_resource_tags(net5_id)
-        self._assertEqualTags([], tags)
+    def _prepare_resource_tags(self):
+        self.res1 = self._make_resource()
+        self.res2 = self._make_resource()
+        self.res3 = self._make_resource()
+        self.res4 = self._make_resource()
+        self.res5 = self._make_resource()
+
+        self._make_tags(self.res1, ['red'])
+        self._make_tags(self.res2, ['red', 'blue'])
+        self._make_tags(self.res3, ['red', 'blue', 'green'])
+        self._make_tags(self.res4, ['green'])
+        # res5: no tags
 
     def _assertEqualResources(self, expected, res):
-        actual = [n['name'] for n in res]
+        actual = [n['id'] for n in res]
         self.assertEqual(set(expected), set(actual))
 
     def test_filter_tags_single(self):
         res = self._get_tags_filter_resources(tags=['red'])
-        self._assertEqualResources(['net1', 'net2', 'net3'], res)
+        self._assertEqualResources([self.res1, self.res2, self.res3], res)
 
     def test_filter_tags_multi(self):
         res = self._get_tags_filter_resources(tags=['red', 'blue'])
-        self._assertEqualResources(['net2', 'net3'], res)
+        self._assertEqualResources([self.res2, self.res3], res)
 
     def test_filter_tags_any_single(self):
         res = self._get_tags_filter_resources(tags_any=['blue'])
-        self._assertEqualResources(['net2', 'net3'], res)
+        self._assertEqualResources([self.res2, self.res3], res)
 
     def test_filter_tags_any_multi(self):
         res = self._get_tags_filter_resources(tags_any=['red', 'blue'])
-        self._assertEqualResources(['net1', 'net2', 'net3'], res)
+        self._assertEqualResources([self.res1, self.res2, self.res3], res)
 
     def test_filter_not_tags_single(self):
         res = self._get_tags_filter_resources(not_tags=['red'])
-        self._assertEqualResources(['net4', 'net5'], res)
+        self._assertEqualResources([self.res4, self.res5], res)
 
     def test_filter_not_tags_multi(self):
         res = self._get_tags_filter_resources(not_tags=['red', 'blue'])
-        self._assertEqualResources(['net1', 'net4', 'net5'], res)
+        self._assertEqualResources([self.res1, self.res4, self.res5], res)
 
     def test_filter_not_tags_any_single(self):
         res = self._get_tags_filter_resources(not_tags_any=['blue'])
-        self._assertEqualResources(['net1', 'net4', 'net5'], res)
+        self._assertEqualResources([self.res1, self.res4, self.res5], res)
 
     def test_filter_not_tags_any_multi(self):
         res = self._get_tags_filter_resources(not_tags_any=['red', 'blue'])
-        self._assertEqualResources(['net4', 'net5'], res)
+        self._assertEqualResources([self.res4, self.res5], res)
