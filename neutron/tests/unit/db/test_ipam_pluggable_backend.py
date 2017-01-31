6123d20766cd82fab723760fcd8a7969c63bea3d
John Davidge <john.davidge@rackspace.com>
2017-01-31 13:49:21 +0000

    Fix port update for service subnets
    Updating a port's fixed_ips will currently fail for a subnet that has
    a service type, regardless of whether the port has a matching
    device_owner. This is because _update_ips_for_port in
    ipam_pluggable_backend.py does not take the device_owner into account
    when calling _ipam_get_subnets.
    
    This patch fixes the bug by passing the port's device_owner to
    _ipam_get_subnets in _update_ips_for_port, and includes a new unit
    test to prevent future regressions.
    
    Change-Id: I09ac773ae27eebe104d5c39e1e8863c02880f85d
    Closes-Bug: #1660607
    (cherry picked from commit b61b630f0b8e620b218a29d72a250382e07055c1)

--- neutron/tests/unit/db/test_ipam_pluggable_backend.py
+++ neutron/tests/unit/db/test_ipam_pluggable_backend.py
@@ -659,7 +659,8 @@ class TestDbBasePluginIpam(test_db_base.NeutronDbPluginV2TestCase):
                                            original_ips, new_ips, mac)
         mocks['driver'].get_address_request_factory.assert_called_once_with()
         mocks['ipam']._ipam_get_subnets.assert_called_once_with(
-            context, network_id=port_dict['network_id'], host=None)
+            context, network_id=port_dict['network_id'], host=None,
+            service_type=port_dict['device_owner'])
         # Validate port_dict is passed into address_factory
         address_factory.get_request.assert_called_once_with(context,
                                                             port_dict,
