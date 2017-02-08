3e1ed94e389c427f1da56cde43a458832078f073
venkata anil <anilvenkata@redhat.com>
2017-02-08 15:49:47 +0000

    Avoid router ri.process if initialize() fails
    When router_info initialize() fails(with trace) some resources(
    like keepalived process) may not be created. While handling this
    exception, l3 agent calls _process_updated_router instead of
    again calling _process_added_router, which also fails trying to
    access resources which are not created.
    
    In this change, agent will have new router_info(i.e
    self.router_info[router_id] = ri) only when initialize() succeeds.
    When initialize() fails, as router_info is not part of agent,
    "_process_router_if_compatible" will again call initialize().
    We also cleanup router_info when initialize() fails.
    
    Closes-bug: #1662804
    Change-Id: I278ac83de57713c93d6e50846d79034d774c5d47

--- neutron/agent/l3/ha_router.py
+++ neutron/agent/l3/ha_router.py
@@ -20,7 +20,7 @@ import netaddr
 from neutron_lib import constants as n_consts
 from oslo_log import log as logging
 
-from neutron._i18n import _LE
+from neutron._i18n import _, _LE
 from neutron.agent.l3 import namespaces
 from neutron.agent.l3 import router_info as router
 from neutron.agent.linux import external_process
@@ -105,12 +105,13 @@ class HaRouter(router.RouterInfo):
             return False
 
     def initialize(self, process_monitor):
-        super(HaRouter, self).initialize(process_monitor)
         ha_port = self.router.get(n_consts.HA_INTERFACE_KEY)
         if not ha_port:
-            LOG.error(_LE('Unable to process HA router %s without HA port'),
-                      self.router_id)
-            return
+            msg = _("Unable to process HA router %s without "
+                    "HA port") % self.router_id
+            LOG.exception(msg)
+            raise Exception(msg)
+        super(HaRouter, self).initialize(process_monitor)
 
         self.ha_port = ha_port
         self._init_keepalived_manager(process_monitor)
