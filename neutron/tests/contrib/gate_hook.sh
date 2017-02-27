7b1a2e9d7255e84a6b3a3945e9acfee372b9700d
Sean Dague <sean@dague.net>
2017-02-27 14:56:31 -0500

    Convert gate_hook to devstack-tools
    Devstack does not support multiple duplicate sections in local.conf.
    Currently it works by chance, and is not guaranteed to work when next
    changes land for devstack. So we should make sure we generate local.conf
    that does not have duplicate sections.
    
    This can be achieved using tools from devstack-tools package, that allow
    to produce and merge multiple local.conf files compatible with devstack.
    
    This change will need to be backport to all stable branches to
    accommodate for branchless devstack-gate repo.
    
    Conflicts:
    	neutron/tests/contrib/gate_hook.sh
    
    Related-Bug: #1667331
    Co-Authored-By: Ihar Hrachyshka <ihrachys@redhat.com>
    Depends-On: I53119876b82eeaec19382dbe115cb1740bed0cec
    Change-Id: Ibd0f67f9131e7f67f3a4a62cb6ad28bf80e11bbf
    (cherry picked from commit 8626979c75be44c006d3019b921cd44a1339d466)
    (cherry picked from commit a78880cea5687c51caeb4e6bcfd8a85eacdf3c7e)

--- neutron/tests/contrib/gate_hook.sh
+++ neutron/tests/contrib/gate_hook.sh
@@ -8,41 +8,42 @@ GATE_DEST=$BASE/new
 NEUTRON_PATH=$GATE_DEST/neutron
 GATE_HOOKS=$NEUTRON_PATH/neutron/tests/contrib/hooks
 DEVSTACK_PATH=$GATE_DEST/devstack
-LOCAL_CONF=$DEVSTACK_PATH/local.conf
+LOCAL_CONF=$DEVSTACK_PATH/late-local.conf
 RALLY_EXTRA_DIR=$NEUTRON_PATH/rally-jobs/extra
+DSCONF=/tmp/devstack-tools/bin/dsconf
 
+# Install devstack-tools used to produce local.conf; we can't rely on
+# test-requirements.txt because the gate hook is triggered before neutron is
+# installed
+sudo -H pip install virtualenv
+virtualenv /tmp/devstack-tools
+/tmp/devstack-tools/bin/pip install -U devstack-tools==0.4.0
 
 # Inject config from hook into localrc
 function load_rc_hook {
     local hook="$1"
+    local tmpfile
+    local config
+    tmpfile=$(tempfile)
     config=$(cat $GATE_HOOKS/$hook)
-    export DEVSTACK_LOCAL_CONFIG+="
-# generated from hook '$hook'
-[[local|localrc]]
-${config}
-"
+    echo "[[local|localrc]]" > $tmpfile
+    $DSCONF setlc_raw $tmpfile "$config"
+    $DSCONF merge_lc $LOCAL_CONF $tmpfile
+    rm -f $tmpfile
 }
 
 
 # Inject config from hook into local.conf
 function load_conf_hook {
     local hook="$1"
-    config=$(cat $GATE_HOOKS/$hook)
-    export DEVSTACK_LOCAL_CONFIG+="
-# generated from hook '$hook'
-${config}
-"
+    $DSCONF merge_lc $LOCAL_CONF $GATE_HOOKS/$hook
 }
 
 
 # Tweak gate configuration for our rally scenarios
 function load_rc_for_rally {
     for file in $(ls $RALLY_EXTRA_DIR/*.setup); do
-        local config=$(cat $file)
-        export DEVSTACK_LOCAL_CONFIG+="
-# generated from hook '$file'
-${config}
-"
+        $DSCONF merge_lc $LOCAL_CONF $file
     done
 }
 
@@ -54,6 +55,7 @@ case $VENV in
     GATE_STACK_USER=stack
     PROJECT_NAME=neutron
     IS_GATE=True
+    LOCAL_CONF=$DEVSTACK_PATH/local.conf
 
     source $DEVSTACK_PATH/functions
     source $NEUTRON_PATH/devstack/lib/ovs
@@ -92,11 +94,13 @@ case $VENV in
         load_conf_hook pecan
     fi
 
+    export DEVSTACK_LOCALCONF=$(cat $LOCAL_CONF)
     $BASE/new/devstack-gate/devstack-vm-gate.sh
     ;;
 
 "rally")
     load_rc_for_rally
+    export DEVSTACK_LOCALCONF=$(cat $LOCAL_CONF)
     $BASE/new/devstack-gate/devstack-vm-gate.sh
     ;;
 
