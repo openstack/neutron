1361f55d1684550f0942e9342abaf367e4d73c21
Kevin Benton <kevin@benton.pub>
2017-03-01 10:47:10 -0800

    Scan for mac through all devices
    The first device the agent happens to pick could be something
    without a mac address like a 6in6 interface. This was causing
    the agent to fail to start if it was unlucky enough to pick
    that address.
    
    This patch just adjusts the logic to keep iterating through
    the list until we find a mac.
    
    Conflicts:
    	neutron/plugins/ml2/drivers/linuxbridge/agent/linuxbridge_neutron_agent.py
    	neutron/tests/unit/plugins/ml2/drivers/linuxbridge/agent/test_linuxbridge_neutron_agent.py
    
    Closes-Bug: #1669087
    Change-Id: I6e934a2dff8fd441e1c70c9a5857fd6150835e2a
    (cherry picked from commit 42631e311779b9a779ad8e7bd6654a58c6124fca)

--- neutron/plugins/ml2/drivers/linuxbridge/agent/linuxbridge_neutron_agent.py
+++ neutron/plugins/ml2/drivers/linuxbridge/agent/linuxbridge_neutron_agent.py
@@ -725,8 +725,10 @@ class LinuxBridgeManager(amb.CommonAgentManagerBase):
                 list(self.bridge_mappings.values())[0])
         else:
             devices = ip_lib.IPWrapper().get_devices(True)
-            if devices:
-                mac = utils.get_interface_mac(devices[0].name)
+            for device in devices:
+                mac = utils.get_interface_mac(device.name)
+                if mac:
+                    break
             else:
                 LOG.error(_LE("Unable to obtain MAC address for unique ID. "
                               "Agent terminated!"))
