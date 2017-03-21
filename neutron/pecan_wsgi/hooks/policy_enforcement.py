0193777e60216844019963f785cd28ca205c42a4
Anindita Das <anindita.das@intel.com>
2017-03-21 03:37:25 +0000

    [Pecan] Fix custom tenant_id project_id matching
    This patch fixes the missing custom tenant_id and project_id
    matching on policy_enforcement.py
    
    Change-Id: I278759f6b65cce7caa4f66fa694488b75f0459b6
    Closes-Bug: #1674517

--- neutron/pecan_wsgi/hooks/policy_enforcement.py
+++ neutron/pecan_wsgi/hooks/policy_enforcement.py
@@ -223,6 +223,13 @@ class PolicyHook(hooks.PecanHook):
         """
         attributes_to_exclude = []
         for attr_name in data.keys():
+            # TODO(amotoki): All attribute maps have tenant_id and
+            # it determines excluded attributes based on tenant_id.
+            # We need to migrate tenant_id to project_id later
+            # as attr_info is referred to in various places and we need
+            # to check all logs carefully.
+            if attr_name == 'project_id':
+                continue
             attr_data = controller.resource_info.get(attr_name)
             if attr_data and attr_data['is_visible']:
                 if policy.check(
@@ -238,4 +245,9 @@ class PolicyHook(hooks.PecanHook):
             # if the code reaches this point then either the policy check
             # failed or the attribute was not visible in the first place
             attributes_to_exclude.append(attr_name)
+            # TODO(amotoki): As mentioned in the above TODO,
+            # we treat project_id and tenant_id equivalently.
+            # This should be migrated to project_id later.
+            if attr_name == 'tenant_id':
+                attributes_to_exclude.append('project_id')
         return attributes_to_exclude
