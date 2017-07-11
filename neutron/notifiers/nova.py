87b02a255232f774c3548101681d8b5687a6adf5
Trevor McCasland <TM2086@att.com>
2017-07-11 10:04:59 -0500

    TrivialFix: Remove only_contrib argument
    While running unit tests plugins/ml2/drivers/test_type_vlan.py
    the following message is observed:
    
    """ UserWarning: Discovering extensions only by contrib path is
    no longer supported since all contrib extensions have either
    been made required or removed. The only_contrib argument is
    deprecated and will be removed in a future release.
    """
    
    From the source code you can see the TODO message to remove
    the argument in Queens.
    https://github.com/openstack/python-novaclient/blob/264c22a9cc0955a1d0f545d5bc5de30bdf7b5072/novaclient/client.py#L181
    
    This commit removes the argument and the stderr captured by unit
    tests.
    
    Change-Id: Ia4270c69c1519342636206fa439640240a1e13bb

--- neutron/notifiers/nova.py
+++ neutron/notifiers/nova.py
@@ -64,8 +64,7 @@ class Notifier(object):
             auth=auth)
 
         extensions = [
-            ext for ext in nova_client.discover_extensions(NOVA_API_VERSION,
-                                                           only_contrib=True)
+            ext for ext in nova_client.discover_extensions(NOVA_API_VERSION)
             if ext.name == "server_external_events"]
         self.nclient = nova_client.Client(
             api_versions.APIVersion(NOVA_API_VERSION),
