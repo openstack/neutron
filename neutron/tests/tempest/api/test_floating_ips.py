62503dcb78ac4c72c8949c7dd77bef3d4a4418c3
Ihar Hrachyshka <ihrachys@redhat.com>
2017-08-03 12:37:43 -0700

    tests: don't set description in test_blank_update_clears_association
    'description' attribute of floating IPs is part of
    standard-attr-description API extension, so using it in the test case
    without requiring the extension is incorrect. But instead of adding the
    new requirement, this patch removes 'description' from the floating IP
    payload because it's not needed to prove the point of the test case
    (that an empty update body will disassociate the floating IP).
    
    Change-Id: I9ab5b0ba5f32f73818f8eac8a0001eeb13248423

--- neutron/tests/tempest/api/test_floating_ips.py
+++ neutron/tests/tempest/api/test_floating_ips.py
@@ -51,7 +51,6 @@ class FloatingIPTestJSON(base.BaseNetworkTest):
         body = self.client.create_floatingip(
             floating_network_id=self.ext_net_id,
             port_id=self.ports[0]['id'],
-            description='d1'
         )['floatingip']
         self.floating_ips.append(body)
         self.assertEqual(self.ports[0]['id'], body['port_id'])
