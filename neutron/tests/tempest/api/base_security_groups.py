2a479053ecdf0feec7abed6409ac69b75df53520
Trevor McCasland <TM2086@att.com>
2017-08-31 15:08:52 -0500

    Security Groups: Test all protocols names and nums
    The following operations are enforced by this change:
      * all v4 names/ints can be created in both v4 and v6 networks
      * all v6 names/ints can be created in v6 networks
      * no v6 names/ints can be created in v4 networks
    
    This patch is a follow up to the doc chage:
    Iff56c9290d8e86cfc0e2bfa24176f8d1e338a06e
    
    Change-Id: I78dee3b665185d7da9d854a23f1a7c9d6dbe96de
    Closes-Bug: #1692134

--- neutron/tests/tempest/api/base_security_groups.py
+++ neutron/tests/tempest/api/base_security_groups.py
@@ -13,9 +13,23 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from neutron_lib import constants
 from tempest.lib.common.utils import data_utils
 
 from neutron.tests.tempest.api import base
+
+
+V4_PROTOCOL_NAMES = set(key for key in constants.IP_PROTOCOL_MAP if
+                        'v6' not in key)
+V4_PROTOCOL_INTS = set(v for k, v in constants.IP_PROTOCOL_MAP.items()
+                       if 'v6' not in k)
+V6_PROTOCOL_LEGACY = set([constants.PROTO_NAME_IPV6_ICMP_LEGACY])
+V6_PROTOCOL_NAMES = (
+    set(key for key in constants.IP_PROTOCOL_MAP if 'v6' in key) -
+    V6_PROTOCOL_LEGACY
+)
+V6_PROTOCOL_INTS = set(v for k, v in constants.IP_PROTOCOL_MAP.items() if
+                       'v6' in k)
 
 
 class BaseSecGroupTest(base.BaseNetworkTest):
@@ -39,3 +53,41 @@ class BaseSecGroupTest(base.BaseNetworkTest):
         for secgroup in list_body['security_groups']:
             secgroup_list.append(secgroup['id'])
         self.assertNotIn(secgroup_id, secgroup_list)
+
+    def _create_security_group_rule(self, **kwargs):
+        rule_create_body = self.client.create_security_group_rule(**kwargs)
+        # List rules and verify created rule is in response
+        rule_list_body = (
+            self.client.list_security_group_rules())
+        rule_list = [rule['id']
+                     for rule in rule_list_body['security_group_rules']]
+        self.assertIn(rule_create_body['security_group_rule']['id'],
+                      rule_list)
+        self.addCleanup(self._delete_security_group_rule,
+                        rule_create_body['security_group_rule']['id'])
+        return rule_create_body
+
+    def _show_security_group_rule(self, **kwargs):
+        show_rule_body = self.client.show_security_group_rule(kwargs['id'])
+        for key, value in kwargs.items():
+            self.assertEqual(value,
+                             show_rule_body['security_group_rule'][key],
+                             "%s does not match." % key)
+
+    def _delete_security_group_rule(self, secgroup_rule_id):
+        self.client.delete_security_group_rule(secgroup_rule_id)
+        rule_list_body = self.client.list_security_group_rules()
+        rule_list = [rule['id']
+                     for rule in rule_list_body['security_group_rules']]
+        self.assertNotIn(secgroup_rule_id, rule_list)
+
+    def _test_create_show_delete_security_group_rule(self, **kwargs):
+        # The security group rule is deleted by the cleanup call in
+        # _create_security_group_rule.
+        rule_create_body = (
+            self._create_security_group_rule(**kwargs)['security_group_rule'])
+        self._show_security_group_rule(
+            id=rule_create_body['id'],
+            protocol=rule_create_body['protocol'],
+            direction=rule_create_body['direction'],
+            ethertype=rule_create_body['ethertype'])
