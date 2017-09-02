dd9deaf3a14f264df52002eb3f0aee6d232b62de
Jens Harbott <j.harbott@x-ion.de>
2017-09-02 08:00:51 +0000

    Fix port deletion when dns_integration is enabled
    The records found in ip_allocations contain objects of type IPAddress,
    but the external dns service expects them as string, so we need to
    insert a conversion.
    
    Conflicts:
    	neutron/plugins/ml2/extensions/dns_integration.py
    
    Change-Id: I622993fc273121bfd051d2fd9c7811e2ae49a1d8
    Closes-Bug: 1714641
    (cherry picked from commit 77a596ab7c3f0015f71b4e8a359478141807d11e)

--- neutron/plugins/ml2/extensions/dns_integration.py
+++ neutron/plugins/ml2/extensions/dns_integration.py
@@ -433,7 +433,7 @@ def _delete_port_in_external_dns_service(resource, event, trigger, **kwargs):
     if dns_data_db['current_dns_name']:
         ip_allocations = context.session.query(
             models_v2.IPAllocation).filter_by(port_id=port_id).all()
-        records = [alloc['ip_address'] for alloc in ip_allocations]
+        records = [str(alloc['ip_address']) for alloc in ip_allocations]
         _remove_data_from_external_dns_service(
             context, dns_driver, dns_data_db['current_dns_domain'],
             dns_data_db['current_dns_name'], records)
