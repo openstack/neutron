284bbce66e2733f60acebf897e05b9cb7b05b670
Boden R <bodenvmw@gmail.com>
2017-11-15 11:34:16 -0700

    use dns domain ports api def from neutron-lib
    The dns domain ports extension's API definition was rehomed into
    neutron-lib with I79a96e72daa5baf579fcece2a703a47b8059423e
    This patch consumes it by removing the rehomed code and using the
    APIExtensionDescriptor for the extensions parent class.
    
    NeutronLibImpact
    
    Change-Id: I8517a5b7c1ae7f4909f4c9829cf8ddea865f5210

--- neutron/extensions/dns_domain_ports.py
+++ neutron/extensions/dns_domain_ports.py
@@ -13,49 +13,11 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from neutron_lib.api import converters
-from neutron_lib.api.definitions import dns as dns_apidef
+from neutron_lib.api.definitions import dns_domain_ports as apidef
 from neutron_lib.api import extensions
-from neutron_lib.db import constants
 
 
-EXTENDED_ATTRIBUTES_2_0 = {
-    'ports': {
-        dns_apidef.DNSDOMAIN: {
-            'allow_post': True, 'allow_put': True,
-            'default': '',
-            'convert_to': converters.convert_string_to_case_insensitive,
-            'validate': {
-                'type:dns_domain_name': constants.FQDN_FIELD_SIZE},
-            'is_visible': True},
-    },
-}
-
-
-class Dns_domain_ports(extensions.ExtensionDescriptor):
+class Dns_domain_ports(extensions.APIExtensionDescriptor):
     """Extension class supporting dns_domain attribute for ports."""
 
-    @classmethod
-    def get_name(cls):
-        return "dns_domain for ports"
-
-    @classmethod
-    def get_alias(cls):
-        return "dns-domain-ports"
-
-    @classmethod
-    def get_description(cls):
-        return "Allows the DNS domain to be specified for a network port."
-
-    @classmethod
-    def get_updated(cls):
-        return "2017-06-25T18:00:00-00:00"
-
-    def get_required_extensions(self):
-        return [dns_apidef.ALIAS]
-
-    def get_extended_resources(self, version):
-        if version == "2.0":
-            return EXTENDED_ATTRIBUTES_2_0
-        else:
-            return {}
+    api_definition = apidef
