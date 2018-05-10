59bc19c14a84283adad555dce8536fd7198b82b3
Hamdy Khader <hamdyk@mellanox.com>
2018-05-10 15:58:42 +0300

    Use Param DHCP_OPT_CLIENT_ID_NUM
    Ironic is sending 'client-id' as a number (61) to meet rfc4776 [1],
    due to this change Neutron DHCP should support this option as well.
    
    [1] https://github.com/openstack/ironic/commit/228a2a7885e1b04d4180fe8daa2992884decaf6d
    
    Closes-Bug: #1770932
    Change-Id: I9728354d5f9e08a0dc23900b2bc22b4a0aedb737

--- neutron/agent/linux/dhcp.py
+++ neutron/agent/linux/dhcp.py
@@ -61,6 +61,9 @@ DNSMASQ_SERVICE_NAME = 'dnsmasq'
 DHCP_RELEASE_TRIES = 3
 DHCP_RELEASE_TRIES_SLEEP = 0.3
 
+# this variable will be removed when neutron-lib is updated with this value
+DHCP_OPT_CLIENT_ID_NUM = 61
+
 
 class DictModel(dict):
     """Convert dict into an object that provides attribute access to values."""
@@ -733,7 +736,8 @@ class Dnsmasq(DhcpLocalProcess):
     def _get_client_id(self, port):
         if self._get_port_extra_dhcp_opts(port):
             for opt in port.extra_dhcp_opts:
-                if opt.opt_name == edo_ext.DHCP_OPT_CLIENT_ID:
+                if opt.opt_name in (edo_ext.DHCP_OPT_CLIENT_ID,
+                                    DHCP_OPT_CLIENT_ID_NUM):
                     return opt.opt_value
 
     def _read_hosts_file_leases(self, filename):
@@ -1021,7 +1025,8 @@ class Dnsmasq(DhcpLocalProcess):
                     [netaddr.IPAddress(ip.ip_address).version
                      for ip in port.fixed_ips])
                 for opt in port.extra_dhcp_opts:
-                    if opt.opt_name == edo_ext.DHCP_OPT_CLIENT_ID:
+                    if opt.opt_name in (edo_ext.DHCP_OPT_CLIENT_ID,
+                                        DHCP_OPT_CLIENT_ID_NUM):
                         continue
                     opt_ip_version = opt.ip_version
                     if opt_ip_version in port_ip_versions:
