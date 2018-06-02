5a91c6de429c59ca7c85f27df3c2ba0d163d4bd6
Yang JianFeng <yjf1970231893@gmail.com>
2018-06-02 05:10:59 +0000

    Don't skip DVR port while neutron-openvswitch-agent is restared.
    neutron-openvswitch-agent will refresh flows when it's restarted.
    But the port's binding status is not changed, update_port_postcommit
    will be skipped at function '_update_individual_port_db_status' in
    'neutron/plugins/ml2/plugin.py', l2pop don't handle DVR ports, the
    fdb entries about DVR port will not be added.
    
    So, we can't skip DVR port at notify_l2pop_port_wiring when agent
    is restared.
    
    Closes-Bug: #1773286
    Change-Id: I54e3db4822830a0c83daf7b5150575f8d6e2497b
    (cherry picked from commit d0fa2c9ac50a783c410fab2df938974c4e829ffa)

--- neutron/plugins/ml2/drivers/l2pop/mech_driver.py
+++ neutron/plugins/ml2/drivers/l2pop/mech_driver.py
@@ -248,6 +248,14 @@ class L2populationMechanismDriver(api.MechanismDriver):
 
         return agents
 
+    def agent_restarted(self, context):
+        agent_host = context.host
+        session = db_api.get_reader_session()
+        agent = l2pop_db.get_agent_by_host(session, agent_host)
+        if l2pop_db.get_agent_uptime(agent) < cfg.CONF.l2pop.agent_boot_time:
+            return True
+        return False
+
     def update_port_down(self, context):
         port = context.current
         agent_host = context.host
@@ -296,9 +304,8 @@ class L2populationMechanismDriver(api.MechanismDriver):
         # with high concurrency more than 1 port may be activated on an agent
         # at the same time (like VM port + a DVR port) so checking for 1 or 2
         is_first_port = agent_active_ports in (1, 2)
-        if is_first_port or (l2pop_db.get_agent_uptime(agent) <
-                             cfg.CONF.l2pop.agent_boot_time):
-            # First port(s) activated on current agent in this network,
+        if is_first_port or self.agent_restarted(context):
+            # First port activated on current agent in this network,
             # we have to provide it with the whole list of fdb entries
             agent_fdb_entries = self._create_agent_fdb(session,
                                                        agent,
