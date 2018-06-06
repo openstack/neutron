e17ff7a32e069c44c6ff02cd9aafa768d413f369
Yang JianFeng <yjf1970231893@gmail.com>
2018-06-06 10:55:00 +0000

    Don't uninstall flow tables in setup_dvr_flows_on_integ_br
    This is a revise for I7b24a159962af7b58c096a1b2766e2169e9f8aed
    
    Br-int's flow tables are already uninstalled in setup_integration_br.
    And setup_integration_br will install some default flows. If we still
    uninstall flow tables of br-int in setup_dvr_flows_on_integ_br, these
    default flow tables will be missing.
    
    Closes-Bug: #1775146
    Change-Id: I71c1f9034dfc913b9e9ae17cc8f6bd084c9ee580
    (cherry picked from commit 760870b6c238f5f2aa5fec33852e51b4ebccb718)

--- neutron/plugins/ml2/drivers/openvswitch/agent/ovs_dvr_neutron_agent.py
+++ neutron/plugins/ml2/drivers/openvswitch/agent/ovs_dvr_neutron_agent.py
@@ -16,7 +16,6 @@
 import sys
 
 from neutron_lib import constants as n_const
-from oslo_config import cfg
 from oslo_log import log as logging
 import oslo_messaging
 from oslo_utils import excutils
@@ -28,9 +27,6 @@ from neutron.plugins.common import constants as p_const
 from neutron.plugins.ml2.drivers.openvswitch.agent.common import constants
 
 LOG = logging.getLogger(__name__)
-
-cfg.CONF.import_group('AGENT', 'neutron.plugins.ml2.drivers.openvswitch.'
-                      'agent.common.config')
 
 
 # A class to represent a DVR-hosted subnet including vif_ports resident on
@@ -142,7 +138,6 @@ class OVSDVRNeutronAgent(object):
         self.dvr_mac_address = None
         if self.enable_distributed_routing:
             self.get_dvr_mac_address()
-        self.conf = cfg.CONF
 
     def setup_dvr_flows(self):
         self.setup_dvr_flows_on_integ_br()
@@ -207,10 +202,6 @@ class OVSDVRNeutronAgent(object):
 
         LOG.info(_LI("L2 Agent operating in DVR Mode with MAC %s"),
                  self.dvr_mac_address)
-        # Remove existing flows in integration bridge
-        if self.conf.AGENT.drop_flows_on_start:
-            self.int_br.uninstall_flows()
-
         # Add a canary flow to int_br to track OVS restarts
         self.int_br.setup_canary_table()
 
