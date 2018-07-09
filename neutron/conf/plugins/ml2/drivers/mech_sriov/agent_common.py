d7d433f7290517c4e68290e46e6bcb859f1930a4
Bence Romsics <bence.romsics@ericsson.com>
2018-07-09 16:16:52 +0200

    sriov-agent: Report resource info in heartbeat
    Example config for sriov-agent:
    
    sriov_agent.ini:
    [sriov_nic]
    physical_device_mappings = physnet0:ens5,physnet0:ens6
    resource_provider_bandwidths = ens5:100000:100000,ens6:100000:100000
    
    Agent configurations now includes 'resource_provider_bandwidths' and
    'resource_provider_inventory_defaults'.
    
    Change-Id: I1af798d404ed94cd30da3f92734bea911bbc3ac7
    Co-Authored-By: Lajos Katona <lajos.katona@ericsson.com>
    Depends-On: https://review.openstack.org/577220
    Partial-Bug: #1578989
    See-Also: https://review.openstack.org/502306 (nova spec)
    See-Also: https://review.openstack.org/508149 (neutron spec)

--- neutron/conf/plugins/ml2/drivers/mech_sriov/agent_common.py
+++ neutron/conf/plugins/ml2/drivers/mech_sriov/agent_common.py
@@ -43,6 +43,35 @@ sriov_nic_opts = [
                        "network_device. The network_device in the mapping "
                        "should appear in the physical_device_mappings "
                        "list.")),
+    cfg.ListOpt('resource_provider_bandwidths',
+                default=[],
+                help=_("Comma-separated list of "
+                       "<network_device>:<egress_bw>:<ingress_bw> tuples, "
+                       "showing the available bandwidth for the given device "
+                       "in the given direction. The direction is meant from "
+                       "VM perspective. Bandwidth is measured in kilobits per "
+                       "second (kbps). The device must appear in "
+                       "physical_device_mappings as the value. But not all "
+                       "devices in physical_device_mappings must be listed "
+                       "here. For a device not listed here we neither create "
+                       "a resource provider in placement nor report "
+                       "inventories against. An omitted direction means we do "
+                       "not report an inventory for the corresponding "
+                       "class.")),
+    cfg.DictOpt('resource_provider_inventory_defaults',
+                default={'allocation_ratio': 1.0,
+                         'min_unit': 1,
+                         'step_size': 1,
+                         'reserved': 0},
+                help=_("Key:value pairs to specify defaults used "
+                       "while reporting resource provider inventories. "
+                       "Possible keys with their types: "
+                       "allocation_ratio:float, "
+                       "max_unit:int, min_unit:int, "
+                       "reserved:int, step_size:int, "
+                       "See also: "
+                       "https://developer.openstack.org/api-ref/placement/"
+                       "#update-resource-provider-inventories")),
 ]
 
 
