eb4221e6f0b48b131f2009168b3b22f5a3d4dd8f
Lajos Katona <lajos.katona@ericsson.com>
2018-07-18 12:41:27 +0200

    Introduce Port resource request extension
    The resources needed by a port are expressed to nova via
    resource_request extending the port.
    If the port requested by nova boot has resource_request attribute, that
    means that placement must enforce the minimum bandwidth requirements.
    
    Change-Id: I82afa56fa784fc43fefd1ce494bff649f51ad1c8
    Depends-On: https://review.openstack.org/584903
    Partial-Bug: #1578989
    See-Also: https://review.openstack.org/502306 (nova spec)
    See-Also: https://review.openstack.org/508149 (neutron spec)

---
+++ neutron/extensions/port_resource_request.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2018 Ericsson
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron_lib.api.definitions import port_resource_request
+from neutron_lib.api import extensions as api_extensions
+
+
+class Port_resource_request(api_extensions.APIExtensionDescriptor):
+    api_definition = port_resource_request
