5c208a313a172a04b935579c93ee29425a553d2d
Boden R <bodenvmw@gmail.com>
2018-08-14 15:24:13 -0600

    shim model query hooks to use neutron-lib
    This patch shims the model query hook functionality to use neutron-lib.
    A shim is used as the query hooks are cached in a shared dict and by
    shimming them to use neutron-lib we can let consumers move over to
    neutron-lib's version at will. Once all current consumers are moved
    over to lib, we can remove the shim and consume from lib directly.
    
    The DBQueryHooksFixture is also used in this patch to replace the
    existing cleanup function in the unit tests.
    
    Change-Id: I18a4e836ce89b607b5452527e5f8dd172cad6b4f

--- neutron/tests/base.py
+++ neutron/tests/base.py
@@ -47,7 +47,6 @@ from neutron.api.rpc.callbacks.producer import registry as rpc_producer_reg
 from neutron.common import config
 from neutron.common import rpc as n_rpc
 from neutron.conf.agent import common as agent_config
-from neutron.db import _model_query as model_query
 from neutron.db import _resource_extend as resource_extend
 from neutron.db import agentschedulers_db
 from neutron import manager
@@ -184,6 +183,8 @@ class DietTestCase(base.BaseTestCase):
         # Make sure we see all relevant deprecation warnings when running tests
         self.useFixture(tools.WarningsFixture())
 
+        self.useFixture(fixture.DBQueryHooksFixture())
+
         # NOTE(ihrachys): oslotest already sets stopall for cleanup, but it
         # does it using six.moves.mock (the library was moved into
         # unittest.mock in Python 3.4). So until we switch to six.moves.mock
@@ -192,17 +193,12 @@ class DietTestCase(base.BaseTestCase):
         # six before removing the cleanup callback from here.
         self.addCleanup(mock.patch.stopall)
 
-        self.addCleanup(self.reset_model_query_hooks)
         self.addCleanup(self.reset_resource_extend_functions)
 
         self.addOnException(self.check_for_systemexit)
         self.orig_pid = os.getpid()
 
         tools.reset_random_seed()
-
-    @staticmethod
-    def reset_model_query_hooks():
-        model_query._model_query_hooks = {}
 
     @staticmethod
     def reset_resource_extend_functions():
