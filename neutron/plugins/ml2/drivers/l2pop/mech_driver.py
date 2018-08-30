ff7923f48d41ceccfcc3ab62558db0905b50cf06
Oleg Bondarev <obondarev@mirantis.com>
2018-08-30 12:25:07 +0400

    l2 pop: check for more than 1 first active port on a node
    With high concurrency more than 1 port may be activated on an
    OVS agent at the same time (like VM port + a DVR port),
    so the patch mitigates the condition by checking for 1 or 2
    first active ports.
    
    Given that the condition also contains "or self.agent_restarted(context)"
    which makes it True first 180 sec (by default) after agent restart,
    I believe the downside of changing 1 to 2 should be negligible.
    
    Please see bug for more details on the issue.
    
    Closes-Bug: #1789846
    Change-Id: Ieab0186cbe05185d47bbf5a31141563cf923f66f
    (cherry picked from commit b32db30874a7729c4e9209cfc18e106a7e9fc697)

--- neutron/plugins/ml2/drivers/l2pop/mech_driver.py
+++ neutron/plugins/ml2/drivers/l2pop/mech_driver.py
@@ -298,8 +298,11 @@ class L2populationMechanismDriver(api.MechanismDriver):
             segment, agent_ip, network_id)
         other_fdb_ports = other_fdb_entries[network_id]['ports']
 
-        if agent_active_ports == 1 or self.agent_restarted(context):
-            # First port activated on current agent in this network,
+        # with high concurrency more than 1 port may be activated on an agent
+        # at the same time (like VM port + a DVR port) so checking for 1 or 2
+        is_first_port = agent_active_ports in (1, 2)
+        if is_first_port or self.agent_restarted(context):
+            # First port(s) activated on current agent in this network,
             # we have to provide it with the whole list of fdb entries
             agent_fdb_entries = self._create_agent_fdb(port_context,
                                                        agent,
