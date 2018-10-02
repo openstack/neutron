0fa1d469799e98d309e7a8b965ca97753447e288
lizheng <lizheng_1@chinatelecom.cn>
2018-10-02 14:10:52 +0800

    Verify port_forwarding subnet and IP address both
    Free subnet can not remove from router if other router's
    subnets have port_forwarding. This patch fixed it by
    checking the router interface subnet and IP address.
    
    Co-Authored-By: LIU Yulong <i@liuyulong.me>
    Closes-Bug: #1799140
    Change-Id: Idace35126bb00139fa1f9f48be3aa3aab265b9d8
    (cherry picked from commit f5d3a4159bcd526384ab2063ad9082f1a0d291b1)

--- neutron/tests/unit/extensions/test_expose_port_forwarding_in_fip.py
+++ neutron/tests/unit/extensions/test_expose_port_forwarding_in_fip.py
@@ -28,6 +28,16 @@ L3_PLUGIN = 'neutron.tests.unit.extensions.test_l3.TestL3NatServicePlugin'
 CORE_PLUGIN = 'neutron.tests.unit.extensions.test_l3.TestNoL3NatPlugin'
 
 
+def _get_expected(ref):
+    want_fields = [apidef.INTERNAL_IP_ADDRESS, apidef.PROTOCOL,
+                   apidef.INTERNAL_PORT, apidef.EXTERNAL_PORT]
+    expect = {
+        key: value
+        for key, value in ref[apidef.RESOURCE_NAME].items()
+        if key in want_fields}
+    return expect
+
+
 class ExtendFipPortForwardingExtensionManager(object):
 
     def get_resources(self):
@@ -78,14 +88,70 @@ class TestExtendFipPortForwardingExtension(
                 self._router_interface_action('add', router['router']['id'],
                                               insub['subnet']['id'], None)
 
-                def _get_expected(ref):
-                    want_fields = [apidef.INTERNAL_IP_ADDRESS, apidef.PROTOCOL,
-                                   apidef.INTERNAL_PORT, apidef.EXTERNAL_PORT]
-                    expect = {
-                        key: value
-                        for key, value in ref[apidef.RESOURCE_NAME].items()
-                        if key in want_fields}
-                    return expect
+                with self.port(subnet=insub) as port1,\
+                        self.port(subnet=insub) as port2:
+                    update_dict1 = {
+                        apidef.INTERNAL_PORT_ID: port1['port']['id'],
+                        apidef.INTERNAL_IP_ADDRESS:
+                            port1['port']['fixed_ips'][0]['ip_address']}
+                    port_forwarding[apidef.RESOURCE_NAME].update(update_dict1)
+                    self.pf_plugin.create_floatingip_port_forwarding(
+                        ctx, fip['floatingip']['id'], port_forwarding)
+
+                    body = self._show('floatingips', fip['floatingip']['id'])
+                    self.assertEqual(
+                        1, len(body['floatingip'][apidef.COLLECTION_NAME]))
+
+                    expect_result1 = _get_expected(port_forwarding)
+                    self.assertEqual(
+                        expect_result1,
+                        body['floatingip'][apidef.COLLECTION_NAME][0])
+
+                    update_dict2 = {
+                        apidef.EXTERNAL_PORT: 2226,
+                        apidef.INTERNAL_PORT_ID: port2['port']['id'],
+                        apidef.INTERNAL_IP_ADDRESS:
+                            port2['port']['fixed_ips'][0]['ip_address']}
+                    port_forwarding[apidef.RESOURCE_NAME].update(update_dict2)
+                    self.pf_plugin.create_floatingip_port_forwarding(
+                        ctx, fip['floatingip']['id'], port_forwarding)
+
+                    body = self._show('floatingips', fip['floatingip']['id'])
+                    self.assertEqual(
+                        2, len(body['floatingip'][apidef.COLLECTION_NAME]))
+                    expect_result2 = _get_expected(port_forwarding)
+                    expect = [expect_result1, expect_result2]
+                    self.assertEqual(
+                        expect, body['floatingip'][apidef.COLLECTION_NAME])
+
+    def test_create_port_forwarding_and_remove_subnets(self):
+        port_forwarding = {
+            apidef.RESOURCE_NAME:
+                {apidef.EXTERNAL_PORT: 2225,
+                 apidef.INTERNAL_PORT: 25,
+                 apidef.INTERNAL_PORT_ID: None,
+                 apidef.PROTOCOL: "tcp",
+                 apidef.INTERNAL_IP_ADDRESS: None}}
+        ctx = context.get_admin_context()
+        kwargs = {'arg_list': (extnet_apidef.EXTERNAL,),
+                  extnet_apidef.EXTERNAL: True}
+        with self.network(**kwargs) as extnet, self.network() as innet:
+            with self.subnet(network=extnet, cidr='200.0.0.0/22'),\
+                    self.subnet(network=innet, cidr='10.0.0.0/24') as insub,\
+                    self.subnet(network=innet, cidr='10.0.8.0/24') as insub2,\
+                    self.subnet(network=innet, cidr='10.0.9.0/24') as insub3,\
+                    self.router() as router:
+                fip = self._make_floatingip(self.fmt, extnet['network']['id'])
+                # check the floatingip response contains port_forwarding field
+                self.assertIn(apidef.COLLECTION_NAME, fip['floatingip'])
+                self._add_external_gateway_to_router(router['router']['id'],
+                                                     extnet['network']['id'])
+                self._router_interface_action('add', router['router']['id'],
+                                              insub['subnet']['id'], None)
+                self._router_interface_action('add', router['router']['id'],
+                                              insub2['subnet']['id'], None)
+                self._router_interface_action('add', router['router']['id'],
+                                              insub3['subnet']['id'], None)
 
                 with self.port(subnet=insub) as port1,\
                         self.port(subnet=insub) as port2:
@@ -122,3 +188,10 @@ class TestExtendFipPortForwardingExtension(
                     expect = [expect_result1, expect_result2]
                     self.assertEqual(
                         expect, body['floatingip'][apidef.COLLECTION_NAME])
+
+                    self._router_interface_action(
+                        'remove', router['router']['id'],
+                        insub2['subnet']['id'], None)
+                    self._router_interface_action(
+                        'remove', router['router']['id'],
+                        insub3['subnet']['id'], None)
