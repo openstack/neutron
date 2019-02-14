bcc9769b0210ee44841dc6d8c93a85f431d86768
Slawek Kaplonski <skaplons@redhat.com>
2019-02-14 14:35:25 +0100

    Fix notification about arp entries for dvr routers
    In method _generate_arp_table_and_notify_agent in neutron.db.l3_dvr_db
    module notifiations about arp table was send only to one router
    connected to subnet.
    Now it will check if subnet is connected to more than one
    dvr router and will send same notification to all such routers.
    
    Closes-Bug: #1815913
    
    Change-Id: I6a7d7f6645a8a7b5219788d51e17d54844d145bc
    (cherry picked from commit 1f104a093c1f27ddb1967e72cd452d43120b4cc2)
    (cherry picked from commit 3af8e2719c772097c579c65fe088fea6a1f56a4d)

--- neutron/db/l3_dvr_db.py
+++ neutron/db/l3_dvr_db.py
@@ -780,16 +780,15 @@ class L3_NAT_with_dvr_db_mixin(l3_db.L3_NAT_db_mixin,
         """Generates the arp table entry and notifies the l3 agent."""
         ip_address = fixed_ip['ip_address']
         subnet = fixed_ip['subnet_id']
-        filters = {'fixed_ips': {'subnet_id': [subnet]},
-                   'device_owner': [const.DEVICE_OWNER_DVR_INTERFACE]}
-        ports = self._core_plugin.get_ports(context, filters=filters)
-        router_id = next((port['device_id'] for port in ports), None)
-        if not router_id:
-            return
         arp_table = {'ip_address': ip_address,
                      'mac_address': mac_address,
                      'subnet_id': subnet}
-        notifier(context, router_id, arp_table)
+        filters = {'fixed_ips': {'subnet_id': [subnet]},
+                   'device_owner': [const.DEVICE_OWNER_DVR_INTERFACE]}
+        ports = self._core_plugin.get_ports(context, filters=filters)
+        routers = [port['device_id'] for port in ports]
+        for router_id in routers:
+            notifier(context, router_id, arp_table)
 
     def _should_update_arp_entry_for_dvr_service_port(self, port_dict):
         # Check this is a valid VM or service port
