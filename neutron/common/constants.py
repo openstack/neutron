49df07c7039206c537c17f40140d290e1b28a3f4
LIU Yulong <liuyul@chinatelecom.cn>
2019-02-20 16:47:42 +0800

    Divide-and-conquer security group beasts
    In one specific compute node, the security group rules
    can be enormous quantity. This patch adds a step-by-step
    processing method to deal with the large number of the
    security group rules. And also changes or adds some LOG.
    
    Related-Bug: #1813703
    Related-Bug: #1813704
    Related-Bug: #1813707
    
    Conflicts:
    	neutron/common/constants.py
            neutron/agent/common/ovs_lib.py
    Conflicts:
    	neutron/agent/securitygroups_rpc.py
    	neutron/common/constants.py
    	neutron/plugins/ml2/drivers/openvswitch/agent/ovs_neutron_agent.py
    
    Conflicts:
    	neutron/agent/common/ovs_lib.py
    	neutron/common/constants.py
    
    Change-Id: I57bf27ec75cf848271c5a28b22beee12b8bd5faa
    (cherry picked from commit 6ac420df7eb3ed324669472c61fec41b3d9cb35b)
    (cherry picked from commit f5d110e15f60753d056da942414ca6ecd6b78d8a)
    (cherry picked from commit 5424b9a68cb3ac1fcc04ed8ae603c421bde2dee3)

--- neutron/common/constants.py
+++ neutron/common/constants.py
@@ -230,3 +230,15 @@ PORT_BINDING_STATUS_ACTIVE = 'ACTIVE'
 PORT_BINDING_STATUS_INACTIVE = 'INACTIVE'
 PORT_BINDING_STATUSES = (PORT_BINDING_STATUS_ACTIVE,
                          PORT_BINDING_STATUS_INACTIVE)
+
+# Number of resources for neutron agent side functions to deal
+# with large sets.
+# Setting this value does not count on special conditions, it is just a human
+# countable or scalable number. [1] gives us the method to test the scale
+# issue. And we have tested the value of 1000, 500, 200, 100. But for 100,
+# ovs-agent will have a lower timeout probability. And according to the
+# testing result, step size 100 can indeed cost about 10% much more time
+# than 500/1000. But such extra time looks inevitably needed to be sacrificed
+# for the restart success rate.
+# [1] http://paste.openstack.org/show/745685/
+AGENT_RES_PROCESSING_STEP = 100
