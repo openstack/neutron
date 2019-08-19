12760c94c89f654885d0432a4da37110c112be1a
Miguel Lavalle <miguel.lavalle@verizonmedia.com>
2019-08-18 20:27:15 -0500

    Adding LOG statements to debug 1838449
    Adding LOG statements to debug 1838449
    
    Change-Id: I6f9cbe2e6b4ea0f122f5f8318dbbc31fce6b61f4
    Related-Bug: #1838449

--- neutron/api/rpc/handlers/l3_rpc.py
+++ neutron/api/rpc/handlers/l3_rpc.py
@@ -116,6 +116,9 @@ class L3RpcCallback(object):
         router_ids = kwargs.get('router_ids')
         host = kwargs.get('host')
         context = neutron_context.get_admin_context()
+        LOG.debug('Sync routers for ids %(router_ids)s in %(host)s',
+                  {'router_ids': router_ids,
+                   'host': host})
         routers = self._routers_to_sync(context, router_ids, host)
         if extensions.is_extension_supported(
                 self.plugin, constants.PORT_BINDING_EXT_ALIAS):
@@ -125,6 +128,10 @@ class L3RpcCallback(object):
         pf_plugin = directory.get_plugin(plugin_constants.PORTFORWARDING)
         if pf_plugin:
             pf_plugin.sync_port_forwarding_fip(context, routers)
+        LOG.debug('The sync data for ids %(router_ids)s in %(host)s is: '
+                  '%(routers)s', {'router_ids': router_ids,
+                                  'host': host,
+                                  'routers': routers})
         return routers
 
     def _routers_to_sync(self, context, router_ids, host=None):
