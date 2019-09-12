47890e9b85826951387fedb57ab474bd20ab1c3b
Slawek Kaplonski <skaplons@redhat.com>
2019-09-12 22:02:52 +0200

    List SG rules which belongs to tenant's SG
    In case when user's security group contains rules created e.g.
    by admin, and such rules has got admin's tenant as tenant_id,
    owner of security group should be able to see those rules.
    Some time ago this was addressed for request:
    
    GET /v2.0/security-groups/<sec_group_id>
    
    But it is also required to behave in same way for
    
    GET /v2.0/security-group-rules
    
    So this patch fixes this behaviour for listing of security
    group rules.
    To achieve that this patch also adds new policy rule:
    ADMIN_OWNER_OR_SG_OWNER which is similar to already existing
    ADMIN_OWNER_OR_NETWORK_OWNER used e.g. for listing or creating
    ports.
    
    Change-Id: I09114712582d2d38d14cf1683b87a8ce3a8e8c3c
    Closes-Bug: #1824248
    (cherry picked from commit b898d2e3c08b50e576ee849fbe8614c66f360c62)

--- neutron/db/securitygroups_db.py
+++ neutron/db/securitygroups_db.py
@@ -680,8 +680,13 @@ class SecurityGroupDbMixin(ext_sg.SecurityGroupPluginBase,
         pager = base_obj.Pager(
             sorts=sorts, marker=marker, limit=limit, page_reverse=page_reverse)
 
+        # NOTE(slaweq): use admin context here to be able to get all rules
+        # which fits filters' criteria. Later in policy engine rules will be
+        # filtered and only those which are allowed according to policy will
+        # be returned
         rule_objs = sg_obj.SecurityGroupRule.get_objects(
-            context, _pager=pager, validate_filters=False, **filters
+            context_lib.get_admin_context(), _pager=pager,
+            validate_filters=False, **filters
         )
         return [
             self._make_security_group_rule_dict(obj.db_obj, fields)
@@ -696,7 +701,12 @@ class SecurityGroupDbMixin(ext_sg.SecurityGroupPluginBase,
 
     @db_api.retry_if_session_inactive()
     def get_security_group_rule(self, context, id, fields=None):
-        security_group_rule = self._get_security_group_rule(context, id)
+        # NOTE(slaweq): use admin context here to be able to get all rules
+        # which fits filters' criteria. Later in policy engine rules will be
+        # filtered and only those which are allowed according to policy will
+        # be returned
+        security_group_rule = self._get_security_group_rule(
+            context_lib.get_admin_context(), id)
         return self._make_security_group_rule_dict(
             security_group_rule.db_obj, fields)
 
