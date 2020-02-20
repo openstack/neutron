3d3b61f8792277b303e10bce51512d9a73ef187e
Maciej Józefczyk <mjozefcz@redhat.com>
2020-02-20 11:27:13 +0000

    Revert "[OVN] Set 'unknown' address properly when port sec is disabled"
    We can now revert this patch, because main cause has been already
    fixed in Core OVN [1]. With this fix the ARP responder flows are not
    installed on LS pipeline, when LSP has port security disabled, and 
    an 'unknown' address is set in addresses column.
    This makes MAC spoofing possible.
    
    
    [1] https://patchwork.ozlabs.org/patch/1258152/
    
    
    This reverts commit 03b87ad963d5d8165a92e5c7c284c1517333dd00.
    
    
    
    Change-Id: Ie4c87d325b671348e133d62818d99af147d50ca2
    Closes-Bug: #1864027

--- neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/ovn_client.py
+++ neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/ovn_client.py
@@ -277,10 +277,7 @@ class OVNClient(object):
                 # OVN allows any mac address from a port if "unknown"
                 # is added to the Logical_Switch_Port.addresses column.
                 # So add it.
-                addresses = [ovn_const.UNKNOWN_ADDR]
-            else:
-                addresses = [address]
-                addresses.extend(new_macs)
+                addresses.append(ovn_const.UNKNOWN_ADDR)
 
         dhcpv4_options = self._get_port_dhcp_options(port, const.IP_VERSION_4)
         dhcpv6_options = self._get_port_dhcp_options(port, const.IP_VERSION_6)
