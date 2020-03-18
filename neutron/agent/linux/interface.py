e79afe115cb4db588fdd6bbd7500775173fd88c5
Alexander Vlasov <avlasov@mirantis.com>
2020-03-18 13:35:20 -0500

    Workaround for TCP checksum issue with ovs-dpdk and  veth pair
    The need for this change stems from following issues:
    1) When ovs_use_veth = False with ovs-dpdk issue with ovs
    was observed - after vswitch restart interface is not comming up.
    Meaning ovs-dpdk uses ovs internal ports and it is not able to bring
    them up on restart.
    2) When ovs_use_veth = True and ovs-dpkd is used, packets sent with
    incorrect checksum due to the fact that ovs-dpdk does not do checksum
    calculations for veth interface.
    
    This commit allows to use second option and resolve checksum issue by
    disabling checksum offload.
    
    Closes-Bug: #1832021
    Related-Bug: #1831935
    
    Change-Id: Iecce8d2c6c2c46718cc1020c6e8f914cd4560e4b
    (cherry picked from commit 11838a2bc50caa40e776bce211f5f2d1d16a14af)

--- neutron/agent/linux/interface.py
+++ neutron/agent/linux/interface.py
@@ -25,8 +25,12 @@ from pyroute2.netlink import exceptions as pyroute2_exc
 import six
 
 from neutron.agent.common import ovs_lib
+from neutron.agent.linux import ethtool
 from neutron.agent.linux import ip_lib
 from neutron.common import utils
+from neutron.conf.plugins.ml2.drivers import ovs_conf
+from neutron.plugins.ml2.drivers.openvswitch.agent.common \
+    import constants as ovs_const
 
 LOG = logging.getLogger(__name__)
 
@@ -346,6 +350,7 @@ class OVSInterfaceDriver(LinuxInterfaceDriver):
 
     def __init__(self, conf, **kwargs):
         super(OVSInterfaceDriver, self).__init__(conf, **kwargs)
+        ovs_conf.register_ovs_agent_opts(self.conf)
         if self.conf.ovs_use_veth:
             self.DEV_NAME_PREFIX = 'ns-'
 
@@ -433,6 +438,11 @@ class OVSInterfaceDriver(LinuxInterfaceDriver):
         if link_up:
             ns_dev.link.set_up()
         if self.conf.ovs_use_veth:
+            # ovs-dpdk does not do checksum calculations for veth interface
+            # (bug 1832021)
+            if self.conf.OVS.datapath_type == ovs_const.OVS_DATAPATH_NETDEV:
+                ethtool.Ethtool.offload(ns_dev.name, rx=False, tx=False,
+                                        namespace=namespace)
             root_dev.link.set_up()
 
     def unplug(self, device_name, bridge=None, namespace=None, prefix=None):
