2b2abfc76a4993f25624e10a1403cfabfa53abd9
Rodolfo Alonso Hernandez <ralonsoh@redhat.com>
2020-05-11 17:59:48 +0000

    Use pyroute2 for SRIOV VF commands
    Recent changes in some versions of iproute2 CLI output (v4.18),
    have invalidated the regular expression used to parse the
    "ip link" output.
    
    To solve this problem and avoid future ones, pyroute2 is used to
    retrieve the virtual functions information and set the VF attributes
    (spoofcheck, min_tx_rate, max_tx_rate and link_state).
    
    pyroute2 extended the "ip link" support to retrieve this information,
    adding "ext_mask=1" in the get command. If no virtual functions are
    present in this particular network interface, the added method,
    "get_link_vfs",  will return an empty list.
    
    The set commands can return a "InterfaceOperationNotSupported" in
    case the operation is not supported. For min_tx_rate, if the driver
    does not support to set a minimum bandwidth, an "InvalidArgument"
    (from a pyroute2.NetlinkError(22)) exception will be raised.
    
    Conflicts:
        neutron/tests/unit/agent/linux/test_ip_link_support.py
    
    Change-Id: I680da4f64bd114f1caecaaeedbf8a4b1915a0849
    Closes-Bug: #1878042
    (cherry picked from commit c5d8fd6329e405d1d584f4fedd7d0ede7da278da)

--- neutron/plugins/ml2/drivers/mech_sriov/agent/pci_lib.py
+++ neutron/plugins/ml2/drivers/mech_sriov/agent/pci_lib.py
@@ -13,64 +13,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import re
+import enum
 
 from oslo_log import log as logging
 
 from neutron.agent.linux import ip_lib
-from neutron.plugins.ml2.drivers.mech_sriov.agent.common \
-    import exceptions as exc
+
 
 LOG = logging.getLogger(__name__)
 
 
-class LinkState(object):
-    ENABLE = "enable"
-    DISABLE = "disable"
-    AUTO = "auto"
+class LinkState(enum.Enum):
+    auto = 0
+    enable = 1
+    disable = 2
 
 
 class PciDeviceIPWrapper(ip_lib.IPWrapper):
-    """Wrapper class for ip link commands.
-
-    wrapper for getting/setting pci device details using ip link...
-    """
-    VF_PATTERN = r"^vf\s+(?P<vf_index>\d+)\s+"
-    MAC_PATTERN = r"MAC\s+(?P<mac>[a-fA-F0-9:]+),"
-    STATE_PATTERN = r"\s+link-state\s+(?P<state>\w+)"
-    ANY_PATTERN = ".*,"
-
-    VF_LINE_FORMAT = VF_PATTERN + MAC_PATTERN + ANY_PATTERN + STATE_PATTERN
-    VF_DETAILS_REG_EX = re.compile(VF_LINE_FORMAT)
-
-    IP_LINK_OP_NOT_SUPPORTED = 'RTNETLINK answers: Operation not supported'
+    """Wrapper class for ip link commands related to virtual functions."""
 
     def __init__(self, dev_name):
         super(PciDeviceIPWrapper, self).__init__()
         self.dev_name = dev_name
-
-    def _set_feature(self, vf_index, feature, value):
-        """Sets vf feature
-
-        Checks if the feature is not supported or there's some
-        general error during ip link invocation and raises
-        exception accordingly.
-
-        :param vf_index: vf index
-        :param feature: name of a feature to be passed to ip link,
-                        such as 'state' or 'spoofchk'
-        :param value: value of the feature setting
-        """
-        try:
-            self._as_root([], "link", ("set", self.dev_name, "vf",
-                                       str(vf_index), feature, value))
-        except Exception as e:
-            if self.IP_LINK_OP_NOT_SUPPORTED in str(e):
-                raise exc.IpCommandOperationNotSupportedError(
-                    dev_name=self.dev_name)
-            else:
-                raise exc.IpCommandError(dev_name=self.dev_name,
-                                         reason=str(e))
 
     def get_assigned_macs(self, vf_list):
         """Get assigned mac addresses for vf list.
@@ -78,21 +42,13 @@ class PciDeviceIPWrapper(ip_lib.IPWrapper):
         @param vf_list: list of vf indexes
         @return: dict mapping of vf to mac
         """
-        try:
-            out = self._as_root([], "link", ("show", self.dev_name))
-        except Exception as e:
-            LOG.exception("Failed executing ip command")
-            raise exc.IpCommandError(dev_name=self.dev_name,
-                                     reason=e)
+        ip = self.device(self.dev_name)
+        vfs = ip.link.get_vfs()
         vf_to_mac_mapping = {}
-        vf_lines = self._get_vf_link_show(vf_list, out)
-        if vf_lines:
-            for vf_line in vf_lines:
-                vf_details = self._parse_vf_link_show(vf_line)
-                if vf_details:
-                    vf_num = vf_details.get('vf')
-                    vf_mac = vf_details.get("MAC")
-                    vf_to_mac_mapping[vf_num] = vf_mac
+        for vf_num in vf_list:
+            if vfs.get(vf_num):
+                vf_to_mac_mapping[vf_num] = vfs[vf_num]['mac']
+
         return vf_to_mac_mapping
 
     def get_vf_state(self, vf_index):
@@ -100,88 +56,48 @@ class PciDeviceIPWrapper(ip_lib.IPWrapper):
 
         @param vf_index: vf index
         """
-        try:
-            out = self._as_root([], "link", ("show", self.dev_name))
-        except Exception as e:
-            LOG.exception("Failed executing ip command")
-            raise exc.IpCommandError(dev_name=self.dev_name,
-                                     reason=e)
-        vf_lines = self._get_vf_link_show([vf_index], out)
-        if vf_lines:
-            vf_details = self._parse_vf_link_show(vf_lines[0])
-            if vf_details:
-                state = vf_details.get("link-state",
-                                       LinkState.DISABLE)
-            if state in (LinkState.AUTO, LinkState.ENABLE):
-                return state
-        return LinkState.DISABLE
+        ip = self.device(self.dev_name)
+        vfs = ip.link.get_vfs()
+        vf = vfs.get(vf_index)
+        if vf:
+            return LinkState(int(vf['link_state'])).name
+
+        return LinkState.disable.name
 
     def set_vf_state(self, vf_index, state, auto=False):
         """sets vf state.
 
         @param vf_index: vf index
-        @param state: required state {True/False}
+        @param state: required state {True: enable (1)
+                                      False: disable (2)}
+        @param auto: set link_state to auto (0)
         """
+        ip = self.device(self.dev_name)
         if auto:
-            status_str = LinkState.AUTO
+            link_state = 0
         else:
-            status_str = LinkState.ENABLE if state else \
-                LinkState.DISABLE
-        self._set_feature(vf_index, "state", status_str)
+            link_state = 1 if state else 2
+        vf_config = {'vf': vf_index, 'link_state': link_state}
+        ip.link.set_vf_feature(vf_config)
 
     def set_vf_spoofcheck(self, vf_index, enabled):
         """sets vf spoofcheck
 
         @param vf_index: vf index
-        @param enabled: True to enable spoof checking,
-                        False to disable
+        @param enabled: True to enable (1) spoof checking,
+                        False to disable (0)
         """
-        setting = "on" if enabled else "off"
-        self._set_feature(vf_index, "spoofchk", setting)
+        ip = self.device(self.dev_name)
+        vf_config = {'vf': vf_index, 'spoofchk': int(enabled)}
+        ip.link.set_vf_feature(vf_config)
 
     def set_vf_rate(self, vf_index, rate_type, rate_value):
         """sets vf rate.
 
         @param vf_index: vf index
-        @param rate_type: vf rate type ('rate', 'min_tx_rate')
+        @param rate_type: vf rate type ('max_tx_rate', 'min_tx_rate')
         @param rate_value: vf rate in Mbps
         """
-        self._set_feature(vf_index, rate_type, str(rate_value))
-
-    def _get_vf_link_show(self, vf_list, link_show_out):
-        """Get link show output for VFs
-
-        get vf link show command output filtered by given vf list
-        @param vf_list: list of vf indexes
-        @param link_show_out: link show command output
-        @return: list of output rows regarding given vf_list
-        """
-        vf_lines = []
-        for line in link_show_out.split("\n"):
-            line = line.strip()
-            if line.startswith("vf"):
-                details = line.split()
-                index = int(details[1])
-                if index in vf_list:
-                    vf_lines.append(line)
-        if not vf_lines:
-            LOG.warning("Cannot find vfs %(vfs)s in device %(dev_name)s",
-                        {'vfs': vf_list, 'dev_name': self.dev_name})
-        return vf_lines
-
-    def _parse_vf_link_show(self, vf_line):
-        """Parses vf link show command output line.
-
-        @param vf_line: link show vf line
-        """
-        vf_details = {}
-        pattern_match = self.VF_DETAILS_REG_EX.match(vf_line)
-        if pattern_match:
-            vf_details["vf"] = int(pattern_match.group("vf_index"))
-            vf_details["MAC"] = pattern_match.group("mac")
-            vf_details["link-state"] = pattern_match.group("state")
-        else:
-            LOG.warning("failed to parse vf link show line %(line)s: "
-                        "for %(device)s",
-                        {'line': vf_line, 'device': self.dev_name})
-        return vf_details
+        ip = self.device(self.dev_name)
+        vf_config = {'vf': vf_index, 'rate': {rate_type: int(rate_value)}}
+        ip.link.set_vf_feature(vf_config)
