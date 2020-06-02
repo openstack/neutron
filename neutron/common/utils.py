c874c44cdc0dc32ae1bdcedfce4a2a9319d8e1eb
Rodolfo Alonso Hernandez <ralonsoh@redhat.com>
2020-06-02 11:10:21 +0000

    Make NeutronOvsdbIdl singleton
    "NeutronOvsdbIdl" should be a singleton class; only one instance
    should be shared everywhere is called. Currently the connection
    and the "OvsIdlMonitor" instances are global; those instances are
    used to declare the "NeutronOvsdbIdl" instance.
    
    As commented in the related bug, this singleton will ensure that
    the OvsIdl indexes are created just once.
    
    Related-Bug: #1881424
    
    Conflicts:
        neutron/common/utils.py
    
    Change-Id: I639cf673a983b7b1be810495d8a8c2d89919a9b6
    (cherry picked from commit 178227c8b6636433fcdab4dfd342468335b76aa6)

--- neutron/common/utils.py
+++ neutron/common/utils.py
@@ -987,3 +987,15 @@ def timecost(f):
         ret = f(*args, **kwargs)
         return ret
     return wrapper
+
+
+class SingletonDecorator(object):
+
+    def __init__(self, klass):
+        self._klass = klass
+        self._instance = None
+
+    def __call__(self, *args, **kwargs):
+        if self._instance is None:
+            self._instance = self._klass(*args, **kwargs)
+        return self._instance
