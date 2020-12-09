7339fca5eb4eedbba68e1d7786c92412605a763f
Slawek Kaplonski <skaplons@redhat.com>
2020-12-10 00:10:38 +0100

    Fix multicast traffic with IGMP snooping enabled
    In the ML2/OVS when igmp_snooping is enabled but there is no
    external querier multicast traffic will stop working after few minutes
    as packets will not be flooded to tunnel/external bridges.
    
    So this patch sets "mcast-snooping-disable-flood-unregistered" option
    of the br-int to False (default value) even when igmp_snooping is
    enabled in the neutron-ovs-agent's config file.
    
    Additionally it configures "mcast-snooping-flood-reports" and
    "mcast-snooping-flood" on patch ports in br-int to True.
    
    That way we can provide best effort snooping: multicast isolation where
    IGMP queriers are available and flood everywhere else?
    
    Conflicts:
        neutron/plugins/ml2/drivers/openvswitch/agent/ovs_neutron_agent.py
        neutron/tests/functional/agent/common/test_ovs_lib.py
        neutron/tests/unit/plugins/ml2/drivers/openvswitch/agent/test_ovs_neutron_agent.py
    
    Closes-Bug: #1884723
    Change-Id: Iefa0044dba9e92592295a79448e5d57d9e14a40b
    (cherry picked from commit b4070c975274f53a4a2caaabeb5af55683232d3d)

--- neutron/tests/unit/plugins/ml2/drivers/openvswitch/agent/test_ovs_tunnel.py
+++ neutron/tests/unit/plugins/ml2/drivers/openvswitch/agent/test_ovs_tunnel.py
@@ -234,6 +234,8 @@ class TunnelTest(object):
             mock.call.port_exists('int-%s' % self.MAP_TUN_BRIDGE),
             mock.call.add_patch_port('int-%s' % self.MAP_TUN_BRIDGE,
                                      constants.NONEXISTENT_PEER),
+            mock.call.set_igmp_snooping_flood('int-%s' % self.MAP_TUN_BRIDGE,
+                                              igmp_snooping),
         ]
 
         self.mock_int_bridge_expected += [
@@ -263,6 +265,7 @@ class TunnelTest(object):
         self.mock_int_bridge_expected += [
             mock.call.port_exists('patch-tun'),
             mock.call.add_patch_port('patch-tun', 'patch-int'),
+            mock.call.set_igmp_snooping_flood('patch-tun', igmp_snooping),
         ]
         self.mock_int_bridge_expected += [
             mock.call.get_vif_ports((ovs_lib.INVALID_OFPORT,
@@ -719,7 +722,9 @@ class TunnelTestUseVethInterco(TunnelTest):
         self.mock_int_bridge_expected += [
             mock.call.db_get_val('Interface', 'int-%s' % self.MAP_TUN_BRIDGE,
                                  'type', log_errors=False),
-            mock.call.add_port('int-%s' % self.MAP_TUN_BRIDGE)
+            mock.call.add_port('int-%s' % self.MAP_TUN_BRIDGE),
+            mock.call.set_igmp_snooping_flood('int-%s' % self.MAP_TUN_BRIDGE,
+                                              igmp_snooping),
         ]
 
         self.mock_int_bridge_expected += [
@@ -742,7 +747,8 @@ class TunnelTestUseVethInterco(TunnelTest):
         ]
         self.mock_int_bridge_expected += [
             mock.call.port_exists('patch-tun'),
-            mock.call.add_patch_port('patch-tun', 'patch-int')
+            mock.call.add_patch_port('patch-tun', 'patch-int'),
+            mock.call.set_igmp_snooping_flood('patch-tun', igmp_snooping),
         ]
         self.mock_int_bridge_expected += [
             mock.call.get_vif_ports((ovs_lib.INVALID_OFPORT,
