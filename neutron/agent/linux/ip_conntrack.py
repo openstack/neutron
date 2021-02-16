496a5a166ddff7b879cabfc359b68e11f93821bf
Slawek Kaplonski <skaplons@redhat.com>
2021-02-16 11:19:44 +0100

    [OVS FW] Clean conntrack entries with mark == CT_MARK_INVALID
    In case when security group is removed from port, connections
    established to/from port which were hitting some of the removed SG
    rules will be marked with CT_MARK_INVALID in the conntrack table.
    Due to that such existing connections will be stopped.
    But then if SG will be added again to the port, those conntrack entries
    marked as invalid will not be removed and those hang connections will
    not be revived.
    To fix that, this patch adds deletion of the conntrack entries marked
    with CT_MARK_INVALID and related to the port of which SG are
    updated/added.
    
    Closes-Bug: #1915530
    Change-Id: I84718b8ac4d8d6e39426e1c4485c2d7fe28185dd
    (cherry picked from commit 1e220d7136965acbfe50215d982cac0bd3dcf9c3)

--- neutron/agent/linux/ip_conntrack.py
+++ neutron/agent/linux/ip_conntrack.py
@@ -116,10 +116,13 @@ class IpConntrackManager(object):
         ethertype = rule.get('ethertype')
         protocol = rule.get('protocol')
         direction = rule.get('direction')
+        mark = rule.get('mark')
         cmd = ['conntrack', '-D']
         if protocol:
             cmd.extend(['-p', str(protocol)])
         cmd.extend(['-f', str(ethertype).lower()])
+        if mark is not None:
+            cmd.extend(['-m', str(mark)])
         cmd.append('-d' if direction == 'ingress' else '-s')
         cmd_ns = []
         if namespace:
@@ -170,10 +173,12 @@ class IpConntrackManager(object):
         self._process(device_info_list, rule)
 
     def delete_conntrack_state_by_remote_ips(self, device_info_list,
-                                             ethertype, remote_ips):
+                                             ethertype, remote_ips, mark=None):
         for direction in ['ingress', 'egress']:
             rule = {'ethertype': str(ethertype).lower(),
                     'direction': direction}
+            if mark:
+                rule['mark'] = mark
             self._process(device_info_list, rule, remote_ips)
 
     def _populate_initial_zone_map(self):
@@ -251,3 +256,21 @@ class IpConntrackManager(object):
                 return index + ZONE_START
         # conntrack zones exhausted :( :(
         raise exceptions.CTZoneExhaustedError()
+
+
+class OvsIpConntrackManager(IpConntrackManager):
+
+    def __init__(self, execute=None):
+        super(OvsIpConntrackManager, self).__init__(
+            get_rules_for_table_func=None,
+            filtered_ports={}, unfiltered_ports={},
+            execute=execute, namespace=None, zone_per_port=False)
+
+    def _populate_initial_zone_map(self):
+        self._device_zone_map = {}
+
+    def get_device_zone(self, port, create=False):
+        of_port = port.get('of_port')
+        if of_port is None:
+            return
+        return of_port.vlan_tag
