9cba039b32e586fda2d6a854adc15643b066004f
Rodolfo Alonso Hernandez <ralonsoh@redhat.com>
2021-07-12 15:30:47 +0000

    [OVN] Set NB/SB "connection" inactivity probe
    For each NB/SB databases, set the inactivity probe timeout value on
    the Neutron configured connection register.
    
    Neutron connects to the NB/SB databases with the value defined in
    "ovn_nb_connection"/"ovn_sb_connection" config parameters, with the
    format:
    - tcp:IP:PORT
    - ssl:IP:PORT
    - unix:socket_file
    
    The connection register "target" column has the following format:
    - ptcp:PORT:IP
    - pssl:PORT:IP
    - punix:socket_file
    
    Conflicts:
        neutron/common/ovn/utils.py
        neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/ovsdb_monitor.py
        neutron/tests/functional/plugins/ml2/drivers/ovn/mech_driver/test_mech_driver.py
    
    Closes-Bug: #1935842
    Change-Id: If82ff2c92b8bec18ae3e895ce19a65cfeed9c90d
    (cherry picked from commit 28f3017a90ecec6208aef9696cd7947972ec17d8)
    (cherry picked from commit f8aefd600b164f866b4a1dde25ae3459797b87d3)
    (cherry picked from commit f6eda4f96546e017ac3986a113dd2f5b227a9c79)

--- neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/ovsdb_monitor.py
+++ neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/ovsdb_monitor.py
@@ -544,6 +544,7 @@ class OvnSbIdl(OvnIdlDistributedLock):
         helper.register_table('Port_Binding')
         helper.register_table('Datapath_Binding')
         helper.register_table('MAC_Binding')
+        helper.register_table('Connection')
         return cls(driver, connection_string, helper)
 
     def post_connect(self):
