32cc39663ae68fa14ba01bcd38c783c9a8004f40
Nguyen Thanh Cong <ntcong1705@gmail.com>
2021-11-11 21:00:23 +0700

    [OVN] Fix port disable security dead when run neutron-ovn-db-sync-util
    Port disable security should not have been in acl neutron_pg_drop, but
    when run neutron-ovn-db-sync-util, port disable security still added
    to ACL neutron_pg_drop. It because port disable security is not
    trusted port.
    
    Co-authored-by: archiephan <chungphan7819@gmail.com>
    
    Closes-Bug: #1939723
    Change-Id: Iebce0929e3e68ac5be0acaf5cdac4f5833cb9f2f
    (cherry picked from commit 4511290b726f605384285228a28ad7b32a4b8c43)

--- neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/test_mech_driver.py
+++ neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/test_mech_driver.py
@@ -3019,7 +3019,12 @@ class TestOVNMechanismDriverSecurityGroup(
     # test acl methods invoking. Content correctness of args of acl methods
     # is mainly guaranteed by acl_test.py.
 
+    _extension_drivers = ['port_security']
+
     def setUp(self):
+        cfg.CONF.set_override('extension_drivers',
+                              self._extension_drivers,
+                              group='ml2')
         cfg.CONF.set_override('mechanism_drivers',
                               ['logger', 'ovn'],
                               'ml2')
@@ -3212,8 +3217,9 @@ class TestOVNMechanismDriverSecurityGroup(
             req = self.new_update_request('ports', data, p['id'])
             req.get_response(self.api)
 
+            # Default neutron_pg_drop, 2 security group
             self.assertEqual(
-                2, self.mech_driver._nb_ovn.pg_add_ports.call_count)
+                3, self.mech_driver._nb_ovn.pg_add_ports.call_count)
 
     def test_update_sg_change_rule(self):
         with self.network() as n, self.subnet(n):
