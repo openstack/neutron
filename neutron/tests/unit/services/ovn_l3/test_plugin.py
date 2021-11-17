34b4a8d5c894c48f98dbb4ec5469f6564167d7eb
Arnau Verdaguer <averdagu@redhat.com>
2021-11-17 16:59:28 +0100

    [OVN] Prevent deleting the only IP of a router port
    Neutron allows deleting the only IP of a router port but
    the OVN NB DB doesn't, since it expects that the
    network value of a port is greater than 0. This should
    not be possible since it causes that the DB are not
    perfectly sync.
    
    It is needed to check BEFORE_UPDATE if the port
    that will be updated is of type router owned and
    if it will have an IP after the update. If not
    an error needs to be raised.
    
    Comparet to master patch, this has changed payload to kwargs
    Conflicts:
        neutron/services/ovn_l3/plugin.py
    
    Closes-Bug: #1948457
    Change-Id: I206c31201470f178efdde8839622be7900c6ae3e
    (cherry picked from commit fa2179278f064f75e057f7020e4f0c34f358de1b)

--- neutron/tests/unit/services/ovn_l3/test_plugin.py
+++ neutron/tests/unit/services/ovn_l3/test_plugin.py
@@ -1379,12 +1379,14 @@ class TestOVNL3RouterPlugin(test_mech_driver.Ml2PluginV2TestCase):
                 'ovn_client.OVNClient.update_router_port')
     def test_port_update_postcommit(self, update_rp_mock):
         kwargs = {'port': {'device_owner': 'foo'},
+                  'original_port': '',
                   'context': 'fake_context'}
         self.l3_inst._port_update(resources.PORT, events.AFTER_UPDATE, None,
                                   **kwargs)
         update_rp_mock.assert_not_called()
 
         kwargs = {'port': {'device_owner': constants.DEVICE_OWNER_ROUTER_INTF},
+                  'original_port': '',
                   'context': 'fake_context'}
         self.l3_inst._port_update(resources.PORT, events.AFTER_UPDATE, None,
                                   **kwargs)
@@ -1392,6 +1394,26 @@ class TestOVNL3RouterPlugin(test_mech_driver.Ml2PluginV2TestCase):
         update_rp_mock.assert_called_once_with(kwargs['context'],
                                                kwargs['port'],
                                                if_exists=True)
+
+    def test_port_update_before_update_router_port_without_ip(self):
+        context = 'fake_context'
+        og_port = {'device_owner': constants.DEVICE_OWNER_ROUTER_INTF,
+                'fixed_ips': ['10.0.0.10'],
+                'id': 'port-id'}
+        port = {'device_owner': constants.DEVICE_OWNER_ROUTER_INTF,
+                'fixed_ips': [],
+                'id': 'port-id'}
+        kwargs = {'original_port': og_port,
+                  'port': port,
+                  'context': context}
+        self.assertRaises(
+            n_exc.ServicePortInUse,
+            self.l3_inst._port_update,
+            resources.PORT,
+            events.BEFORE_UPDATE,
+            None,
+            **kwargs,
+        )
 
     @mock.patch('neutron.plugins.ml2.plugin.Ml2Plugin.update_port_status')
     @mock.patch('neutron.plugins.ml2.plugin.Ml2Plugin.update_port')
