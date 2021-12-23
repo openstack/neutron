02a986a01f3701695a78b3392920fd0dd6b7b789
Rodolfo Alonso Hernandez <ralonsoh@redhat.com>
2021-12-23 11:00:09 +0000

    [OVN] Allow only one physical network per bridge
    Same as in other ML2 plugins (OVS, Linux Bridge), OVN mechanism driver
    should allow only one physical network per bridge. The rule "one
    network, one bridge" should be present in OVN too.
    
    By allowing only one physical network per bridge, Neutron prevents
    having two networks with subnets with the same CIDR in the same bridge.
    Currently is possible and this CIDR clash is not prevented (shouldn't be
    by the API). This architectural limitation prevents this situation.
    
    This limitation is already present in deployment tools as TripleO.
    
    Conflicts:
      neutron/tests/functional/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_impl_idl.py
    
    Closes-Bug: #1956476
    Change-Id: I74a2ca9a344a93219deb94d60247478ee3200659
    (cherry picked from commit 55afd9bc923f55bc8f621316a080ad4271182ebc)
    (cherry picked from commit eddb3a79025b8936292c0b89ca498d5d4899860c)

--- neutron/tests/functional/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_impl_idl.py
+++ neutron/tests/functional/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_impl_idl.py
@@ -36,7 +36,7 @@ class TestSbApi(base.FunctionalTestCase,
                 {'external_ids': {'ovn-bridge-mappings':
                                   'public:br-ex,private:br-0'}},
                 {'external_ids': {'ovn-bridge-mappings':
-                                  'public:br-ex,public2:br-ex'}},
+                                  'public:br-ex,public2:br-ex2'}},
                 {'external_ids': {'ovn-bridge-mappings':
                                   'public:br-ex'}},
             ]
@@ -85,6 +85,15 @@ class TestSbApi(base.FunctionalTestCase,
         self.assertLessEqual(len(self.data['chassis']), len(mapping))
         self.assertGreaterEqual(set(mapping.keys()),
                                 {c['name'] for c in self.data['chassis']})
+
+    def test_multiple_physnets_in_one_bridge(self):
+        self.data = {
+            'chassis': [
+                {'external_ids': {'ovn-bridge-mappings': 'p1:br-ex,p2:br-ex'}}
+            ]
+        }
+        self.load_test_data()
+        self.assertRaises(ValueError, self.api.get_chassis_and_physnets)
 
     def _add_switch_port(self, chassis_name, type='localport'):
         sname, pname = (utils.get_rand_device_name(prefix=p)
