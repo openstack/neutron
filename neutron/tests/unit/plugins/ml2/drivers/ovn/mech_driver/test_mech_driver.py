014c661924855080b028587e277c513729309fff
Rodolfo Alonso Hernandez <ralonsoh@redhat.com>
2022-10-06 07:14:06 +0200

    Load the required configuration options in the UT classes
    Some test classes are not loading the required configuration options
    during the setup process. That prevents from launching thoses tests
    or classes individually. This patch solves this issue by importing the
    required options in the "setUp" test class method.
    
    Closes-Bug: #1993502
    Change-Id: I79bf7fa85c7b29fa37f81feb51f3eb68cef3bdee
    (cherry picked from commit 7b8b797dea2514328b3f72cd1ad23ebb915d6cad)

--- neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/test_mech_driver.py
+++ neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/test_mech_driver.py
@@ -3670,6 +3670,7 @@ class TestOVNMechanismDriverSecurityGroup(MechDriverSetupBase,
     _extension_drivers = ['port_security']
 
     def setUp(self):
+        ovn_conf.register_opts()
         cfg.CONF.set_override('extension_drivers',
                               self._extension_drivers,
                               group='ml2')
