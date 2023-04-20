c3602ac19b62b77d3cb763746e124881d4c061e4
Elvira García <egarciar@redhat.com>
2023-04-20 16:39:12 +0200

    [OVN] Update ovn meter when neutron server reloads
    Up until now, we needed to remove all logging objects to see the
    meter-band properties being changed after a server restart. Now we check
    for inconsistencies between the neutron configuration and the OVN
    meter-band object after a restart. The function create_ovn_fair_meter is
    now located in the ovn_driver instead of the log_driver so as to be able
    to call it from the maintenance task.
    
    Closes-bug: #2017145
    Signed-off-by: Elvira García <egarciar@redhat.com>
    Change-Id: I24cef85ed68c893a740445707f88296d763c8de8

--- neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_ovn_client.py
+++ neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_ovn_client.py
@@ -21,9 +21,12 @@ from neutron.conf.plugins.ml2.drivers.ovn import ovn_conf
 from neutron.plugins.ml2.drivers.ovn.mech_driver.ovsdb import ovn_client
 from neutron.tests import base
 from neutron.tests.unit import fake_resources as fakes
+from neutron.tests.unit.services.logapi.drivers.ovn \
+    import test_driver as test_log_driver
 from neutron_lib.api.definitions import l3
 from neutron_lib.api.definitions import portbindings
 from neutron_lib import constants as const
+from neutron_lib.services.logapi import constants as log_const
 
 
 class TestOVNClientBase(base.BaseTestCase):
@@ -191,3 +194,61 @@ class TestOVNClientDetermineBindHost(TestOVNClientBase):
         self.assertEqual(
             self.fake_smartnic_hostname,
             self.ovn_client.determine_bind_host({}, port_context=context))
+
+
+class TestOVNClientFairMeter(TestOVNClientBase,
+                             test_log_driver.TestOVNDriverBase):
+
+    def test_create_ovn_fair_meter(self):
+        mock_find_rows = mock.Mock()
+        mock_find_rows.execute.return_value = None
+        self.nb_idl.db_find_rows.return_value = mock_find_rows
+        self.ovn_client.create_ovn_fair_meter(self._log_driver.meter_name)
+        self.assertFalse(self.nb_idl.meter_del.called)
+        self.assertTrue(self.nb_idl.meter_add.called)
+        self.nb_idl.meter_add.assert_called_once_with(
+            name=self._log_driver.meter_name,
+            unit="pktps",
+            rate=self.fake_cfg_network_log.rate_limit,
+            fair=True,
+            burst_size=self.fake_cfg_network_log.burst_limit,
+            may_exist=False,
+            external_ids={constants.OVN_DEVICE_OWNER_EXT_ID_KEY:
+                          log_const.LOGGING_PLUGIN})
+
+    def test_create_ovn_fair_meter_unchanged(self):
+        mock_find_rows = mock.Mock()
+        mock_find_rows.execute.return_value = [self._fake_meter()]
+        self.nb_idl.db_find_rows.return_value = mock_find_rows
+        self.nb_idl.lookup.side_effect = lambda table, key, default: (
+            self._fake_meter_band() if key == "test_band" else default)
+        self.ovn_client.create_ovn_fair_meter(self._log_driver.meter_name)
+        self.assertFalse(self.nb_idl.meter_del.called)
+        self.assertFalse(self.nb_idl.meter_add.called)
+
+    def test_create_ovn_fair_meter_changed(self):
+        mock_find_rows = mock.Mock()
+        mock_find_rows.execute.return_value = [self._fake_meter(fair=[False])]
+        self.nb_idl.db_find_rows.return_value = mock_find_rows
+        self.nb_idl.lookup.return_value = self._fake_meter_band()
+        self.ovn_client.create_ovn_fair_meter(self._log_driver.meter_name)
+        self.assertTrue(self.nb_idl.meter_del.called)
+        self.assertTrue(self.nb_idl.meter_add.called)
+
+    def test_create_ovn_fair_meter_band_changed(self):
+        mock_find_rows = mock.Mock()
+        mock_find_rows.execute.return_value = [self._fake_meter()]
+        self.nb_idl.db_find_rows.return_value = mock_find_rows
+        self.nb_idl.lookup.return_value = self._fake_meter_band(rate=666)
+        self.ovn_client.create_ovn_fair_meter(self._log_driver.meter_name)
+        self.assertTrue(self.nb_idl.meter_del.called)
+        self.assertTrue(self.nb_idl.meter_add.called)
+
+    def test_create_ovn_fair_meter_band_missing(self):
+        mock_find_rows = mock.Mock()
+        mock_find_rows.execute.return_value = [self._fake_meter()]
+        self.nb_idl.db_find_rows.return_value = mock_find_rows
+        self.nb_idl.lookup.side_effect = None
+        self.ovn_client.create_ovn_fair_meter(self._log_driver.meter_name)
+        self.assertTrue(self.nb_idl.meter_del.called)
+        self.assertTrue(self.nb_idl.meter_add.called)
