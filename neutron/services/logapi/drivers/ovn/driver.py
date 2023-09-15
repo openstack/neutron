53484b897b76d3dd8a9f8c1992591dfb52f1dece
Elvira García <egarciar@redhat.com>
2023-09-15 16:33:43 +0200

    Use safer methods to get security groups on security group logging
    There is a chance on real environment that a port group doesn't have any
    correspondent security group (and there are maintenance tasks that will
    remove them). This patch avoids a DriverError from Neutron in case we
    are in an environment with a port group that was mistakenly left over
    due to any reason. Instead, a Warning log will be raised.
    
    Related-bug: #2032929
    Change-Id: I42208557c8522d6fbc29df8a3c7d0367cace31e4
    (cherry picked from commit 67bd591c5b9280f1af7c8a4942dcbb3bd1270cfb)

--- neutron/services/logapi/drivers/ovn/driver.py
+++ neutron/services/logapi/drivers/ovn/driver.py
@@ -201,12 +201,16 @@ class OVNDriver(base.DriverBase):
         acl_changes, acl_visits = 0, 0
         for pg in pgs:
             meter_name = self.meter_name
-            if ovn_const.OVN_DROP_PORT_GROUP_NAME not in pg["name"]:
-                stateful = (sg_obj.SecurityGroup
-                            .get_sg_by_id(context, pg["name"]
-                                          .replace('pg_', '', 1)
-                                          .replace('_', '-')).stateful)
-                if not stateful:
+            if pg["name"] != ovn_const.OVN_DROP_PORT_GROUP_NAME:
+                sg = sg_obj.SecurityGroup.get_sg_by_id(
+                    context,
+                    pg["external_ids"][ovn_const.OVN_SG_EXT_ID_KEY])
+                if not sg:
+                    LOG.warning("Port Group %s is missing a corresponding "
+                                "security group, skipping its network log "
+                                "setting...", pg["name"])
+                    continue
+                if not sg.stateful:
                     meter_name = meter_name + ("_stateless")
             for acl_uuid in pg["acls"]:
                 acl_visits += 1
@@ -241,7 +245,8 @@ class OVNDriver(base.DriverBase):
 
     def _pgs_all(self):
         return self.ovn_nb.db_list(
-            "Port_Group", columns=["name", "acls"]).execute(check_error=True)
+            "Port_Group",
+            columns=["name", "external_ids", "acls"]).execute(check_error=True)
 
     def _pgs_from_log_obj(self, context, log_obj):
         """Map Neutron log_obj into affected port groups in OVN.
@@ -260,11 +265,13 @@ class OVNDriver(base.DriverBase):
                 # No sg, no port, DROP: return DROP pg
                 if log_obj.event == log_const.DROP_EVENT:
                     return [{"name": pg_drop.name,
-                        "acls": [r.uuid for r in pg_drop.acls]}]
+                             "external_ids": pg_drop.external_ids,
+                             "acls": [r.uuid for r in pg_drop.acls]}]
                 # No sg, no port, ACCEPT: return all except DROP pg
                 pgs = self._pgs_all()
                 pgs.remove({"name": pg_drop.name,
-                    "acls": [r.uuid for r in pg_drop.acls]})
+                            "external_ids": pg_drop.external_ids,
+                            "acls": [r.uuid for r in pg_drop.acls]})
                 return pgs
             except idlutils.RowNotFound:
                 pass
@@ -276,6 +283,7 @@ class OVNDriver(base.DriverBase):
                 pg = self.ovn_nb.lookup("Port_Group",
                                         ovn_const.OVN_DROP_PORT_GROUP_NAME)
                 pgs.append({"name": pg.name,
+                            "external_ids": pg.external_ids,
                             "acls": [r.uuid for r in pg.acls]})
             except idlutils.RowNotFound:
                 pass
@@ -288,6 +296,7 @@ class OVNDriver(base.DriverBase):
                                         utils.ovn_port_group_name(
                                             log_obj.resource_id))
                 pgs.append({"name": pg.name,
+                            "external_ids": pg.external_ids,
                             "acls": [r.uuid for r in pg.acls]})
             except idlutils.RowNotFound:
                 pass
@@ -301,6 +310,7 @@ class OVNDriver(base.DriverBase):
                     pg = self.ovn_nb.lookup("Port_Group",
                                             utils.ovn_port_group_name(sg_id))
                     pgs.append({"name": pg.name,
+                                "external_ids": pg.external_ids,
                                 "acls": [r.uuid for r in pg.acls]})
                 except idlutils.RowNotFound:
                     pass
