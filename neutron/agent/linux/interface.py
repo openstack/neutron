0396b33bc4db43798490fefaf97a3bfcbdba29d0
Rodolfo Alonso <ralonsoh@redhat.com>
2024-01-24 07:39:09 +0000

    Revert "Add sleep before checking if ovs port is in the namespace"
    This reverts commit 222c997022392561c2de2cb493f0f5214eb20dfc.
    
    Reason for revert:
    This patch seems to be breaking the test
    "test_multiple_agents_for_network(Open vSwitch agent)" in the
    fullstack job.
    
    Change-Id: Ib93ebdcceb177c297b3b287fda01e3d57a275cb4
    Related-Bug: #1961740

--- neutron/agent/linux/interface.py
+++ neutron/agent/linux/interface.py
@@ -355,7 +355,7 @@ class OVSInterfaceDriver(LinuxInterfaceDriver):
         namespace_obj = ip_wrapper.ensure_namespace(namespace)
         for i in range(9):
             try:
-                namespace_obj.add_device_to_namespace(device, is_ovs_port=True)
+                namespace_obj.add_device_to_namespace(device)
                 break
             except ip_lib.NetworkInterfaceNotFound:
                 # NOTE(slaweq): if the exception was NetworkInterfaceNotFound
