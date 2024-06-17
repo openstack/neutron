980f9bdab2f8a0895bb0373fc73788cec93ee649
Rodolfo Alonso Hernandez <ralonsoh@redhat.com>
2024-06-17 07:10:03 +0000

    [OVN] Add a new process to spawn ML2/OVN maintenance worker
    When using the Neutron WSGI module, the ML2/OVN maintenance worker needs
    to be spawned in a separate service. This patch adds the service
    ``neutron-ovn-maintenance-worker``, that is a single process service tha
    runs the ``MaintenanceWorker`` instance. This process is in charge of
    performing periodic routines related to the ML2/OVN driver.
    
    This new service should be included in any deployment project that
    allows to spawn Neutron ML2/OVN with WSGI. Along with this patch, a new
    one for devstack will be proposed.
    
    Related-Bug: #1912359
    Change-Id: Iea2995adb3343aae74a1b617fbccfce5c62c6b87

---
+++ neutron/server/ovn_maintenance.py
@@ -0,0 +1,42 @@
+# Copyright 2024 Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron_lib.api import attributes
+from oslo_log import log
+
+from neutron._i18n import _
+from neutron.api import extensions
+from neutron import manager
+from neutron import service
+
+LOG = log.getLogger(__name__)
+
+
+def eventlet_ovn_maintenance_worker():
+    LOG.info('Eventlet OVN maintenance process starting...')
+
+    try:
+        manager.init()
+        ext_mgr = extensions.PluginAwareExtensionManager.get_instance()
+        ext_mgr.extend_resources('2.0', attributes.RESOURCES)
+        ovn_maintenance_worker = service.start_ovn_maintenance_worker()
+        if not ovn_maintenance_worker:
+            raise RuntimeError(_('OVN maintenance worker not loaded, ML2/OVN '
+                                 'mechanism driver must be used'))
+    except NotImplementedError:
+        LOG.info('OVN maintenance worker was already started in parent '
+                 'process by plugin.')
+    else:
+        ovn_maintenance_worker.wait()
