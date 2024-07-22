openstack/neutron

commit 0a554b4f29a800694539b285c75eb7f8a2a69193

Add support for OVN MAC_Binding aging
OVN added support for aging out MAC_Binding entries [1][2].
Without this feature, the MAC_Bindings table can grow indefinitely.

[1] https://github.com/ovn-org/ovn/commit/1a947dd3073628d2f2655f46ee7d3db62ed15b55
[2] https://github.com/ovn-org/ovn/commit/cecac71c0e49f9bfb6595bc03a13f3f7644dd268

Closes-Bug: 2033932
Change-Id: I91070ad6addb30ffdedba5d561984d2f6626e2b7

neutron/conf/plugins/ml2/drivers/ovn/ovn_conf.py
@@ -232,6 +232,11 @@ ovn_opts = [
                help=_('The number of seconds to keep FDB entries in the OVN '
                       'DB. The value defaults to 0, which means disabled. '
                       'This is supported by OVN >= 23.09.')),
+    cfg.IntOpt('mac_binding_age_threshold',
+               min=0,
+               default=0,
+               help=_('The number of seconds to keep MAC_Binding entries in '
+                      'the OVN DB. 0 to disable aging.')),
 ]
 
 nb_global_opts = [
@@ -255,6 +260,14 @@ nb_global_opts = [
                       'is 0, which is unlimited. When the limit is reached, '
                       'the next batch removal is delayed by 5 seconds. '
                       'This is supported by OVN >= 23.09.')),
+    cfg.IntOpt('mac_binding_removal_limit',
+               min=0,
+               default=0,
+               help=_('MAC binding aging bulk removal limit. This limits how '
+                      'many entries can expire in a single transaction. '
+                      'The default is 0 which is unlimited. When the limit '
+                      'is reached, the next batch removal is delayed by '
+                      '5 seconds.')),
 ]
 
 
@@ -382,3 +395,8 @@ def get_fdb_age_threshold():
 
 def get_fdb_removal_limit():
     return str(cfg.CONF.ovn_nb_global.fdb_removal_limit)
+
+
+def get_ovn_mac_binding_age_threshold():
+    # This value is always stored as a string in the OVN DB
+    return str(cfg.CONF.ovn.mac_binding_age_threshold)

neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/maintenance.py
@@ -832,6 +832,15 @@ class DBInconsistenciesPeriodics(SchemaAwarePeriodicsBase):
                     txn.add(cmd)
         raise periodics.NeverAgain()
 
+    # A static spacing value is used here, but this method will only run
+    # once per lock due to the use of periodics.NeverAgain().
+    @has_lock_periodic(spacing=600, run_immediately=True)
+    def update_mac_aging_settings(self):
+        """Ensure that MAC_Binding aging options are set"""
+        with self._nb_idl.transaction(check_error=True) as txn:
+            txn.add(self._nb_idl.set_router_mac_age_limit())
+        raise periodics.NeverAgain()
+
     # TODO(fnordahl): Remove this in the B+3 cycle. This method removes the
     # now redundant  "external_ids:OVN_GW_NETWORK_EXT_ID_KEY" and
     # "external_ids:OVN_GW_PORT_EXT_ID_KEY" from to each router.

neutron/common/ovn/constants.py
@@ -405,6 +405,8 @@ LSP_OPTIONS_MCAST_FLOOD = 'mcast_flood'
 LSP_OPTIONS_QOS_MIN_RATE = 'qos_min_rate'
 LSP_OPTIONS_LOCALNET_LEARN_FDB = 'localnet_learn_fdb'
 
+LR_OPTIONS_MAC_AGE_LIMIT = 'mac_binding_age_threshold'
+
 LRP_OPTIONS_RESIDE_REDIR_CH = 'reside-on-redirect-chassis'
 LRP_OPTIONS_REDIRECT_TYPE = 'redirect-type'
 BRIDGE_REDIRECT_TYPE = "bridged"

neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/api.py
@@ -183,6 +183,15 @@ class API(api.API, metaclass=abc.ABCMeta):
         """
 
     @abc.abstractmethod
+    def set_router_mac_age_limit(self, router=None):
+        """Set the OVN MAC_Binding age threshold
+
+        :param router: The name or UUID of a router, or None for all routers
+        :type router:  uuid.UUID or string or None
+        :returns:      :class:`Command` with no result
+        """
+
+    @abc.abstractmethod
     def add_acl(self, lswitch, lport, **columns):
         """Create an ACL for a logical port.
 

neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/commands.py
@@ -472,6 +472,40 @@ class SetLRouterPortInLSwitchPortCommand(command.BaseCommand):
         setattr(port, 'addresses', self.lsp_address)
 
 
+class SetLRouterMacAgeLimitCommand(command.BaseCommand):
+    def __init__(self, api, router, threshold):
+        super().__init__(api)
+        self.router = router
+        self.threshold = str(threshold)  # Just in case an integer sneaks in
+
+    def run_idl(self, txn):
+        # Creating a Command object that iterates over the list of Routers
+        # from inside a transaction avoids the issue of doing two
+        # transactions: one for list_rows() and the other for setting the
+        # values on routers, which would allow routers to be added and removed
+        # between the two transactions.
+        if self.router is None:
+            routers = self.api.tables["Logical_Router"].rows.values()
+        else:
+            routers = [self.api.lookup("Logical_Router", self.router)]
+
+        for router in routers:
+            # It's not technically necessary to check the value before setting
+            # it as python-ovs is smart enough to avoid sending operations to
+            # the server that would result in no change. The overhead of
+            # setkey() though is > than the overhead of checking the value here
+            try:
+                if (router.options.get(ovn_const.LR_OPTIONS_MAC_AGE_LIMIT) ==
+                        self.threshold):
+                    continue
+            except AttributeError:
+                # The Logical_Router is newly created in this txn and has no
+                # "options" set yet, which the following setkey will rectify
+                pass
+            router.setkey("options", ovn_const.LR_OPTIONS_MAC_AGE_LIMIT,
+                          self.threshold)
+
+
 class AddACLCommand(command.BaseCommand):
     def __init__(self, api, lswitch, lport, **columns):
         super(AddACLCommand, self).__init__(api)

neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/impl_idl_ovn.py
@@ -853,6 +853,11 @@ class OvsdbNbOvnIdl(nb_impl_idl.OvnNbApiIdlImpl, Backend):
         LOG.debug("Setting NB_Global options: %s", options)
         return self.db_set("NB_Global", ".", options=options)
 
+    def set_router_mac_age_limit(self, router=None):
+        # Set the MAC_Binding age limit on OVN Logical Routers
+        return cmd.SetLRouterMacAgeLimitCommand(
+            self, router, cfg.get_ovn_mac_binding_age_threshold())
+
 
 class OvsdbSbOvnIdl(sb_impl_idl.OvnSbApiIdlImpl, Backend):
     def __init__(self, connection):

neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/ovn_client.py
@@ -1346,7 +1346,9 @@ class OVNClient(object):
         lrouter_name = utils.ovn_name(router['id'])
         added_gw_port = None
         options = {'always_learn_from_arp_request': 'false',
-                   'dynamic_neigh_routers': 'true'}
+                   'dynamic_neigh_routers': 'true',
+                   ovn_const.LR_OPTIONS_MAC_AGE_LIMIT:
+                   ovn_conf.get_ovn_mac_binding_age_threshold()}
         with self._nb_idl.transaction(check_error=True) as txn:
             txn.add(self._nb_idl.create_lrouter(lrouter_name,
                                                 external_ids=external_ids,

neutron/tests/functional/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_impl_idl.py
@@ -383,6 +383,27 @@ class TestNbApi(BaseOvnIdlTest):
         lsp = self.nbapi.lookup('Logical_Switch_Port', lsp_name)
         self.assertEqual([], lsp.ha_chassis_group)
 
+    def test_set_router_mac_aging(self):
+        name = "aging_router1"
+        with self.nbapi.transaction(check_error=True) as txn:
+            r = txn.add(self.nbapi.lr_add(name))
+            txn.add(self.nbapi.set_router_mac_age_limit(router=name))
+        self.assertEqual(r.result.options[ovn_const.LR_OPTIONS_MAC_AGE_LIMIT],
+                         ovn_conf.get_ovn_mac_binding_age_threshold())
+
+    def test_set_router_mac_aging_all(self):
+        ovn_conf.cfg.CONF.set_override("mac_binding_age_threshold", 5,
+                                       group="ovn")
+        names = ["aging_router2", "aging_router3"]
+        with self.nbapi.transaction(check_error=True) as txn:
+            for name in names:
+                txn.add(self.nbapi.lr_add(name))
+            txn.add(self.nbapi.set_router_mac_age_limit())
+        for name in names:
+            r = self.nbapi.lookup("Logical_Router", name)
+            self.assertEqual(r.options[ovn_const.LR_OPTIONS_MAC_AGE_LIMIT],
+                             ovn_conf.get_ovn_mac_binding_age_threshold())
+
 
 class TestIgnoreConnectionTimeout(BaseOvnIdlTest):
     @classmethod

commit b4c8cc600a21469e247a0012585969a7897a0929

Actualy set global "removal limit" options
Neither fdb_removal_limit nor mac_binding_removal_limit config
options currently get set in the OVN DB. This patch corrects that
and adds missing testing for the MAC_Binding aging maintenance
task.

Fixes: 0a554b4f29 ("Add support for OVN MAC_Binding aging")
Fixes: 1e9f50c736 ("Add support for FDB aging")
Closes-Bug: #2073309
Change-Id: I80d79faeb9f1057d398ee750ae6e246598fd13d2

neutron/conf/plugins/ml2/drivers/ovn/ovn_conf.py
@@ -386,5 +386,9 @@ def get_ovn_mac_binding_age_threshold():
     return str(cfg.CONF.ovn.mac_binding_age_threshold)
 
 
+def get_ovn_mac_binding_removal_limit():
+    return str(cfg.CONF.ovn_nb_global.mac_binding_removal_limit)
+
+
 def is_broadcast_arps_to_all_routers_enabled():
     return cfg.CONF.ovn.broadcast_arps_to_all_routers

neutron/plugins/ml2/drivers/ovn/mech_driver/ovsdb/maintenance.py
@@ -761,7 +761,10 @@ class DBInconsistenciesPeriodics(SchemaAwarePeriodicsBase):
         Ensure FDB aging settings are enforced.
         """
         context = n_context.get_admin_context()
-        cmds = []
+        cmds = [self._nb_idl.db_set(
+                    "NB_Global", '.',
+                    options={"fdb_removal_limit":
+                             ovn_conf.get_fdb_removal_limit()})]
 
         config_fdb_age_threshold = ovn_conf.get_fdb_age_threshold()
         # Get provider networks
@@ -792,6 +795,10 @@ class DBInconsistenciesPeriodics(SchemaAwarePeriodicsBase):
     def update_mac_aging_settings(self):
         """Ensure that MAC_Binding aging options are set"""
         with self._nb_idl.transaction(check_error=True) as txn:
+            txn.add(self._nb_idl.db_set(
+                "NB_Global", ".",
+                options={"mac_binding_removal_limit":
+                         ovn_conf.get_ovn_mac_binding_removal_limit()}))
             txn.add(self._nb_idl.set_router_mac_age_limit())
         raise periodics.NeverAgain()
 

neutron/tests/functional/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_maintenance.py
@@ -775,8 +775,12 @@ class TestMaintenance(_TestMaintenanceHelper):
         self.assertEqual(
             '0', ls.other_config.get(ovn_const.LS_OPTIONS_FDB_AGE_THRESHOLD))
 
+        self.assertEqual(
+            '0', self.nb_api.nb_global.options.get("fdb_removal_limit", '0'))
+
         # Change the value of the configuration
         cfg.CONF.set_override('fdb_age_threshold', 5, group='ovn')
+        cfg.CONF.set_override('fdb_removal_limit', 100, group='ovn_nb_global')
 
         # Call the maintenance task and check that the value has been
         # updated in the Logical Switch
@@ -787,6 +791,51 @@ class TestMaintenance(_TestMaintenanceHelper):
 
         self.assertEqual(
             '5', ls.other_config.get(ovn_const.LS_OPTIONS_FDB_AGE_THRESHOLD))
+        self.assertEqual(
+            '100', self.nb_api.nb_global.options.get("fdb_removal_limit"))
+
+    def test_update_mac_aging_settings(self):
+        ext_net = self._create_network('ext_networktest', external=True)
+        ext_subnet = self._create_subnet(
+            'ext_subnettest',
+            ext_net['id'],
+            **{'cidr': '100.0.0.0/24',
+               'gateway_ip': '100.0.0.254',
+               'allocation_pools': [
+                   {'start': '100.0.0.2', 'end': '100.0.0.253'}],
+               'enable_dhcp': False})
+        self._create_network('network1test', external=False)
+        external_gateway_info = {
+            'enable_snat': True,
+            'network_id': ext_net['id'],
+            'external_fixed_ips': [
+                {'ip_address': '100.0.0.2', 'subnet_id': ext_subnet['id']}]}
+        router = self._create_router(
+            'routertest', external_gateway_info=external_gateway_info)
+
+        options = self.nb_api.nb_global.options
+        lr = self.nb_api.get_lrouter(router["id"])
+
+        self.assertEqual(
+            '0', lr.options.get(ovn_const.LR_OPTIONS_MAC_AGE_LIMIT))
+
+        self.assertEqual('0', options.get('mac_binding_removal_limit', '0'))
+
+        cfg.CONF.set_override("mac_binding_age_threshold", 5, group="ovn")
+        cfg.CONF.set_override("mac_binding_removal_limit", 100,
+                              group="ovn_nb_global")
+
+        # Call the maintenance task and check that the value has been
+        # updated in the Logical Switch
+        self.assertRaises(periodics.NeverAgain,
+                          self.maint.update_mac_aging_settings)
+
+        lr = self.nb_api.get_lrouter(router['id'])
+        options = self.nb_api.nb_global.options
+
+        self.assertEqual(
+            '5', lr.options.get(ovn_const.LR_OPTIONS_MAC_AGE_LIMIT))
+        self.assertEqual('100', options['mac_binding_removal_limit'])
 
     def test_floating_ip(self):
         ext_net = self._create_network('ext_networktest', external=True)

neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_maintenance.py
@@ -833,9 +833,13 @@ class TestDBInconsistenciesPeriodics(testlib_api.SqlTestCaseLight,
             periodics.NeverAgain,
             self.periodic.check_fdb_aging_settings)
 
-        self.fake_ovn_client._nb_idl.db_set.assert_called_once_with(
-            'Logical_Switch', 'neutron-foo',
-            ('other_config', {constants.LS_OPTIONS_FDB_AGE_THRESHOLD: '5'}))
+        self.fake_ovn_client._nb_idl.db_set.assert_has_calls([
+            mock.call('NB_Global', '.',
+                      options={'fdb_removal_limit':
+                               ovn_conf.get_fdb_removal_limit()}),
+            mock.call('Logical_Switch', 'neutron-foo',
+                      ('other_config',
+                      {constants.LS_OPTIONS_FDB_AGE_THRESHOLD: '5'}))])
 
     def test_check_fdb_aging_settings_with_threshold_set(self):
         cfg.CONF.set_override('fdb_age_threshold', 5, group='ovn')
@@ -850,7 +854,12 @@ class TestDBInconsistenciesPeriodics(testlib_api.SqlTestCaseLight,
             periodics.NeverAgain,
             self.periodic.check_fdb_aging_settings)
 
-        self.fake_ovn_client._nb_idl.db_set.assert_not_called()
+        # It doesn't really matter if db_set is called or not for the
+        # ls. This is called one time at startup and python-ovs will
+        # not send the transaction if it doesn't cause a change
+        self.fake_ovn_client._nb_idl.db_set.assert_called_once_with(
+            'NB_Global', '.',
+            options={'fdb_removal_limit': ovn_conf.get_fdb_removal_limit()})
 
     def test_remove_gw_ext_ids_from_logical_router(self):
         nb_idl = self.fake_ovn_client._nb_idl