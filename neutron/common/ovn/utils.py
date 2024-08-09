6f77c811f73ca1e5c5f4e599ff5c788dd28f6916
Elvira García <egarciar@redhat.com>
2024-08-09 18:16:59 +0200

    Get ips from system dns resolver without scope
    Currently, is_valid_ipv6 accepts ipv6 addresses with scope. However
    netaddr library won't accept an address with scope. Now,
    get_noscope_ipv6() can be used to avoid this situation. In a future we
    will be able to use the same function which is also being defined on
    oslo.utils. https://review.opendev.org/c/openstack/oslo.utils/+/925469
    
    Closes-Bug: #2073894
    Signed-off-by: Elvira García <egarciar@redhat.com>
    Change-Id: I27f25f90c54d7aaa3c4a7b5317b4b8a4122e4068
    (cherry picked from commit 1ed8609a6818d99133bf56483adb9bce8c886fd6)

--- neutron/common/ovn/utils.py
+++ neutron/common/ovn/utils.py
@@ -41,6 +41,7 @@ import tenacity
 
 from neutron._i18n import _
 from neutron.common import _constants as n_const
+from neutron.common import ipv6_utils
 from neutron.common.ovn import constants
 from neutron.common.ovn import exceptions as ovn_exc
 from neutron.common import utils as common_utils
@@ -605,6 +606,8 @@ def get_system_dns_resolvers(resolver_file=DNS_RESOLVER_FILE):
             valid_ip = (netutils.is_valid_ipv4(line, strict=True) or
                         netutils.is_valid_ipv6(line))
             if valid_ip:
+                if netutils.is_valid_ipv6(line):
+                    line = ipv6_utils.get_noscope_ipv6(line)
                 resolvers.append(line)
 
     return resolvers
