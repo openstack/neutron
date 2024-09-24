e78a2768c0375eb199d04539a4ee26d1faa711e9
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-09-24 22:34:24 +0900

    Drop workaround for eventlet < 0.22.0
    ... because now the minimum required version is 0.36.1 .
    
    Change-Id: I74938a54ffd1e05d732a8d79ed492e572f724f7e

--- neutron/common/eventlet_utils.py
+++ neutron/common/eventlet_utils.py
@@ -20,13 +20,6 @@ from oslo_utils import importutils
 
 
 def monkey_patch():
-    # NOTE(slaweq): to workaround issue with import cycles in
-    # eventlet < 0.22.0;
-    # This issue is fixed in eventlet with patch
-    # https://github.com/eventlet/eventlet/commit/b756447bab51046dfc6f1e0e299cc997ab343701
-    # For details please check https://bugs.launchpad.net/neutron/+bug/1745013
-    hub = eventlet.hubs.get_hub()
-    hub.is_available = lambda: True
     if os.name != 'nt':
         eventlet.monkey_patch()
 
