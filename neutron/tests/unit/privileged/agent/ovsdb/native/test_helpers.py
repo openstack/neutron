openstack/neutron

commit 03a976d933865b45f481ba1539b61e199aab7446

Fix wrong handling of IPv6 ovsdb_connection
The current logic is not able to handle ovsdb_connection in case it
contains IPv6 address, because it does not care about the points
below.
 - Distinguish ':' in address and ':' as a separator
 - Escape IPv6 address

Also validate configured values to ensure that connection contains
the appropriate protocol prefix.

Change-Id: Ib48212b52b2ab4c2469da3d2a5fbcdf7d1bbf417

neutron/conf/agent/ovsdb_api.py
@@ -20,6 +20,7 @@ from oslo_config import cfg
 API_OPTS = [
     cfg.StrOpt('ovsdb_connection',
                default='tcp:127.0.0.1:6640',
+               regex=r'^(tcp|tls|ssl):.+',
                help=_('The connection string for the OVSDB backend. '
                       'Will be used for all OVSDB commands and '
                       'by ovsdb-client when monitoring'

neutron/tests/unit/privileged/agent/ovsdb/native/test_helpers.py
@@ -1,0 +1,51 @@
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+
+from neutron.tests import base
+
+from neutron.privileged.agent.ovsdb.native import helpers
+
+
+class OvsdbNativeHelpersTestCase(base.BaseTestCase):
+
+    def test__connect_to_manager_uri_ipv4(self):
+        self.assertEqual(
+            'ptcp:6640:127.0.0.1',
+            helpers._connection_to_manager_uri('tcp:127.0.0.1:6640'))
+        self.assertEqual(
+            'ptls:6640:127.0.0.1',
+            helpers._connection_to_manager_uri('tls:127.0.0.1:6640'))
+        self.assertEqual(
+            'ptcp:127.0.0.1',
+            helpers._connection_to_manager_uri('tcp:127.0.0.1'))
+
+    def test__connect_to_manager_uri_ipv6(self):
+        self.assertEqual(
+            'ptcp:6640:[::1]',
+            helpers._connection_to_manager_uri('tcp:[::1]:6640'))
+        self.assertEqual(
+            'ptls:6640:[::1]',
+            helpers._connection_to_manager_uri('tls:[::1]:6640'))
+        self.assertEqual(
+            'ptcp:[::1]',
+            helpers._connection_to_manager_uri('tcp:[::1]'))
+
+    def test__connect_to_manager_uri_hostname(self):
+        self.assertEqual(
+            'ptcp:6640:localhost',
+            helpers._connection_to_manager_uri('tcp:localhost:6640'))
+        self.assertEqual(
+            'ptls:6640:localhost',
+            helpers._connection_to_manager_uri('tls:localhost:6640'))
+        self.assertEqual(
+            'ptcp:localhost',
+            helpers._connection_to_manager_uri('tcp:localhost'))

neutron/conf/agent/ovn/metadata/config.py
@@ -24,6 +24,7 @@ from neutron._i18n import _
 OVS_OPTS = [
     cfg.StrOpt('ovsdb_connection',
                default='unix:/usr/local/var/run/openvswitch/db.sock',
+               regex=r'^(tcp|ssl|unix):.+',
                help=_('The connection string for the native OVSDB backend. '
                       'Use tcp:IP:PORT for TCP connections. '
                       'Use unix:FILE for unix domain socket connections.')),

neutron/privileged/agent/ovsdb/native/helpers.py
@@ -13,14 +13,16 @@
 #    under the License.
 
 from oslo_concurrency import processutils
+from oslo_utils import netutils
 
 from neutron import privileged
 
 
 def _connection_to_manager_uri(conn_uri):
     proto, addr = conn_uri.split(':', 1)
-    if ':' in addr:
-        ip, port = addr.split(':', 1)
+    ip, port = netutils.parse_host_port(addr)
+    if port is not None:
+        ip = netutils.escape_ipv6(ip)
         return 'p%s:%s:%s' % (proto, port, ip)
     return 'p%s:%s' % (proto, addr)
 

commit dad75a8202b410a4597a2ffd6c9659115d64bdc1

Fix wrong rejex for [ovs] ovsdb_connection
The change 03a976d933865b45f481ba1539b61e199aab7446 introduced
the wrong regex which rejects usage of unix domain socket.

Also 'tls' is not a protocol actually accepted.

Closes-Bug: #2099450
Change-Id: I119f33a85df6f95c8cdc937fa2bb02b425a34de3

neutron/conf/agent/ovsdb_api.py
@@ -20,7 +20,7 @@ from oslo_config import cfg
 API_OPTS = [
     cfg.StrOpt('ovsdb_connection',
                default='tcp:127.0.0.1:6640',
-               regex=r'^(tcp|tls|ssl):.+',
+               regex=r'^(tcp|ssl|unix):.+',
                help=_('The connection string for the OVSDB backend. '
                       'Will be used for all OVSDB commands and '
                       'by ovsdb-client when monitoring'

neutron/tests/unit/privileged/agent/ovsdb/native/test_helpers.py
@@ -22,8 +22,8 @@ class OvsdbNativeHelpersTestCase(base.BaseTestCase):
             'ptcp:6640:127.0.0.1',
             helpers._connection_to_manager_uri('tcp:127.0.0.1:6640'))
         self.assertEqual(
-            'ptls:6640:127.0.0.1',
-            helpers._connection_to_manager_uri('tls:127.0.0.1:6640'))
+            'pssl:6640:127.0.0.1',
+            helpers._connection_to_manager_uri('ssl:127.0.0.1:6640'))
         self.assertEqual(
             'ptcp:127.0.0.1',
             helpers._connection_to_manager_uri('tcp:127.0.0.1'))
@@ -33,8 +33,8 @@ class OvsdbNativeHelpersTestCase(base.BaseTestCase):
             'ptcp:6640:[::1]',
             helpers._connection_to_manager_uri('tcp:[::1]:6640'))
         self.assertEqual(
-            'ptls:6640:[::1]',
-            helpers._connection_to_manager_uri('tls:[::1]:6640'))
+            'pssl:6640:[::1]',
+            helpers._connection_to_manager_uri('ssl:[::1]:6640'))
         self.assertEqual(
             'ptcp:[::1]',
             helpers._connection_to_manager_uri('tcp:[::1]'))
@@ -44,8 +44,14 @@ class OvsdbNativeHelpersTestCase(base.BaseTestCase):
             'ptcp:6640:localhost',
             helpers._connection_to_manager_uri('tcp:localhost:6640'))
         self.assertEqual(
-            'ptls:6640:localhost',
-            helpers._connection_to_manager_uri('tls:localhost:6640'))
+            'pssl:6640:localhost',
+            helpers._connection_to_manager_uri('ssl:localhost:6640'))
         self.assertEqual(
             'ptcp:localhost',
             helpers._connection_to_manager_uri('tcp:localhost'))
+
+    def test__connect_to_manager_uri_unix(self):
+        self.assertEqual(
+            'punix:/run/openvswitch/db.sock',
+            helpers._connection_to_manager_uri(
+                'unix:/run/openvswitch/db.sock'))