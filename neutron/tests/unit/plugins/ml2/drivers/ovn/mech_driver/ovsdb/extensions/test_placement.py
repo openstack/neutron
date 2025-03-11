de237bc92f56bd99c7021ebd79cdbbd8dfe5f450
Rodolfo Alonso Hernandez <ralonsoh@redhat.com>
2025-03-11 14:52:08 +0000

    [OVN] Delete a resource provider removed from the agent config
    In ML2/OVN, The Placement information per compute node is stored in the
    local OVS database, in the Open vSwitch register. Each time this
    information changes, ML2/OVN receives this event and updates the OVN
    agent child registers assigned to devices.
    
    Since this patch, if a resource provider is removed, the corresponding
    Placement resource provider register (and its inventory) is removed too.
    
    NOTE: if a resource provider has allocations, the delete operation will
    fail with HTTP 409 (Conflict) and the Neutron API will log this error.
    The error message will be:
      Unable to delete resource provider <UUID>: Resource provider has
      allocations.
    
    Closes-Bug: #2101998
    Change-Id: I78319cc3c94bf869383ecfaf14cb276b46086b8e

--- neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/ovsdb/extensions/test_placement.py
+++ neutron/tests/unit/plugins/ml2/drivers/ovn/mech_driver/ovsdb/extensions/test_placement.py
@@ -15,6 +15,7 @@
 from unittest import mock
 
 from neutron_lib import constants as n_const
+from oslo_utils import uuidutils
 
 from neutron.plugins.ml2.drivers.ovn.mech_driver.ovsdb.extensions \
     import placement as p_extension
@@ -32,6 +33,9 @@ class TestOVNClientPlacementExtension(test_plugin.Ml2PluginV2TestCase):
         self.plugin_driver = mock.Mock()
         self.placement_driver = p_extension.OVNClientPlacementExtension(
             self.plugin_driver)
+        # Ensure the ``OVNClientPlacementExtension`` object is new, that the
+        # previous instance has been deleted.
+        self.assertEqual(self.plugin_driver, self.placement_driver._driver)
         self.placement_client = mock.Mock(
             update_trait=mock.Mock(__name__='update_trait'),
             ensure_resource_provider=mock.Mock(__name__='ensure_rp'),
@@ -46,6 +50,18 @@ class TestOVNClientPlacementExtension(test_plugin.Ml2PluginV2TestCase):
             'resource_providers': [{'name': 'compute1', 'uuid': 'uuid1'},
                                    {'name': 'compute2', 'uuid': 'uuid2'}]
         }
+        self.name2uuid = self._gen_name2uuid(['compute1',
+                                              'compute2',
+                                              ])
+        self.addCleanup(self._delete_placement_singleton_instance)
+
+    def _delete_placement_singleton_instance(self):
+        del self.placement_driver
+
+    @staticmethod
+    def _gen_name2uuid(hypervisor_list):
+        return {hypervisor: uuidutils.generate_uuid() for
+                hypervisor in hypervisor_list}
 
     def test_read_initial_chassis_config(self):
         # Add two public networks, a RP per bridge and the correlation between
@@ -231,3 +247,43 @@ class TestOVNClientPlacementExtension(test_plugin.Ml2PluginV2TestCase):
             n_const.RP_HYPERVISORS: {}
         }}
         _check_expected_config(init_conf, expected)
+
+    def test_build_placement_state_no_rp_deleted(self):
+        chassis = fakes.FakeChassis.create(
+            bridge_mappings=['public1:br-ext1', 'public2:br-ext2'],
+            rp_bandwidths=['br-ext1:1000:2000', 'br-ext2:3000:4000'],
+            rp_inventory_defaults={'allocation_ratio': 1.0, 'min_unit': 5},
+            rp_hypervisors=['br-ext1:compute1', 'br-ext2:compute1'])
+        chassis_old = fakes.FakeChassis.create(
+            bridge_mappings=['public1:br-ext1', 'public2:br-ext2'],
+            rp_bandwidths=['br-ext1:1001:2002', 'br-ext2:3003:4004'],
+            rp_inventory_defaults={'allocation_ratio': 1.0, 'min_unit': 5},
+            rp_hypervisors=['br-ext1:compute1', 'br-ext2:compute1'])
+        report = self.placement_driver.build_placement_state(
+            chassis, self.name2uuid, chassis_old=chassis_old)
+        self.assertEqual(set(), report._rp_deleted)
+
+    def test_build_placement_state_rp_deleted(self):
+        chassis = fakes.FakeChassis.create(
+            bridge_mappings=['public1:br-ext1', 'public2:br-ext2'],
+            rp_bandwidths=['br-ext1:1000:2000'],
+            rp_inventory_defaults={'allocation_ratio': 1.0, 'min_unit': 5},
+            rp_hypervisors=['br-ext1:compute1', 'br-ext2:compute1'])
+        chassis_old = fakes.FakeChassis.create(
+            bridge_mappings=['public1:br-ext1', 'public2:br-ext2'],
+            rp_bandwidths=['br-ext1:1001:2002', 'br-ext2:3003:4004'],
+            rp_inventory_defaults={'allocation_ratio': 1.0, 'min_unit': 5},
+            rp_hypervisors=['br-ext1:compute1', 'br-ext2:compute1'])
+        report = self.placement_driver.build_placement_state(
+            chassis, self.name2uuid, chassis_old=chassis_old)
+        self.assertEqual({'br-ext2'}, report._rp_deleted)
+
+    def test_build_placement_state_no_old_chassis(self):
+        chassis = fakes.FakeChassis.create(
+            bridge_mappings=['public1:br-ext1', 'public2:br-ext2'],
+            rp_bandwidths=['br-ext1:1000:2000'],
+            rp_inventory_defaults={'allocation_ratio': 1.0, 'min_unit': 5},
+            rp_hypervisors=['br-ext1:compute1', 'br-ext2:compute1'])
+        report = self.placement_driver.build_placement_state(
+            chassis, self.name2uuid)
+        self.assertEqual(set(), report._rp_deleted)
