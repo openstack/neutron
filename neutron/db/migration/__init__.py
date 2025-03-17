125c8b8147258284210ba362809e92929bf43905
Rodolfo Alonso Hernandez <ralonsoh@redhat.com>
2025-03-17 20:46:40 +0000

    Open the 2025.2 (Flamingo) DB branch
    Change-Id: I9db60e78af6496a8fbf3c6a9e06b4d027ab08863

--- neutron/db/migration/__init__.py
+++ neutron/db/migration/__init__.py
@@ -42,6 +42,7 @@ RELEASE_2023_2 = '2023.2'
 RELEASE_2024_1 = '2024.1'
 RELEASE_2024_2 = '2024.2'
 RELEASE_2025_1 = '2025.1'
+RELEASE_2025_2 = '2025.2'
 
 NEUTRON_MILESTONES = [
     # earlier milestones were not tagged
@@ -64,6 +65,7 @@ NEUTRON_MILESTONES = [
     RELEASE_2023_2,
     RELEASE_2024_1,
     RELEASE_2024_2,
+    RELEASE_2025_1,
     # Do not add the milestone until the end of the release
 ]
 
