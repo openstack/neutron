8f3f0cd43dc0efc233eca9c66af70fbfbe6b00f0
Rodolfo Alonso Hernandez <ralonsoh@redhat.com>
2025-03-18 11:14:25 +0000

    [OVN][FT] Use MySQL backend for ``TestNBDbMonitor*`` classes
    This solution is similar to what was implemented in [1].
    
    [1]https://review.opendev.org/q/topic:%22bug/2088423%22
    
    Conflicts:
        neutron/tests/functional/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_ovsdb_monitor.py
    
    Related-Bug: #2088423
    Change-Id: Icd734d0bc26e6a2f62f7f9a53d6885fb38fded62
    (cherry picked from commit 0c345dce5b6ee024e54df4cbc128569d9d768cc6)
    (cherry picked from commit b62e7329a17d19bdc64ea950900e9c0883a18ae1)
    (cherry picked from commit fbff2ef269144b4ff820d08d0c8fcddbb0e9603b)

--- neutron/tests/functional/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_ovsdb_monitor.py
+++ neutron/tests/functional/plugins/ml2/drivers/ovn/mech_driver/ovsdb/test_ovsdb_monitor.py
@@ -28,6 +28,7 @@ from oslo_utils import timeutils
 from oslo_utils import uuidutils
 from ovsdbapp.backend.ovs_idl import event
 from ovsdbapp.backend.ovs_idl import idlutils
+from sqlalchemy.dialects.mysql import dialect as mysql_dialect
 import tenacity
 
 from neutron.common.ovn import constants as ovn_const
@@ -43,6 +44,7 @@ from neutron.tests.functional.resources.ovsdb import fixtures
 from neutron.tests.functional.resources import process
 from neutron.tests.unit.api import test_extensions
 from neutron.tests.unit.extensions import test_l3
+from neutron.tests.unit import testlib_api
 
 
 class WaitForDataPathBindingCreateEvent(event.WaitEvent):
@@ -85,10 +87,12 @@ class GlobalTestEvent(DistributedLockTestEvent):
     GLOBAL = True
 
 
-class TestNBDbMonitor(base.TestOVNFunctionalBase):
+class TestNBDbMonitor(testlib_api.MySQLTestCaseMixin,
+                      base.TestOVNFunctionalBase):
 
     def setUp(self):
         super(TestNBDbMonitor, self).setUp()
+        self.assertEqual(mysql_dialect.name, self.db.engine.dialect.name)
         self.chassis = self.add_fake_chassis('ovs-host1')
         self.l3_plugin = directory.get_plugin(plugin_constants.L3)
         self.net = self._make_network(self.fmt, 'net1', True)
