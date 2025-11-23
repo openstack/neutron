cf41465164e834cf5e24033ab24628cecd8d9647
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-11-24 01:34:36 +0900

    Use common constant for neutron-ovn-agent process name
    It was added to neutron-lib in its 0.10.0 release but hasn't been used
    by neutron actually.
    
    Depends-on: https://review.opendev.org/c/openstack/neutron-lib/+/931549
    Change-Id: Ib1e5264a397ff4ac84248fe1e93f2a16501a8cc3
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- neutron/cmd/agents/ovn_neutron_agent.py
+++ neutron/cmd/agents/ovn_neutron_agent.py
@@ -17,15 +17,13 @@ oslo_service_backend.init_backend(oslo_service_backend.BackendType.THREADING)
 # pylint: disable=wrong-import-position
 import setproctitle  # noqa: E402
 
+from neutron_lib import constants as lib_constants  # noqa: E402
+
 from neutron.agent.ovn import ovn_neutron_agent  # noqa: E402
 
 
-# TODO(ralonsoh): move to ``neutron_lib.constants``.
-AGENT_PROCESS_OVN_NEUTRON_AGENT = 'neutron-ovn-agent'
-
-
 def main():
-    proctitle = "{} ({})".format(AGENT_PROCESS_OVN_NEUTRON_AGENT,
+    proctitle = "{} ({})".format(lib_constants.AGENT_PROCESS_OVN,
                                  setproctitle.getproctitle())
     setproctitle.setproctitle(proctitle)
     ovn_neutron_agent.main()
